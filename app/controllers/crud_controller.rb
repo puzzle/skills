141c7dd5e02e1cb7a7b959d29d8a443df93ff01e
Janiss Binder <binder@puzzle.ch>
2016-10-18 14:21:16 +0200

    #20828 implemented dry_crud

---
+++ app/controllers/crud_controller.rb
@@ -0,0 +1,91 @@
+# A generic controller to display, create, update and destroy entries of a certain model class.
+class CrudController < ListController
+
+  class_attribute :permitted_attrs, :nested_models
+
+  # GET /users/1
+  def show(options = {})
+    authorize entry
+    render_entry(options[:render_options])
+  end
+
+  # POST /users
+  def create(options = {})
+    build_entry
+    authorize entry
+    if entry.save
+      render_entry({ status: :created,
+                     location: entry_url }
+                   .merge(options[:render_options] || {}))
+    else
+      render_errors
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update(options = {})
+    authorize entry
+    entry.attributes = model_params
+    if entry.save
+      render_entry(options[:render_options])
+    else
+      render_errors
+    end
+  end
+
+  # DELETE /users/1
+  def destroy(_options = {})
+    authorize entry
+    if entry.destroy
+      head 204
+    else
+      render_errors
+    end
+  end
+
+  private
+
+  def entry
+    instance_variable_get(:"@#{ivar_name}") ||
+      instance_variable_set(:"@#{ivar_name}", fetch_entry)
+  end
+
+  def fetch_entry
+    model_scope.find(params.fetch(:id))
+  end
+
+  def build_entry
+    instance_variable_set(:"@#{ivar_name}", model_scope.new(model_params))
+  end
+
+
+  def render_entry(options = {})
+    render({ json: entry,
+             serializer: model_serializer,
+             root: model_root_key }
+           .merge(render_options)
+           .merge(options || {}))
+  end
+
+  def render_errors
+    render json: entry.errors.details, status: :unprocessable_entity
+  end
+
+  def entry_url
+    send("#{self.class.name.underscore.
+         gsub(/_controller$/, '').
+         gsub(/\//, '_').singularize}_url", entry
+        )
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def model_params
+    attrs = params.require(model_identifier).permit(permitted_attrs)
+    AttributeDeserializer.new(attrs, nested_models: nested_models).run
+  end
+
+  def ivar_name
+    model_class.model_name.param_key
+  end
+
+end
---
+++ app/controllers/dry_crud/searchable.rb
@@ -0,0 +1,70 @@
+# The search functionality for the index table.
+# Define an array of searchable string columns in your subclassing
+# controllers using the class attribute +search_columns+.
+module Searchable
+
+  def self.prepended(klass)
+    klass.class_attribute :search_columns
+    klass.search_columns = []
+
+    klass.extend ClassMethods
+  end
+
+  private
+
+  # Enhance the list entries with an optional search criteria
+  def fetch_entries
+    super.where(search_conditions)
+  end
+
+  # Concat the word clauses with AND.
+  def search_conditions
+    if search_support? && params[:q].present?
+      search_word_conditions.reduce do |query, condition|
+        query.and(condition)
+      end
+    end
+  end
+
+  # Split the search query in single words and create a list of word clauses.
+  def search_word_conditions
+    params[:q].split(/\s+/).map { |w| search_word_condition(w) }
+  end
+
+  # Concat the column queries of the given word with OR.
+  def search_word_condition(word)
+    search_column_condition(word).reduce do |query, condition|
+      query.or(condition)
+    end
+  end
+
+  # Create a list of Arel #matches queries for each column and the given
+  # word.
+  def search_column_condition(word)
+    self.class.search_tables_and_fields.map do |table_name, field|
+      table = Arel::Table.new(table_name)
+      table[field].matches(Arel::Nodes::Quoted.new("%#{word}%"))
+    end
+  end
+
+  # Returns true if this controller has searchable columns.
+  def search_support?
+    search_columns.present?
+  end
+
+  # Class methods for Searchable.
+  module ClassMethods
+
+    # All search columns divided in table and field names.
+    def search_tables_and_fields
+      @search_tables_and_fields ||= search_columns.map do |f|
+        if f.to_s.include?('.')
+          f.split('.', 2)
+        else
+          [model_class.table_name, f]
+        end
+      end
+    end
+  end
+
+end
