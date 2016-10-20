011f66c48192887a9a11c49cfad557c3d486e420
Felix Ammann <ammann@puzzle.ch>
2016-10-20 14:24:27 +0200

    add scheme.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,79 +15,113 @@ ActiveRecord::Schema.define(version: 20161003130519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.text     "role"
-    t.integer  "year_from"
-    t.integer  "year_to"
-    t.integer  "person_id"
+  create_table "activities", id: :integer, force: :cascade do |t|
+    t.string   "description", limit: 5000, null: false
+    t.datetime "updated_at",               null: false
+    t.string   "updated_by",  limit: 255,  null: false
+    t.string   "role",        limit: 500,  null: false
+    t.integer  "year_from",                null: false
+    t.integer  "year_to",                  null: false
+    t.integer  "person_id",                null: false
     t.datetime "created_at"
   end
 
-  create_table "advanced_trainings", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.integer  "year_from"
-    t.integer  "year_to"
-    t.integer  "person_id"
+  create_table "advanced_trainings", id: :integer, force: :cascade do |t|
+    t.string   "description", limit: 5000, null: false
+    t.datetime "updated_at",               null: false
+    t.string   "updated_by",  limit: 255,  null: false
+    t.integer  "year_from",                null: false
+    t.integer  "year_to",                  null: false
+    t.integer  "person_id",                null: false
     t.datetime "created_at"
   end
 
-  create_table "competences", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.integer  "person_id"
+  create_table "competences", id: :integer, force: :cascade do |t|
+    t.string   "description", limit: 5000,             null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "updated_by",  limit: 255,              null: false
+    t.integer  "person_id",                default: 1, null: false
+    t.datetime "created_at"
+    t.index ["person_id"], name: "uk_d8au84bmlr3io7bk9c2hu0puq", unique: true, using: :btree
+  end
+
+  create_table "educations", id: :integer, force: :cascade do |t|
+    t.string   "location",   limit: 500, null: false
+    t.string   "type",       limit: 500, null: false
+    t.datetime "updated_at",             null: false
+    t.string   "updated_by", limit: 255, null: false
+    t.integer  "year_from",              null: false
+    t.integer  "year_to",                null: false
+    t.integer  "person_id",              null: false
     t.datetime "created_at"
   end
 
-  create_table "educations", force: :cascade do |t|
-    t.text     "location"
-    t.text     "type"
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.integer  "year_from"
-    t.integer  "year_to"
-    t.integer  "person_id"
+  create_table "expertise_category", id: :integer, force: :cascade do |t|
+    t.string "expertise_discipline", limit: 255, null: false
+    t.string "name",                 limit: 255, null: false
+  end
+
+  create_table "expertise_topic", id: :integer, force: :cascade do |t|
+    t.string  "name",                 limit: 255, null: false
+    t.boolean "user_topic",                       null: false
+    t.integer "fk_expertisecategory",             null: false
+  end
+
+  create_table "expertise_topic_skill_values", id: :integer, force: :cascade do |t|
+    t.string  "description",         limit: 255, null: false
+    t.integer "number_of_projects",              null: false
+    t.string  "skill_level",         limit: 255, null: false
+    t.integer "year_of_last_use",                null: false
+    t.integer "years_of_experience",             null: false
+    t.integer "fk_expertise_topic",              null: false
+    t.integer "fk_person",                       null: false
+  end
+
+  create_table "people", id: :integer, force: :cascade do |t|
+    t.datetime "birthdate",                                                          null: false
+    t.integer  "profile_picture"
+    t.string   "language",        limit: 100,                                        null: false
+    t.string   "location",        limit: 100,                                        null: false
+    t.string   "martial_status",  limit: 100,                                        null: false
+    t.datetime "updated_at",                                                         null: false
+    t.string   "updated_by",      limit: 255,                                        null: false
+    t.string   "name",            limit: 100,                                        null: false
+    t.string   "origin",          limit: 100,                                        null: false
+    t.string   "role",            limit: 100,                                        null: false
+    t.string   "title",           limit: 100,                                        null: false
+    t.integer  "status_id",                   default: 1,                            null: false
+    t.integer  "variation_id",                default: -1,                           null: false
+    t.string   "variation_name",  limit: 100, default: "aktuelles CV",               null: false
+    t.datetime "variation_date",              default: -> { "('now'::text)::date" }, null: false
+    t.datetime "created_at"
+    t.index ["name", "birthdate", "variation_name"], name: "uk_bricss82wi8axdqf9hvmwn2e2", unique: true, using: :btree
+    t.index ["name", "birthdate", "variation_name"], name: "uk_name_birtdate_variation", unique: true, using: :btree
+  end
+
+  create_table "projects", id: :integer, force: :cascade do |t|
+    t.datetime "updated_at",                           null: false
+    t.string   "updated_by",  limit: 255,              null: false
+    t.string   "description", limit: 5000,             null: false
+    t.string   "title",       limit: 500,              null: false
+    t.string   "role",        limit: 5000,             null: false
+    t.string   "technology",  limit: 5000,             null: false
+    t.integer  "yearfrom",                             null: false
+    t.integer  "year_to",                              null: false
+    t.integer  "person_id",                default: 1, null: false
     t.datetime "created_at"
   end
 
-  create_table "people", force: :cascade do |t|
-    t.datetime "birthdate"
-    t.binary   "profile_picture", null: false
-    t.string   "language"
-    t.string   "location"
-    t.string   "martial_status"
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.string   "name"
-    t.string   "origin"
-    t.string   "role"
-    t.string   "title"
-    t.integer  "status_id"
-    t.integer  "variation_id"
-    t.string   "variation_name"
-    t.datetime "variation_date"
-    t.datetime "created_at"
+  create_table "statuses", id: :integer, force: :cascade do |t|
+    t.string "status", limit: 100, null: false
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.datetime "updated_at"
-    t.string   "updated_by"
-    t.text     "description"
-    t.text     "title"
-    t.text     "role"
-    t.text     "technology"
-    t.integer  "year_to"
-    t.integer  "person_id"
-    t.datetime "created_at"
-  end
-
-  create_table "statuses", force: :cascade do |t|
-    t.string "status"
-  end
-
+  add_foreign_key "activities", "people", name: "fk_h60x6cdgg0viu1yqxmksjqhrd"
+  add_foreign_key "advanced_trainings", "people", name: "fk_9d1no9ju30dtstakwm89jwtev"
+  add_foreign_key "competences", "people", name: "fk_person_competence"
+  add_foreign_key "educations", "people", name: "fk_qr8q3lcwnth3jlf68cr8unohs"
+  add_foreign_key "expertise_topic", "expertise_category", column: "fk_expertisecategory", name: "fk_sgn8rfc9d3cmay8ifiwuftd0s"
+  add_foreign_key "expertise_topic_skill_values", "expertise_topic", column: "fk_expertise_topic", name: "fk_43p7gm1x9blbpeccvrcv3xdt8"
+  add_foreign_key "expertise_topic_skill_values", "people", column: "fk_person", name: "fk_f8q96ahd85871dq3396xboprs"
+  add_foreign_key "people", "statuses", name: "fk_fvgor3lwaybcryho1rkecma08"
+  add_foreign_key "projects", "people", name: "fk_person_project"
 end
