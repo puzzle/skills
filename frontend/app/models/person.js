6c74d13cbecd814753a85bcd1a233ec8e0fb8997
Felix Ammann <ammann@puzzle.ch>
2016-12-13 10:14:34 +0100

    extend person model

--- frontend/app/models/person.js
+++ frontend/app/models/person.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  birthdate: DS.attr('date'),
+  profile_picture: DS.attr('string'),
+  language: DS.attr('string'),
+  location: DS.attr('string'),
+  martial_status: DS.attr('string'),
+  origin: DS.attr('string'),
+  role: DS.attr('string'),
+  title: DS.attr('string'),
+  type: DS.attr('string')
 });
