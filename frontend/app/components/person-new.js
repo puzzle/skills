d4345c3f93e091eb5df6f60c31cc388ba28319c5
Felix Ammann <ammann@puzzle.ch>
2017-01-05 15:55:20 +0100

    #23024

--- frontend/app/components/person-new.js
+++ frontend/app/components/person-new.js
@@ -14,8 +14,10 @@ export default Ember.Component.extend({
   actions: {
     submit(newPerson) {
       return newPerson.save()
-        .then(() => this.sendAction('submit'))
-       this.get('router').transitionTo('people')
+        .then(
+          () => this.sendAction('submit'),
+          this.get('router').transitionTo('people')
+          )
     }
   }
 });
