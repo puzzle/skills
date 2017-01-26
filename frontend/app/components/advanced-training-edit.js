952f1287b4c3cc4fe66e77ccb8649fd8e0c5d053
Janiss Binder <binder@puzzle.ch>
2017-01-26 16:09:58 +0100

    wip

--- frontend/app/components/advanced-training-edit.js
+++ frontend/app/components/advanced-training-edit.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    submit(changeset) {
-      console.log(changeset);
-      return changeset.save()
-        .then(() => this.sendAction('submit'));
+    submit(changeset, event) {
+      event.preventDefault();
+      return changeset.save().then(
+        advanced_training => this.sendAction('done')
+      );
+    },
+    deleteEducation(education, event) {
+      education.destroyRecord().then(
+        advanced_training => this.sendAction('done')
+      );
     }
   }
 });
