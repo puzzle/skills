382bca40796f7f776e5d46911b877106f542fce3
Felix Ammann <ammann@puzzle.ch>
2017-02-17 17:50:05 +0100

    #22542 status filter

--- frontend/app/components/people-list.js
+++ frontend/app/components/people-list.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
+import PersonModel from '../models/person';
+
+const { Component, inject, computed } = Ember;
 
 export default Ember.Component.extend({
+  filterBy: 'all',
+  
+  filteredList: function(){
+    if (this.filterBy === 'all'){
+      return this.people
+    }
+    return this.people.filterBy('attributes.status_id', parseInt(this.filterBy))
+  }.property('people', 'filterBy'),
+
+  statusData:computed(function(){
+    return Object.keys(PersonModel.STATUSES).map(id => {
+      return { id, name: PersonModel.STATUSES[id] };
+    });
+  }),
+
+    actions: {
+      setFilter(value){
+        this.set('filterBy', value)
+      }
+    }
 });
