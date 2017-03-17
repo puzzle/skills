fe5bcc5e19a67f9c05c6439efa92e9903290480a
Felix Ammann <ammann@puzzle.ch>
2017-03-17 11:02:24 +0100

    #23772 login in development with random credentials

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,6 +4,8 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize
 
   def sign_in
+    return render json: nil if Rails.env.development?
+
     username = params[:username]
     password = params[:password]
 
