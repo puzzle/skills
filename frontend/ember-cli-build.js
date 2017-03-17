8d13ff276d64ddeb99e97e54ec39b5a8bc95a208
Damian Senn <senn@puzzle.ch>
2017-03-17 13:07:12 +0100

    Update ember-cli

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -1,9 +1,9 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
