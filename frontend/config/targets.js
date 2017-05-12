6677b41e457cea88e0868da8410b948bf67eff9d
Damian Senn <senn@puzzle.ch>
2017-05-12 08:39:20 +0200

    Update ember-cli
    (cherry picked from commit 7edea0e95420689fe82baa8503fa542d6b996c72)

---
+++ frontend/config/targets.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+module.exports = {
+  browsers: [
+    'ie 11',
+    'last 2 Edge versions',
+    'last 2 Chrome versions',
+    'last 2 Firefox versions',
+    'last 2 Safari versions',
+    'Firefox ESR'
+  ]
+};
