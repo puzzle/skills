dd4300126d178a3dbb589bbdc58aa1371259cc0b
Janiss Binder <binder@puzzle.ch>
2017-06-02 13:10:15 +0200

    redirect to login on 401 error, refs #24676

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,10 +1,22 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { UnauthorizedError, ForbiddenError } from 'ember-ajax/errors';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   session: Ember.inject.service('session'),
 
+  isAuthError(error) {
+    return error instanceof UnauthorizedError || error instanceof ForbiddenError ||
+      error instanceof DS.UnauthorizedError || error instanceof DS.ForbiddenError;
+  },
+
   actions: {
+    error(error, transition) {
+      if (this.isAuthError(error)) {
+        this.get('session').invalidate();
+      }
+    },
     invalidateSession() {
       this.get('session').invalidate();
     }
