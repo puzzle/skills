451cc12efd11fedea793e99eea774149546d23da
Janiss Binder <binder@puzzle.ch>
2017-06-20 08:48:57 +0200

    removed unused file

--- frontend/app/helpers/document-export-url.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import ENV from '../config/environment';
-
-export function documentExportUrl([personId]) {
-  return `localhost:3000/api/people/${personId}.odt`;
-}
-
-export default Ember.Helper.helper(documentExportUrl);
