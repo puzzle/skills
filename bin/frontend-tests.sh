72c1cd223f0deded6945ece5dac4eda4d1ed61a8
Damian Senn <senn@puzzle.ch>
2017-09-21 11:48:37 +0200

    Use embers exit code in frontend-tests.sh

--- bin/frontend-tests.sh
+++ bin/frontend-tests.sh
@@ -4,7 +4,11 @@ export RAILS_PORT=3001
 
 rails server -e test -d -p $RAILS_PORT
 
-cd frontend && COVERAGE=true yarn test
+cd frontend
+COVERAGE=true yarn test
+rc=$?
 cd ..
 
 kill -INT $(cat tmp/pids/server.pid)
+
+exit $rc
