d1f2fe9c43f9b85b417a29dd06b68b690e26d08a
Damian Senn <senn@puzzle.ch>
2017-09-25 08:54:57 +0200

    Don't serialize unpermitted person attributes fixes #23971

---
+++ frontend/app/serializers/person.js
@@ -0,0 +1,9 @@
+import ApplicationSerializer from './application';
+
+export default ApplicationSerializer.extend({
+  attrs: {
+    picturePath: { serialize: false },
+    updatedAt: { serialize: false },
+    originPersonId: { serialize: false },
+  },
+});
---
+++ frontend/tests/unit/serializers/person-test.js
@@ -0,0 +1,23 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('person', 'Unit | Serializer | person', {
+  needs: [
+    'model:education',
+    'model:advanced-training',
+    'model:activity',
+    'model:project',
+    'model:expertise-topic-skill-value',
+    'serializer:person',
+  ]
+});
+
+test('it does not serialize unpermitted attrs', function(assert) {
+  let record = this.subject();
+
+  let { data: { attributes: attrs } } = record.serialize();
+
+  assert.notOk('picture_path' in attrs);
+  assert.notOk('updated_at' in attrs);
+  assert.notOk('origin_person_id' in attrs);
+  assert.ok('name' in attrs);
+});
