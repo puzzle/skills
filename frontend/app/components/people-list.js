fc8a9400e52176c8e7ffb2b16d1e6f3a88ed0aa2
Damian Senn <senn@puzzle.ch>
2017-09-25 11:06:26 +0200

    Group people list by alphabet

--- frontend/app/components/people-list.js
+++ frontend/app/components/people-list.js
@@ -5,15 +5,37 @@ import PersonModel from '../models/person';
 import { task, timeout } from 'ember-concurrency';
 
 const SEARCH_TIMEOUT = 250;
+const GROUP_THRESHOLD = 2 * 42;
 
 export default Component.extend({
   filterBy: 'all',
+  search: null,
+
+  showGrouped: computed('filteredList.[]', function() {
+    return this.get('filteredList.length') > GROUP_THRESHOLD;
+  }),
 
   filteredList: computed('people.[]', 'filterBy', function() {
-    if (this.filterBy === 'all') {
-      return this.people;
+    if (this.get('filterBy') === 'all') {
+      return this.get('people');
     }
-    return this.people.filterBy('attributes.status_id', parseInt(this.filterBy));
+    return this.get('people').filterBy(
+      'attributes.status_id',
+      parseInt(this.get('filterBy'), 10),
+    );
+  }),
+
+  groupedList: computed('filteredList.[]', function() {
+    return this.get('filteredList').toArray().reduce((grouped, person) => {
+      let letter = person.attributes.name[0];
+
+      if (grouped[letter] === undefined) {
+        grouped[letter] = [];
+      }
+
+      grouped[letter].push(person);
+      return grouped;
+    }, {});
   }),
 
   statusData: computed(function() {
