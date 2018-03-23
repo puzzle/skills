674516f55d750b17814792654c156fd6f92bedc0
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-03-23 22:10:19 +0100

    fix rubocop detections

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,8 +44,9 @@ class CompaniesController < ApplicationController
       @company = Company.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    # Only allow a trusted parameter "white list" through
+
     def company_params
-      params.require(:company).permit(:name, :location, :web, :email, :phone, :partnermanager, :contact_person, :email_contact_person, :phone_contact_person, :crm, :level, :number_MA_total, :number_MA_dev, :number_MA_sys_mid, :number_MA_PL, :number_MA_UX)
+      params.require(:company).permit(:name)
     end
 end
--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,6 @@
 class CompanySerializer < ApplicationSerializer
-  attributes :id, :name, :location, :web, :email, :phone, :partnermanager, :contact_person, :email_contact_person, :phone_contact_person, :crm, :level, :number_MA_total, :number_MA_dev, :number_MA_sys_mid, :number_MA_PL, :number_MA_UX
+
+  attributes :id, :name
+
 end
+
