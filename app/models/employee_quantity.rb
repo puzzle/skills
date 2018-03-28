c1c608441afef79bb388749c4ce3afe85005ace9
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-03-28 14:30:12 +0200

    Add company, location, employee_quantity model tests

--- app/models/employee_quantity.rb
+++ app/models/employee_quantity.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: employee_quantities
+#
+#  id                    :integer          not null, primary key
+#  category              :string
+#  quantity              :integer
+#  company_id            :integer
+
 class EmployeeQuantity < ApplicationRecord
   belongs_to :company
+  
+  validates :category, :quantity, presence: true
+  validates :category, length: { maximum: 100 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
