01a9bb9fd8244a92643bb18e5c519b233b96448b
Robin481 <robin95@bluewin.ch>
2018-04-02 16:12:02 +0200

    Added basic Company-List functionality

--- frontend/app/models/employee-quantity.js
+++ frontend/app/models/employee-quantity.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   category: DS.attr('string'),
-  amount: DS.attr('number'),
+  quantity: DS.attr('number'),
   company: DS.belongsTo('company')
 });
