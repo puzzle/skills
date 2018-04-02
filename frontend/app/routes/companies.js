9596c3aa42efe1591ae741d762cf3cba312db91a
Robin481 <robin95@bluewin.ch>
2018-04-02 15:58:22 +0200

    Part of Company-List before merge

--- frontend/app/routes/companies.js
+++ frontend/app/routes/companies.js
@@ -2,7 +2,6 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
 export default Route.extend(AuthenticatedRouteMixin, {
   ajax: service(),
 
