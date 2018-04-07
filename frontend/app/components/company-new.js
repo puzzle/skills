c80b7915a74b7b0785c1ace5729729a2af031bed
Robin481 <robin95@bluewin.ch>
2018-04-07 14:07:21 +0200

    Saving over an icon when creating a company and then redirecting to /companies

--- frontend/app/components/company-new.js
+++ frontend/app/components/company-new.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   store: service(),
   i18n: service(),
+  router: service(),
 
 
   actions: {
@@ -11,9 +12,11 @@ export default Component.extend({
       return newCompany.save()
         .then(() => this.sendAction('submit', newCompany))
         .then(() => this.get('notify').success('Firma wurde erstellt'))
+        .then(() => this.get('router').transitionTo('companies'))
         .then(function() {
           newLocation.save(); //TODO: catch error
-        });
+        })
+        
     },
 
     // TODO: use later?
