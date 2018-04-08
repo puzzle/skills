4632e089c7c05f3b438adac522e623e44e675975
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-08 12:35:32 +0200

    company-list lässt sich sortieren

--- frontend/app/components/companies-list.js
+++ frontend/app/components/companies-list.js
@@ -1,4 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  sortProperties: ["attributes.level:asc", "attributes.name:asc"],
+  sortedCompanies: Ember.computed.sort("companies", "sortProperties")
 });
