0496d1ccf1f3f1bb9df65e798e3270d532cb532f
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-08 12:56:16 +0200

    css styling für company.new standorte und mitarbeiterkategorien

--- frontend/app/components/companies-list.js
+++ frontend/app/components/companies-list.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
-  sortProperties: ["attributes.level:asc", "attributes.name:asc"],
+  sortProperties: ['attributes.level:asc', 'attributes.name:asc'],
   sortedCompanies: Ember.computed.sort("companies", "sortProperties")
 });
