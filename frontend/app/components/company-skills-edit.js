83e68d581701ad6d7201e42509c7599cc61f84f9
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-13 12:11:22 +0200

    changed from ember-cli-selectize to ember power select, working dummy-functionality

--- frontend/app/components/company-skills-edit.js
+++ frontend/app/components/company-skills-edit.js
@@ -1,21 +1,22 @@
 import Component from '@ember/component';
+import { A } from '@ember/array';
+import { isBlank } from '@ember/utils';
 
 export default Component.extend({
-  streets: [
-    {
-      id: 1,
-      category: 'Nature',
-      name: 'Bla Bla'
-    },
-    {
-      id: 2,
-      category: 'Nature',
-      name: 'Blub Blub'
-    },
-    {
-      id: 3,
-      category: 'Another category',
-      name: 'NANANNAN'
+  options: A(['Barcelona', 'London', 'New York', 'Porto']),
+  selected: [],
+
+  actions: {
+    createOnEnter(select, e) {
+      if (e.keyCode === 13 && select.isOpen &&
+        !select.highlighted && !isBlank(select.searchText)) {
+
+        let selected = this.get('selected');
+        if (!selected.includes(select.searchText)) {
+          this.get('options').pushObject(select.searchText);
+          select.actions.choose(select.searchText);
+        }
+      }
     }
-  ]
+  }
 });
