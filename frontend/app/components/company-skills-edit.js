2da6892a3cc7a9fd480665aa7a4b25cb44e26980
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-13 11:17:38 +0200

    list of options for selectize

--- frontend/app/components/company-skills-edit.js
+++ frontend/app/components/company-skills-edit.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  streets: [
+    {
+      id: 1,
+      category: 'Nature',
+      name: 'Bla Bla'
+    },
+    {
+      id: 2,
+      category: 'Nature',
+      name: 'Blub Blub'
+    },
+    {
+      id: 3,
+      category: 'Another category',
+      name: 'NANANNAN'
+    }
+  ]
 });
