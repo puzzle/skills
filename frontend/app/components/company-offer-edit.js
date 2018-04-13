bb483a096385809b2437969224fe1b77f499c443
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-13 14:55:13 +0200

    values from select field are now in url

--- frontend/app/components/company-offer-edit.js
+++ frontend/app/components/company-offer-edit.js
@@ -1,12 +1,24 @@
 import Component from '@ember/component';
 import { A } from '@ember/array';
 import { isBlank } from '@ember/utils';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+  i18n: service(),
   options: A(['Barcelona', 'London', 'New York', 'Porto']),
   selected: [],
 
   actions: {
+    submit(newOffer) {
+      return newOffer.save()
+        .then(function() {
+        })
+        .then(() => this.sendAction('submit', newOffer))
+    .then(() => this.get('notify').success('Angebot wurde erstellt'))
+    },
+
     createOnEnter(select, e) {
       if (e.keyCode === 13 && select.isOpen &&
         !select.highlighted && !isBlank(select.searchText)) {
--- frontend/app/models/offer.js
+++ frontend/app/models/offer.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   category: DS.attr('string'),
-  skills: DS.attr('array'),
+  offer: DS.attr('array'),
   company: DS.belongsTo('company')
 });
 
