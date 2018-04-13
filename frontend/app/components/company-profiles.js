92a3623fe8c833ba60acece15ca7338476556056
Robin481 <robin95@bluewin.ch>
2018-04-13 14:39:00 +0200

    Fixed that all the duplicates show up

--- frontend/app/components/company-profiles.js
+++ frontend/app/components/company-profiles.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+  originalPeople: computed(function() {
+    let people = this.get('company').get('people');
+    return people.filterBy('originPersonId', null);
+  })
 });
