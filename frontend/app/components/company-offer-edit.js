3a25305552625177ab8dacb205e569f443d99b6d
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-04-20 15:15:26 +0200

    functioning update, some style fixes

--- frontend/app/components/company-offer-edit.js
+++ frontend/app/components/company-offer-edit.js
@@ -7,7 +7,7 @@ import $ from 'jquery';
 export default Component.extend({
   store: service(),
   i18n: service(),
-  options: A(['Barcelona', 'London', 'New York', 'Porto']),
+  options: A(['Scrum', 'CI/CD', 'Hermes', 'BA', 'Java EE', 'Java SE', 'PHP', 'Docker', 'OpenShift', 'Ember', 'Angular', 'Git']),
   selected: A([]),
 
   actions: {
