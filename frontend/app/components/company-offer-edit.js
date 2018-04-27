589ba66c5b795ee9120f06d0f77a4ba588138046
Simon Sidler <simon_sidler92@hotmail.com>
2018-04-27 13:16:33 +0200

    Add button and funcitonality to submit all changes at once

--- frontend/app/components/company-offer-edit.js
+++ frontend/app/components/company-offer-edit.js
@@ -3,6 +3,7 @@ import { A } from '@ember/array';
 import { isBlank } from '@ember/utils';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import { Promise } from 'rsvp'
 import $ from 'jquery';
 
 export default Component.extend({
@@ -42,13 +43,31 @@ export default Component.extend({
         });
     },
 
+    submitAll(company) {
+      let changedOffers = company.get('offers').filterBy('hasDirtyAttributes', true);
+      let newOffer = this.get('newOffer');
+      newOffer.set('company', company);
+
+      if (newOffer.get('category') != "" && newOffer.get('category') != null) {
+        changedOffers.pushObject(newOffer);
+      }
+
+
+      return company.save()
+        .then(() =>
+          Promise.all([
+            changedOffers.map(offer => offer.save())
+          ])
+        );
+    },
+
     deleteOffer(offerToDelete) {
       //remove overlay from delete confirmation
       $('.modal-backdrop').remove();
       offerToDelete.destroyRecord();
     },
 
-    createOnEnter(select, e) {
+    createOnEnter(select, e, offer) {
       if (e.keyCode === 13 && select.isOpen &&
         !select.highlighted && !isBlank(select.searchText)) {
 
