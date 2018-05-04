a562e116020675cd3ca603f81eb86b018a983662
Robin481 <robin95@bluewin.ch>
2018-05-04 15:00:09 +0200

    Power select now doesnt force you to pick a predetermined value if e.g. you type java but javaee is already in the list

--- frontend/app/components/company-offer-edit.js
+++ frontend/app/components/company-offer-edit.js
@@ -17,25 +17,6 @@ export default Component.extend({
   },
 
   actions: {
-    /*submit(changeset, company) {
-      changeset.set('company', company);
-      return changeset.save()
-        .then(() => this.sendAction('submit'))
-        .then(() => this.get('notify').success('Angebot wurde aktualisiert!'))
-        .catch(() => {
-          let offer = this.get('offer');
-          this.get('notify').alert(offer);
-
-          let errors = offer.get('errors').slice(); // clone array as rollbackAttributes mutates
-
-          offer.rollbackAttributes();
-          errors.forEach(({ attribute, message }) => {
-            let translated_attribute = this.get('i18n').t(`offer.${attribute}`)['string']
-            changeset.pushErrors(attribute, message);
-            this.get('notify').alert(`${translated_attribute} ${message}`, { closeAfter: 10000 });
-          });
-        });
-    },*/
 
     submit(company) {
       company.save()
@@ -75,15 +56,22 @@ export default Component.extend({
     },
 
     createOnEnter(select, e, offer) {
-      if (e.keyCode === 13 && select.isOpen &&
-        !select.highlighted && !isBlank(select.searchText)) {
-
+      if (e.keyCode === 13) {
         let selected = this.get('selected');
+        console.log(selected);
         if (!selected.includes(select.searchText)) {
           this.get('options').pushObject(select.searchText);
+          console.log(select.searchText);
           select.actions.choose(select.searchText);
         }
       }
+    },
+
+    createOffer(selected, searchText)
+    {
+      console.log(searchText);
+      this.get('options').pushObject(searchText);
+      selected.pushObject(searchText);
     }
   }
 });
