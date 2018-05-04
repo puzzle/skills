71d8955a6f62cca6ac2f239854af124676945f25
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-05-04 12:05:33 +0200

    full functioning people search

--- frontend/app/components/people-search.js
+++ frontend/app/components/people-search.js
@@ -1,7 +1,18 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  
+  store: service(),
+  peopleToSelect: computed(function() {
+    return this.get('store').findAll('person');
+  }),
+  selected: '',
+  router: service(),
+  actions: {
+    changePerson(person) {
+      person.reload();
+      this.get('router').transitionTo('person' , person);
+    },
+  }
 });
