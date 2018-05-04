2c64e44f9940b9ba8212b850c521ef6e8e04e29e
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-05-04 09:55:02 +0200

    Update tests

--- frontend/app/components/person-new.js
+++ frontend/app/components/person-new.js
@@ -2,13 +2,14 @@ import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import PersonModel from '../models/person';
+import { inject } from '@ember/service';
 
 export default Component.extend({
   i18n: service(),
 
-  store: Ember.inject.service(),
+  store: inject.service(),
 
-  companiesToSelect: computed(function(){
+  companiesToSelect: computed(function() {
     return this.get('store').findAll('company');
   }),
 
