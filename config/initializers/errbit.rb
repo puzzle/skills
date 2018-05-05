b321f3701ed65e5b6a1617a7050faf011ff320fe
Pascal Simon <simon@puzzle.ch>
2018-05-05 08:29:35 +0200

    airbrake set project id

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.host = 'https://errbit.puzzle.ch'
-  config.project_id = ENV['ERRBIT_PROJECT_ID']
+  config.project_id = '42' # needs to be set to anything when using with errbit
   config.project_key = ENV['ERRBIT_PROJECT_KEY']
 
   config.environment = Rails.env
