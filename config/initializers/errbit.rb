e155214d05680b4a13e09983f4fcfa44c3b5cfe2
Pascal Simon <simon@puzzle.ch>
2018-05-07 07:42:10 +0200

    make airbrake configurable by env vars

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
-  config.host = 'https://errbit.puzzle.ch'
+  config.host = ENV['RAILS_AIRBRAKE_HOST']
   config.project_id = '42' # needs to be set to anything when using with errbit
-  config.project_key = ENV['ERRBIT_PROJECT_KEY']
+  config.project_key = ENV['RAILS_AIRBRAKE_API_KEY']
 
   config.environment = Rails.env
   config.ignore_environments = %w(development test)
