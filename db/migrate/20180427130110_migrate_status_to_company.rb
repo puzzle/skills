3dfe7be535688e0da266583a9157964b8ea39017
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-05-08 21:45:51 +0200

    Update StatusToCompanyMigration

--- db/migrate/20180427130110_migrate_status_to_company.rb
+++ db/migrate/20180427130110_migrate_status_to_company.rb
@@ -1,19 +1,33 @@
 class MigrateStatusToCompany < ActiveRecord::Migration[5.1]
-  def change
+def up
+    #STATUSES = { 1 => 'Mitarbeiter',
+    #             2 => 'Ex Mitarbeiter',
+    #             3 => 'Bewerber',
+    #             4 => 'Partner' }.freeze
+    
+    employees = Company.create!(name: 'Firma')
+    ex_employees = Company.create!(name: 'Ex-Mitarbeiter')
+    candidates = Company.create!(name: 'Bewerber')
+    partner = Company.create!(name: 'Partner')
+ 
+
     Person.find_each do |person|
       case person.status_id
       when 1
-        person.update!(company_id: 1)
+        person.update!(company: employees)
       when 2
-        person.update!(company_id: 2)
+        person.update!(company: ex_employees)
       when 3
-        person.update!(company_id: 3)
+        person.update!(company: candidates)
       when 4
-        person.update!(company_id: 4)
+        person.update!(company: partner)
       else
-        person.update!(company_id: nil)
+        person.update!(company: nil)
       end
     end
+  end
+
+  def down
     remove_column :people, :status_id
   end
 end
