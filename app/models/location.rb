2088d6810c73aab391fd6580c21006833660d2f2
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-05-16 11:09:49 +0200

    Add AssociationsUpdatetAt

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,8 +7,21 @@
 #  company_id            :integer
 
 class Location < ApplicationRecord
+  
+  after_create :update_associations_updatet_at
+  after_update :update_associations_updatet_at
+  after_destroy :update_associations_updatet_at
+
   belongs_to :company, touch: true
 
   validates :location, presence: true
   validates :location, length: { maximum: 100 }
+
+  private
+
+  def update_associations_updatet_at
+  	timestamp = DateTime.now
+    self.company.update!(associations_updatet_at: timestamp)
+  end
+
 end
