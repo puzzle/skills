57f789bed6b31d1751a815482631a2d8d8e7b0e9
Robin481 <robin95@bluewin.ch>
2018-05-18 17:04:13 +0200

    Added more predetermined offers

--- frontend/app/components/company-offer-edit.js
+++ frontend/app/components/company-offer-edit.js
@@ -7,9 +7,14 @@ import { isBlank } from '@ember/utils';
 export default Component.extend({
   store: service(),
   i18n: service(),
-  options: A(['Scrum', 'CI/CD', 'Hermes', 'BA', 'Java EE', 'Java SE', 'PHP',
-    'Docker', 'OpenShift', 'Ember', 'Angular', 'Git']),
-  selected: A(['bla']),
+  options: (['Advanced Routing', 'Angular'
+            , 'BIND', 'C#', 'C', 'C++', 'CSS', 'CentOS', 'DHCP', 'Debian', 'DelayedJob Sidekiq', 'Docker'
+            , 'EJB CDI', 'Fedora', 'GIT', 'HTML', 'Hybrid Mobile Apps', 'IPv6', 'JMS', 'JPA Hibernate'
+            , 'JQuery', 'JUnit', 'Java SE', 'Java EE', 'Java', 'JavaScript/ECMAScript', 'Javascript', 'Jenkins', 'Linux'
+            , 'Message Queues', 'Minitest', 'Mocha', 'Mockito', 'Network Appliances', 'NoSql', 'Openshift'
+            , 'Passenger', 'Perl', 'Puma', 'Python', 'Qunit', 'R', 'REST', 'Red Hat', 'Relationale DBs', 'Resque'
+            , 'Rspec', 'Ruby on Rails', 'Ruby', 'SASS', 'SQL', 'SUSE', 'Servlets', 'Shell Scripting', 'Sonar'
+            , 'Stored Procedures', 'Travis', 'UML', 'Ubuntu', 'VLANs', 'WebSockets', 'WildFly / JBoss EAP']),
 
   init() {
     this._super(...arguments);
