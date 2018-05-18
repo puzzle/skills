22b99d50ac9c20c739d91447703c23a3c572856d
Michael Rötheli <michael.roetheli@students.unibe.ch>
2018-05-18 17:36:50 +0200

    Fix/Add backend tests

--- spec/models/person_competence_spec.rb
+++ spec/models/person_competence_spec.rb
@@ -1,5 +1,31 @@
+# == Schema Information
+#
+# Table name: person_competence
+#
+#  id                    :integer          not null, primary key
+#  category              :string
+#  offer                 :array
+#  person_id            :integer
+
 require 'rails_helper'
 
-RSpec.describe PersonCompetence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe PersonCompetence do
+
+    context 'validations' do
+      it 'checks whether required attribute values are present' do
+        person_competence = PersonCompetence.new
+        person_competence.valid?
+
+        expect(person_competence.errors[:category].first).to eq('muss ausgefüllt werden')
+      end
+
+      it 'should not be more than 100 characters' do
+        person_competence = PersonCompetence.new
+        person_competence.category = SecureRandom.hex(150)
+        person_competence.valid?
+
+        expect(person_competence.errors[:category].first).to eq('ist zu lang (mehr als 100 Zeichen)')
+      end
+    end
 end
--- spec/models/project_technology_spec.rb
+++ spec/models/project_technology_spec.rb
@@ -1,5 +1,13 @@
+# == Schema Information
+#
+# Table name: project_technology
+#
+#  id                    :integer          not null, primary key
+#  offer                 :array
+#  project_id            :integer
+
 require 'rails_helper'
 
-RSpec.describe ProjectTechnology, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe ProjectTechnology do
 end
