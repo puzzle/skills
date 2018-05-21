11b2da9842f8ea7a4ed6e45ddcd91ebbbf13fb6a
Samuel Schwegler <samuel.schwegler@gmx.ch>
2018-05-21 21:32:25 +0200

    company persons allways showing

--- frontend/app/components/company-profiles.js
+++ frontend/app/components/company-profiles.js
@@ -5,6 +5,6 @@ export default Component.extend({
 
   originalPeople: computed(function() {
     let people = this.get('company').get('people');
-    return people.filterBy('originPersonId', null);
+    return people;
   })
 });
