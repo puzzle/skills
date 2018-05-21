ae869ce4a5b13e31765712a6d1bfe1abdd9f695e
Simon Sidler <simon_sidler92@hotmail.com>
2018-05-21 20:08:26 +0200

    company-offer-test now tests something

--- frontend/tests/integration/components/company-offer-test.js
+++ frontend/tests/integration/components/company-offer-test.js
@@ -1,25 +1,28 @@
-import { moduleForComponent, skip } from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('company-offer', 'Integration | Component | company offer', {
   integration: true
 });
 
-skip('it renders', function(assert) {
-
+test('it renders', function(assert) {
+  this.set('company', {
+    offerComment: 'das ist ein kommentar',
+    offers: [
+      { category: 'frontend', offer: ['ember', 'angular'] },
+      { category: 'backend', offer: ['java', 'ruby'] }
+    ]
+  });
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{company-offer}}`);
+  this.render(hbs`{{company-offer company=company}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#company-offer}}
-      template block text
-    {{/company-offer}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$().text().indexOf('das ist ein kommentar') !== -1);
+  assert.ok(this.$().text().indexOf('frontend') !== -1);
+  assert.ok(this.$().text().indexOf('ember') !== -1);
+  assert.ok(this.$().text().indexOf('angular' !== -1));
+  assert.ok(this.$().text().indexOf('backend' !== -1));
+  assert.ok(this.$().text().indexOf('java' !== -1));
+  assert.ok(this.$().text().indexOf('ruby' !== -1));
 });
