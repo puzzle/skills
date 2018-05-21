822b428475b6edeea308c6f7bad7d4aae77b784d
Robin481 <robin95@bluewin.ch>
2018-05-21 13:18:06 +0200

    Non working stuff for simon

--- frontend/app/components/competences-edit.js
+++ frontend/app/components/competences-edit.js
@@ -3,6 +3,12 @@ import Component from '@ember/component';
 
 export default Component.extend({
   i18n: service(),
+  store: service(),
+
+
+  init() {
+    this._super(...arguments);
+  },
 
   actions: {
     submit(changeset) {
@@ -20,6 +26,29 @@ export default Component.extend({
             this.get('notify').alert(`${translated_attribute} ${message}`, { closeAfter: 10000 });
           });
         });
-    }
+    },
+
+    submitPerson(person){
+      person.save()
+        .then (() =>
+          Promise.all([
+            ...person
+              .get('person_competences')
+              .map(person_competence => person_competence.save())
+              .get('competence')
+              .map(competence => competence.save())
+          ])
+        )
+        .then (() => this.sendAction('submit'))
+        .then (() => this.get('notify').success('Successfully saved!'))
+    },
+
+    createNewOffer(person) {
+      let bla = this.get('person');
+      let competence = this.get('store').createRecord('person_competence', { bla });
+      competence.set('offer', []);
+      competence.set('category', "Test");
+      console.log("Created: " + competence);
+    },
   }
 });
