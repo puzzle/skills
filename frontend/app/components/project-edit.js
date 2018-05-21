70189bb8cc9665b50766013fe6c971c89cc65076
Simon Sidler <simon_sidler92@hotmail.com>
2018-05-21 15:18:46 +0200

    project edit now saves technologies. added options to init.

--- frontend/app/components/project-edit.js
+++ frontend/app/components/project-edit.js
@@ -6,6 +6,19 @@ export default Component.extend({
 
   i18n: service(),
 
+  init() {
+    this._super(...arguments);
+    this.options = ['Advanced Routing', 'Angular'
+      , 'BIND', 'C#', 'C', 'C++', 'CSS', 'CentOS', 'DHCP', 'Debian', 'DelayedJob Sidekiq', 'Docker'
+      , 'EJB CDI', 'Fedora', 'GIT', 'HTML', 'Hybrid Mobile Apps', 'IPv6', 'JMS', 'JPA Hibernate'
+      , 'JQuery', 'JUnit', 'Java SE', 'Java EE', 'Java', 'JavaScript/ECMAScript', 'Javascript', 'Jenkins', 'Linux'
+      , 'Message Queues', 'Minitest', 'Mocha', 'Mockito', 'Network Appliances', 'NoSql', 'Openshift'
+      , 'Passenger', 'Perl', 'Puma', 'Python', 'Qunit', 'R', 'REST', 'Red Hat', 'Relationale DBs', 'Resque'
+      , 'Rspec', 'Ruby on Rails', 'Ruby', 'SASS', 'SQL', 'SUSE', 'Servlets', 'Shell Scripting', 'Sonar'
+      , 'Stored Procedures', 'Travis', 'UML', 'Ubuntu', 'VLANs', 'WebSockets', 'WildFly / JBoss EAP'];
+
+  },
+
   suggestion(term) {
     return `"${term}" hinzufügen!`;
   },
@@ -22,6 +35,13 @@ export default Component.extend({
     submit(changeset, event) {
       event.preventDefault();
       return changeset.save()
+        .then(() =>
+          Promise.all([
+            ...changeset
+              .get('projectTechnologies')
+              .map(projectTechnology => projectTechnology.save())
+          ])
+        )
         .then(project => this.sendAction('done'))
         .then(() => this.get('notify').success('Projekt wurde aktualisiert!'))
         .catch(() => {
