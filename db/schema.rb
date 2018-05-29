ddcac3bcecbbd2495e76c5193e54414eaf5a1b2b
Pascal Simon <simon@puzzle.ch>
2018-05-29 09:25:48 +0200

    always use default values for boolean attrs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180518092531) do
+ActiveRecord::Schema.define(version: 20180529072129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20180518092531) do
     t.string "offer_comment"
     t.string "crm"
     t.string "level"
-    t.boolean "my_company"
+    t.boolean "my_company", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "associations_updatet_at"
