228abcb4c507f6dccc38e7d2257ff7a1b92cf28c
Robin Steiner <robin95@bluewin.ch>
2018-09-28 11:24:06 +0200

    RoR from 5.1.5 to 5.1.6

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '5.1.5'
+gem 'rails', '5.1.6'
 
 gem 'active_model_serializers', '~> 0.10.6'
 gem 'activerecord-postgresql-adapter'
--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
 require_relative 'boot'
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
-require 'rails/test_unit/railtie'
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module Puzzlecv2
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,7 +29,5 @@ module Puzzlecv2
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.autoload_paths += %W( #{config.root}/app/uploaders)
-    config.i18n.default_locale = :de
   end
 end
