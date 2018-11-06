321e97f57dfac67e521d40faede520f21c8867a4
Pascal Simon <simon@puzzle.ch>
2018-11-06 11:27:07 +0100

    use isBlank helper method

--- frontend/app/components/company-overview-edit.js
+++ frontend/app/components/company-overview-edit.js
@@ -1,6 +1,7 @@
-import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import $ from 'jquery';
+import { inject as service } from "@ember/service";
+import Component from "@ember/component";
+import $ from "jquery";
+import { isBlank } from "@ember/utils";
 
 export default Component.extend({
   store: service(),
@@ -12,93 +13,104 @@ export default Component.extend({
     records.forEach(record => {
       record[0].forEach(element => {
         record[1].forEach(attr => {
-          if (!element.get(attr) || !element.get(attr).length) {
-            let attribute = this.get('i18n').t(`company.${attr}`)['string']
-            errors.addObject(attribute + ' Feld ist leer, der Eintrag wurde nicht gespeichert')
-            if (!element.get('isDeleted')) element.destroyRecord();
+          if (isBlank(element.get(attr))) {
+            let attribute = this.get("i18n").t(`company.${attr}`)["string"];
+            errors.addObject(
+              attribute + " Feld ist leer, der Eintrag wurde nicht gespeichert"
+            );
+            if (!element.get("isDeleted")) element.destroyRecord();
           }
-        })
+        });
       });
     });
-    return errors
+    return errors;
   },
 
   actions: {
     submit(changeset) {
-      let locations = this.get('company.locations').toArray()
-      let employeeQuantities = this.get('company.employeeQuantities').toArray()
-      let errors = this.removeEmptyElements([locations, ['location']],[employeeQuantities, ['category','quantity']])
+      let locations = this.get("company.locations").toArray();
+      let employeeQuantities = this.get("company.employeeQuantities").toArray();
+      let errors = this.removeEmptyElements(
+        [locations, ["location"]],
+        [employeeQuantities, ["category", "quantity"]]
+      );
       errors.forEach(error => {
-        this.get('notify').alert(error, { closeAfter: 5000 });
+        this.get("notify").alert(error, { closeAfter: 5000 });
       });
 
-      changeset.save()
+      changeset
+        .save()
         // TODO: This triggers a transition to the company detail page
         //       but we might have errors saving related data further down
         //       the promise chain, shouldn't we transition after everything
         //       is successfully saved?
-        .then(() => this.sendAction('submit'))
-        .then(() => this.get('notify').success('Firmenprofil wurde aktualisiert!'))
+        .then(() => this.sendAction("submit"))
+        .then(() =>
+          this.get("notify").success("Firmenprofil wurde aktualisiert!")
+        )
         .then(() =>
           Promise.all([
             ...changeset
-              .get('locations')
-              .filterBy('hasDirtyAttributes')
+              .get("locations")
+              .filterBy("hasDirtyAttributes")
               .map(location => location.save()),
             ...changeset
-              .get('employeeQuantities')
-              .filterBy('hasDirtyAttributes')
+              .get("employeeQuantities")
+              .filterBy("hasDirtyAttributes")
               .map(employeeQuantity => employeeQuantity.save())
           ])
         )
         .catch(() => {
-          let company = this.get('company');
-          let errors = company.get('errors').slice(); // clone array as rollbackAttributes mutates
+          let company = this.get("company");
+          let errors = company.get("errors").slice(); // clone array as rollbackAttributes mutates
           company.rollbackAttributes();
           // TODO: We rolled back company attributes and show company field
           //       errors but the related data might still be in an
           //       invalid state. Maybe there is a better way to handle
           //       these validations/rollbacks in Ember these days?
           errors.forEach(({ attribute, message }) => {
-            let translated_attribute = this.get('i18n').t(`company.${attribute}`)['string']
+            let translated_attribute = this.get("i18n").t(
+              `company.${attribute}`
+            )["string"];
             changeset.pushErrors(attribute, message);
-            this.get('notify').alert(`${translated_attribute} ${message}`, { closeAfter: 10000 });
+            this.get("notify").alert(`${translated_attribute} ${message}`, {
+              closeAfter: 10000
+            });
           });
         });
-
     },
 
     abortEdit() {
-      let locations = this.get('company.locations').toArray();
-      let employeeQuantities = this.get('company.employeeQuantities').toArray();
+      let locations = this.get("company.locations").toArray();
+      let employeeQuantities = this.get("company.employeeQuantities").toArray();
 
       locations.forEach(location => {
-        if (location.get('isNew')) {
+        if (location.get("isNew")) {
           location.destroyRecord();
         }
       });
       employeeQuantities.forEach(quantity => {
-        if (quantity.get('isNew')) {
+        if (quantity.get("isNew")) {
           quantity.destroyRecord();
         }
       });
-      this.sendAction('companyEditing');
+      this.sendAction("companyEditing");
     },
 
     deleteCompany(companyToDelete) {
-      $('.modal-backdrop').remove();
+      $(".modal-backdrop").remove();
       companyToDelete
         .destroyRecord()
-        .then(() => this.get('router').transitionTo('companies'));
+        .then(() => this.get("router").transitionTo("companies"));
       // TODO: Handle error case, display simple notification?
     },
 
     addLocations(company) {
-      this.get('store').createRecord('location', { company });
+      this.get("store").createRecord("location", { company });
     },
 
     addEmployeeQuantity(company) {
-      this.get('store').createRecord('employee-quantity', { company });
+      this.get("store").createRecord("employee-quantity", { company });
     },
 
     deleteLocation(location) {
@@ -108,7 +120,5 @@ export default Component.extend({
     deleteEmployeeQuantity(quantity) {
       return quantity.destroyRecord();
     }
-
   }
-
 });
