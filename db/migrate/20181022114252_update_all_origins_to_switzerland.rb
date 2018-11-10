3eab5e8768505fead8318ff4dd7a5b95e7ae8671
Pascal Simon <simon@puzzle.ch>
2018-11-10 13:31:54 +0100

    remove bad migrations

--- db/migrate/20181022114252_update_all_origins_to_switzerland.rb
+++
@@ -1,8 +0,0 @@
-class UpdateAllOriginsToSwitzerland < ActiveRecord::Migration[5.2]
-  def change
-    Person.find_each do |person|
-      person.origin = 'Switzerland'
-      person.save!
-    end
-  end
-end
--- db/migrate/20181026153302_change_switzerland_to_schweiz.rb
+++
@@ -1,8 +0,0 @@
-class ChangeSwitzerlandToSchweiz < ActiveRecord::Migration[5.2]
-  def change
-    Person.find_each do |person|
-      person.origin = 'Schweiz'
-      person.save!
-    end
-  end
-end
