711ab1b63a1d37f4dd6326a7e41905685bbbea84
Nils Rauch <rauch@puzzle.ch>
2018-11-23 17:18:12 +0100

    added role model test

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,6 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :people, dependent: :restrict
+  validates :name, length: {maximum: 100}
 
   scope :list, -> { order(:name) }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Role do
+  context 'validations' do
+    it 'should not be more than 100 characters' do
+      role = Role.new
+      role.name = SecureRandom.hex(150)
+      role.valid?
+
+      expect(role.errors[:name].first).to eq('ist zu lang (mehr als 100 Zeichen)')
+    end
+  end
+
 end
