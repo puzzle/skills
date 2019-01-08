776907ae69b239f6a93b4d355be31c5b7c65419f
Nils Rauch <rauch@puzzle.ch>
2019-01-08 10:56:28 +0100

    added test for no month given

--- frontend/tests/unit/utils/sort-by-year-test.js
+++ frontend/tests/unit/utils/sort-by-year-test.js
@@ -51,6 +51,38 @@ test('it sorts by finish_at and start_at', function(assert) {
   ]);
 });
 
+test('it sorts by finish_at and start_at and considers 13th', function(assert) {
+  const Thing = EmberObject.extend({
+    sortedEducations: sortByYear('educations'),
+  });
+
+  const thing = Thing.create({
+    educations: [
+      { start_at: new Date(1990, 5, 13), finish_at: new Date(1994, 3, 13) },
+      { start_at: new Date(1993, 2, 1), finish_at: null },
+      { start_at: new Date(1989, 4, 1), finish_at: new Date(1989, 8, 1) },
+      { start_at: new Date(1990, 9, 1), finish_at: new Date(1995, 2, 1) },
+      { start_at: new Date(1990, 5, 1), finish_at: new Date(1994, 3, 1) },
+      { start_at: new Date(2000, 4, 1), finish_at: null },
+      { start_at: new Date(1990, 9, 1), finish_at: new Date(1995, 2, 13) },
+      { start_at: new Date(1993, 2, 13), finish_at: null },
+      { start_at: new Date(1989, 4, 13), finish_at: new Date(1989, 8, 1) },
+    ],
+  });
+
+  assert.deepEqual(thing.get('sortedEducations'), [
+    { start_at: new Date(2000, 4, 1), finish_at: null },
+    { start_at: new Date(1993, 2, 1), finish_at: null },
+    { start_at: new Date(1993, 2, 13), finish_at: null },
+    { start_at: new Date(1990, 9, 1), finish_at: new Date(1995, 2, 1) },
+    { start_at: new Date(1990, 9, 1), finish_at: new Date(1995, 2, 13) },
+    { start_at: new Date(1990, 5, 1), finish_at: new Date(1994, 3, 1) },
+    { start_at: new Date(1990, 5, 13), finish_at: new Date(1994, 3, 13) },
+    { start_at: new Date(1989, 4, 1), finish_at: new Date(1989, 8, 1) },
+    { start_at: new Date(1989, 4, 13), finish_at: new Date(1989, 8, 1) },
+  ]);
+});
+
 test('it recomputes on changes', function(assert) {
   const Thing = EmberObject.extend({
     sortedEducations: sortByYear('educations'),
