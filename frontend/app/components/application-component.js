3328b840dbc4da7232bc0188164a7dcce7f6624d
Robin Steiner <robin95@bluewin.ch>
2019-01-11 08:30:55 +0100

    Linting

--- frontend/app/components/application-component.js
+++ frontend/app/components/application-component.js
@@ -2,11 +2,11 @@ import Component from '@ember/component';
 
 export default Component.extend({
   actions: {
-      setWithTab(saveAction, select, e)
-      {
-        if (e.keyCode == 9) {
-          this.send(saveAction, select.highlighted)
-        }
+    setWithTab(saveAction, select, e)
+    {
+      if (e.keyCode == 9) {
+        this.send(saveAction, select.highlighted)
       }
     }
+  }
 })
