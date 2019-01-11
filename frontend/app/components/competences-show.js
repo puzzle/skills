a1fea4fae64eb80b948a430d7524ae2362ad0293
Robin Steiner <robin95@bluewin.ch>
2019-01-11 14:01:59 +0100

    Removed competenceCounter and styled personNew buttons

--- frontend/app/components/competences-show.js
+++ frontend/app/components/competences-show.js
@@ -9,14 +9,6 @@ export default Component.extend({
       .split('\n')
   }),
 
-  amountOfCompetences: computed('person.competences', function() {
-    const competences = this.get('person.competences');
-    if (competences == null) return 0;
-    const validCompetences =
-      this.removeEmptyStrings(competences.split(/\r\n|\r|\n/));
-    return validCompetences.length;
-  }),
-
   removeEmptyStrings(array) {
     for (let i = array.length; i--;) {
       if (array[i] == '') array.splice(i, 1);
