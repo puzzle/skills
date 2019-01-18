96e6769f6f6dda147241add0cf45a58b191cf5e9
Nils Rauch <rauch@puzzle.ch>
2019-01-18 15:20:41 +0100

    added sorting to language skills

---
+++ frontend/app/utils/sort-by-language.js
@@ -0,0 +1,29 @@
+import { computed, get } from '@ember/object';
+
+export default function sortByLanguage(property) {
+  return computed(`${property}.@each.{language}`, function() {
+    let collection = get(this, property);
+
+    const obligatoryLanguages = ["DE", "EN", "FR"]
+    if (!collection) return [];
+
+    let newCollection = collection.toArray().sort((a, b) => {
+      const aLanguage = get(a, "language")
+      const bLanguage = get(b, "language")
+      if (aLanguage == "-") return 1;
+      if (bLanguage == "-") return -1;
+      const aIsObligatory = obligatoryLanguages.includes(aLanguage);
+      const bIsObligatory = obligatoryLanguages.includes(bLanguage);
+
+      if (aIsObligatory && !bIsObligatory) return -1;
+      if (!aIsObligatory && bIsObligatory) return 1;
+      if (aIsObligatory && bIsObligatory) {
+        return obligatoryLanguages.indexOf(aLanguage) - obligatoryLanguages.indexOf(bLanguage)
+      }
+      if (aLanguage < bLanguage) return -1;
+      if (aLanguage > bLanguage) return 1;
+      return 0;
+    });
+    return newCollection;
+  });
+}
