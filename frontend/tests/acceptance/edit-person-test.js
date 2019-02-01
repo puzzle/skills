fdb3f53b4d50557e635b44a875e66a85fede4e0e
Robin Steiner <32168247+Robin481@users.noreply.github.com>
2019-02-01 15:07:11 +0100

    Pikaday implementation (#124)
    * Changed datepicker to ember-pikaday
    
    * Removed Flatpickr, added pikaday 2.2.6
    
    * Refactored test pages and edited tests
    
    * Added pikaday tests to person-edit and linting
    
    * Removed unnecessary code
    
    * cleanup and comments

--- frontend/tests/acceptance/edit-person-test.js
+++ frontend/tests/acceptance/edit-person-test.js
@@ -1,10 +1,11 @@
 import { test, skip } from 'qunit';
 import moduleForAcceptance from 'frontend/tests/helpers/module-for-acceptance';
 import { authenticateSession } from 'frontend/tests/helpers/ember-simple-auth';
-import { setFlatpickrDate } from 'ember-flatpickr/test-support/helpers';
 import applicationPage from 'frontend/tests/pages/application';
 import page from 'frontend/tests/pages/person-edit';
 import { triggerKeyUp } from 'ember-keyboard';
+import { openDatepicker } from 'ember-pikaday/helpers/pikaday';
+import $ from 'jquery';
 
 moduleForAcceptance('Acceptance | edit person', {
   beforeEach() {
@@ -16,40 +17,74 @@ moduleForAcceptance('Acceptance | edit person', {
 });
 
 test('/people/:id edit person data', async function(assert) {
-  assert.expect(7);
+  assert.expect(11);
 
+  // Go to the start page and select a user from the dropdown
   await applicationPage.visitHome('/');
   await selectChoose('#people-search', '.ember-power-select-option', 0);
 
+  // Go into person-edit
   await page.toggleEditForm();
 
+  // Selection for all the power selects
   /* eslint "no-undef": "off" */
   await selectChoose('#role', '.ember-power-select-option', 0)
   await selectChoose('#company', 'Firma');
   await selectChoose('#nationality', "Samoa");
   await selectChoose('#maritalStatus', 'verheiratet');
 
-  setFlatpickrDate('.flatpickr-input', '26.10.2018')
+  // interactor is the interactable object for the pikaday-datepicker
+  let interactor = openDatepicker($('.birthdate_pikaday > input'));
 
+  // Cant be more/less than +/- 10 Years from today
+  interactor.selectDate(new Date(2019,1,19))
+
+  // Testing if pikaday got the right dates
+  assert.equal(interactor.selectedDay(), 19)
+  assert.equal(interactor.selectedMonth(), 1)
+  assert.equal(interactor.selectedYear(), 2019)
+
+  // Fill out the persons text fields
   await page.editForm.name('Hansjoggeli');
   await page.editForm.title('Dr.');
   await page.editForm.location('Chehrplatz Schwandi');
+
+  // Submit the edited content
   await page.editForm.submit();
 
+  // Assert that all we changed is present
   assert.equal(page.profileData.name, 'Hansjoggeli');
   assert.equal(page.profileData.title, 'Dr.');
   assert.equal(page.profileData.role, 'System-Engineer');
   assert.equal(page.profileData.nationalities, 'Samoa');
   assert.equal(page.profileData.location, 'Chehrplatz Schwandi');
+  assert.equal(page.profileData.birthdate, '19.02.2019')
 
+  // Toggle Edit again
   await page.toggleEditForm();
+
+  // Enable two Nationalities
   await page.toggleNationalities();
+
+  // Select the second nationality
   await selectChoose('#nationality2', "Schweiz");
+
+  // Submit it
   await page.editForm.submit();
+
+  // Assert that it is there
   assert.equal(page.profileData.nationalities, 'Samoa , Schweiz')
+
+  // go into edit again
   await page.toggleEditForm();
+
+  // Toggle the second nationality option off
   await page.toggleNationalities();
+
+  // Submit it
   await page.editForm.submit();
+
+  // Check that the second nationality is gone
   assert.equal(page.profileData.nationalities, 'Samoa')
 });
 
--- frontend/tests/pages/people-new.js
+++ frontend/tests/pages/people-new.js
@@ -3,22 +3,37 @@ import {
   create,
   visitable,
   fillable,
-  clickable
+  clickable,
+  text
 } from 'ember-cli-page-object';
 
 const { resolve } = RSVP;
 
 export default create({
-  visit: visitable('/people/new'),
-  submit: clickable('#profil #submit-button'),
-  name: fillable('[name="person[name]"]'),
-  title: fillable('[name="person[title]"]'),
-  location: fillable('[name="person[location]"]'),
+  newPersonPage: {
+    visit: visitable('/people/new'),
+    submit: clickable('#submit-button'),
 
-  async createPerson(person) {
-    await Object.keys(person)
-      .reduce((p, key) => p.then(() => this[key](person[key])), resolve());
+    name: fillable('[name="person[name]"]'),
+    title: fillable('[name="person[title]"]'),
+    location: fillable('[name="person[location]"]'),
 
-    return this.submit();
-  }
+    async createPerson(person) {
+      await Object.keys(person)
+        .reduce((p, key) => p.then(() => this[key](person[key])), resolve());
+
+      return this.submit();
+    },
+  },
+
+  profileData: {
+    name: text('#data-test-person-name'),
+    title: text('#data-test-person-title'),
+    role: text('#data-test-person-role'),
+    birthdate: text('#data-test-person-birthdate'),
+    nationalities: text('#data-test-person-nationalities'),
+    location: text('#data-test-person-location'),
+    language: text('[data-test-person-language]', { multiple: true }),
+    maritalStatus: text('#data-test-person-marital-status'),
+  },
 });
