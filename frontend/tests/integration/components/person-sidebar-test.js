45af892234cfa9be8dc247d6e012043ef180d59b
Nils Rauch <rauch@puzzle.ch>
2019-02-04 10:56:55 +0100

    added person sidebar

---
+++ frontend/tests/integration/components/person-sidebar-test.js
@@ -0,0 +1,17 @@
+import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('person-sidebar', 'Integration | Component | person sidebar', {
+  integration: true
+});
+
+test('it renders sidebar', function(assert) {
+  this.render(hbs`{{person-sidebar}}`);
+
+  assert.ok(this.$().text().includes('Personalien'))
+  assert.ok(this.$().text().includes('Kernkompetenzen'))
+  assert.ok(this.$().text().includes('Ausbildung'))
+  assert.ok(this.$().text().includes('Weiterbildung'))
+  assert.ok(this.$().text().includes('Stationen'))
+  assert.ok(this.$().text().includes('Projekte'))
+});
