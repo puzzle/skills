3a06a9db549177c1e38ada4a5b01cb9fc584e9eb
Nils Rauch <rauch@puzzle.ch>
2019-02-05 13:28:40 +0100

    refactored authorizer

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -5,8 +5,18 @@ import { pluralize } from 'ember-inflector'
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api',
-  authorizer: 'authorizer:auth',
+  //authorizer: 'authorizer:auth',
   //coalesceFindRequests: true,
+
+  authorize(xhr) {
+    let api_token = this.get('session.data.authenticated.token');
+    let ldap_uid = this.get('session.data.authenticated.ldap_uid');
+
+    if (api_token && ldap_uid) {
+      xhr.setRequestHeader('api-token', api_token);
+      xhr.setRequestHeader('ldap-uid', ldap_uid);
+    }
+  },
 
   pathForType(type) {
     return pluralize(underscore(type));
