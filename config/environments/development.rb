b0b715684915a5dd43e07feeb1c960eb4c4e0936
Miro Zurbuchen <zurbuchen@puzzle.ch>
2019-03-15 15:34:30 +0100

    Update some gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  # Specifying secret key in development mode
+  config.secret_key_base = SecureRandom.hex(64)
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
