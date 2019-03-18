53dfc9b88664572757c0c37366c02b4798f8e68b
Miro <zurbuchen@puzzle.ch>
2019-03-18 07:55:07 +0100

    Update some gems (#175)

--- app/controllers/static_assets_controller.rb
+++ app/controllers/static_assets_controller.rb
@@ -3,7 +3,7 @@ class StaticAssetsController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render file: Rails.root.join('public', 'index.html')
+    render file: Rails.root.join('public', 'index.html'), formats: [:html]
   end
 
 end
