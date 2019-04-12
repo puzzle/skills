4d070e89bd6aab8d887274f708db9ead6f1c2715
Nils Rauch <rauch@puzzle.ch>
2019-04-12 13:53:01 +0200

    Add scrollToTop Button, refs: #48685

---
+++ frontend/app/components/scroll-to-top-button.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+
+  actions: {
+    scrollToTop() {
+      window.scroll({ top: 0, behavior: "smooth" });
+    }
+  }
+});
