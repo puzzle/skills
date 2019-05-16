d4dfcc73e64dd40f7033e5fb017fe362b42d77b1
Robin Steiner <robin95@bluewin.ch>
2019-05-16 12:43:38 +0200

    Fix eslint messages

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,39 +1,36 @@
-import { inject as service } from '@ember/service';
-import Route from '@ember/routing/route';
-import DS from 'ember-data';
-import { UnauthorizedError, ForbiddenError } from 'ember-ajax/errors';
+import { inject as service } from "@ember/service";
+import Route from "@ember/routing/route";
+import DS from "ember-data";
+import { UnauthorizedError, ForbiddenError } from "ember-ajax/errors";
 
 export default Route.extend({
-  session: service('keycloak-session'),
+  session: service("keycloak-session"),
   moment: service(),
   intl: service(),
 
   beforeModel() {
     this.get("moment").setLocale("de");
-    this.get("intl").setLocale(["de"]);
     this._super(...arguments);
 
-    var session = this.get('session');
+    let session = this.get("session");
 
     // Keycloak constructor arguments as described in the keycloak documentation.
-    var options = {
-      'url': 'server_url', //add your url here
-      'realm': 'realm_name', // add your realm here
-      'clientId': 'client_name', // add your clientId here
-      'credentials': {
+    let options = {
+      url: "server_url", //add your url here
+      realm: "realm_name", // add your realm here
+      clientId: "client_name", // add your clientId here
+      credentials: {
         secret: "1234" // add your secret here
       }
-    }
+    };
 
     // this will result in a newly constructed keycloak object
     session.installKeycloak(options);
 
     // set any keycloak init parameters where defaults need to be overidden
-    session.set('responseMode', 'fragment');
+    session.set("responseMode", "fragment");
     // finally init the service and return promise to pause router.
     return session.initKeycloak();
-
-
   },
 
   isAuthError(error) {
