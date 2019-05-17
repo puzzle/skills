f433deebbe8258f3f0c0704424de135a50f0551b
Robin Steiner <robin95@bluewin.ch>
2019-05-17 11:30:26 +0200

    Add admin check for skillset backend

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,9 +1,22 @@
 class SkillsController < CrudController
   include ExportController
 
+  before_action :authorize_admin
+
   self.permitted_attrs = %i[title radar portfolio default_set category_id]
 
   self.nested_models = %i[children parents]
+
+  def authorize_admin
+    #return if Rails.env.development? && ENV['ENABLE_AUTH'].blank?
+    return if ENV['DISABLE_AUTH'].present?
+
+    if has_admin_flag
+      return
+    end
+
+    render_unauthorized
+  end
 
   def index
     return export if params[:format]
@@ -21,6 +34,15 @@ class SkillsController < CrudController
   end
 
   private
+
+  def has_admin_flag
+    uri = request.env['REQUEST_URI']
+    headers = request.env
+    token = Keycloak.service.read_token(uri, headers)
+    decoded_token = Keycloak.service.decode_and_verify(token)
+    roles = decoded_token['resource_access']['pitc-skills-frontend']['roles']
+    roles.include? 'ADMIN'
+  end
 
   def fetch_entries
     entries = if params[:format]
