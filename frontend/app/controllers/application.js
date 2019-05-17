9ceb1f9fd7352ddce7077c2a47d0b6226d88d54d
Robin Steiner <robin95@bluewin.ch>
2019-05-17 08:31:10 +0200

    Fix redirect to user profile

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -3,27 +3,31 @@ import Controller from "@ember/controller";
 import ENV from "../config/environment";
 
 export default Controller.extend({
-  session: service('keycloak-session'),
+  session: service("keycloak-session"),
   router: service(),
   helplink: ENV.helplink,
 
+  init() {
+    let sessionInfo = this.get("session.tokenParsed");
+    this.username = sessionInfo.given_name + " " + sessionInfo.family_name;
+  },
+
   actions: {
     transitionToProfile() {
-      let username = this.get("session.data.authenticated.full_name");
       let people = this.get("store").findAll("person");
       people.then(() => {
-        let person = people.filterBy("name", username)[0];
+        let person = people.filterBy("name", this.get("username"))[0];
         if (person == undefined) {
           this.get("router").transitionTo("people");
         } else {
           this.get("router").transitionTo("person", person.id);
         }
-      })
+      });
     },
 
     logoutUser() {
-      let session = this.get('session');
-      session.logout()
+      let session = this.get("session");
+      session.logout();
     }
   }
 });
