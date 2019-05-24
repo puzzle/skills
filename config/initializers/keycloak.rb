656bcc2884b1053da2c932dee5977e0b9c78ba67
Robin Steiner <robin95@bluewin.ch>
2019-05-24 17:37:36 +0200

    Add /assets to skipped path

--- config/initializers/keycloak.rb
+++ config/initializers/keycloak.rb
@@ -2,8 +2,11 @@ Keycloak.configure do |config|
   config.server_url = "https://sso.puzzle.ch/auth"
   config.realm_id   = "pitc"
   config.logger     = Rails.logger
+  config.skip_paths = {
+    get: [/^\/assets\/.+/]
+  }
   if Rails.env.test? && ENV['FRONTEND_TESTS'] == 1
-    config.skip_paths = {
+    config.skip_paths += {
         get: [/^./],
         post: [/^./],
         delete: [/^./],
