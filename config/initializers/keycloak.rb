5f3fc868246d90c75e5e2562758fda41863ebebd
Robin Steiner <robin95@bluewin.ch>
2019-05-24 17:04:30 +0200

    Differentiate between frontend and backend tests for skipped routes

--- config/initializers/keycloak.rb
+++ config/initializers/keycloak.rb
@@ -2,12 +2,12 @@ Keycloak.configure do |config|
   config.server_url = "https://sso.puzzle.ch/auth"
   config.realm_id   = "pitc"
   config.logger     = Rails.logger
-  if Rails.env.test?
-  config.skip_paths = {
-      get: [/^./],
-      post: [/^./],
-      delete: [/^./],
-      patch: [/^./]
-    }
+  if Rails.env.test? && ENV['FRONTEND_TESTS'] == 1
+    config.skip_paths = {
+        get: [/^./],
+        post: [/^./],
+        delete: [/^./],
+        patch: [/^./]
+      }
   end
 end
--- lib/keycloak_tools.rb
+++ lib/keycloak_tools.rb
@@ -1,8 +1,8 @@
 module KeycloakTools
   def authorize_admin
-    # return if Rails.env.development? && ENV['ENABLE_AUTH'].blank?
+    return if Rails.env.development? && ENV['ENABLE_AUTH'].blank?
     return if ENV['DISABLE_AUTH'].present?
-    return if Rails.env.test?
+    return if Rails.env.test? && ENV['FRONTEND_TESTS'] == 1
 
     return if has_admin_flag?
 
