891fc9039d6a0ee3f93ae5537dae033620719cd2
Robin Steiner <robin95@bluewin.ch>
2019-05-27 10:45:03 +0200

    Fix env variable check

--- config/initializers/keycloak.rb
+++ config/initializers/keycloak.rb
@@ -2,15 +2,12 @@ Keycloak.configure do |config|
   config.server_url = "https://sso.puzzle.ch/auth"
   config.realm_id   = "pitc"
   config.logger     = Rails.logger
-  config.skip_paths = {
-    get: [/^\/assets\/.+/]
-  }
-  if Rails.env.test? && ENV['FRONTEND_TESTS'] == 1
-    config.skip_paths += {
-        get: [/^./],
-        post: [/^./],
-        delete: [/^./],
-        patch: [/^./]
+  if Rails.env.test? && ENV['FRONTEND_TESTS'] == '1'
+    config.skip_paths = {
+        get: [/^.+/],
+        post: [/^.+/],
+        delete: [/^.+/],
+        patch: [/^.+/]
       }
   end
 end
