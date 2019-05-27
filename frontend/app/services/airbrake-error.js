4c9c0fcd5fabcad37370501cac1795d289032fda
Robin Steiner <robin95@bluewin.ch>
2019-05-27 11:34:18 +0200

    Stop Airbrake from accessing test env

--- frontend/app/services/airbrake-error.js
+++ frontend/app/services/airbrake-error.js
@@ -1,28 +1,33 @@
-import Service from '@ember/service';
-import { inject as service } from '@ember/service';
-import Ember from 'ember';
+import Service from "@ember/service";
+import { inject as service } from "@ember/service";
+import Ember from "ember";
+import config from "../config/environment";
 
 export default Service.extend({
   airbrake: service(),
-  session: service('keycloak-session'),
+  session: service("keycloak-session"),
 
   init() {
     this._super(...arguments);
 
-    Ember.onerror = error => {
-      const sessionInfo = this.get('session.tokenParsed');
-      const username = sessionInfo.given_name + " " + sessionInfo.family_name;
-      const session = { username,
-        name: error.toLocaleString(),
-        url: window.location.href,
-        filename: error.fileName,
-        linenumber: error.lineNumber,
-        stack: error.stack }
+    if (config.environment == "production") {
+      Ember.onerror = error => {
+        const sessionInfo = this.get("session.tokenParsed");
+        const username = sessionInfo.given_name + " " + sessionInfo.family_name;
+        const session = {
+          username,
+          name: error.toLocaleString(),
+          url: window.location.href,
+          filename: error.fileName,
+          linenumber: error.lineNumber,
+          stack: error.stack
+        };
 
-      let airbrake = this.get('airbrake');
-      airbrake.setSession(session);
+        let airbrake = this.get("airbrake");
+        airbrake.setSession(session);
 
-      throw error;
+        throw error;
+      };
     }
   }
 });
