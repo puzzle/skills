681ae8c8952c4943c1aed244854320f969ca12c8
Miro Zurbuchen <zurbuchen@puzzle.ch>
2019-05-29 10:05:14 +0200

    Remove unused folders and files, refs #48747

--- frontend/tests/unit/adapters/application-test.js
+++
@@ -1,6 +0,0 @@
-import { moduleFor } from "ember-qunit";
-
-moduleFor("adapter:application", "Unit | Adapter | application", {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
-});
