2ce2b03015ee17ce0bc086f272ec35d955f6a00f
Robin Steiner <robin95@bluewin.ch>
2019-06-07 14:49:17 +0200

    Expand jwt for tests and load config over ENV

--- frontend/tests/helpers/keycloak-stub.js
+++ frontend/tests/helpers/keycloak-stub.js
@@ -2,28 +2,13 @@ import Service from '@ember/service';
 
 export default Service.extend({
   tokenParsed: '1234',
-  given_name: 'Name',
-  token: '1234',
-  roles: 'bla',
   keycloak: Object.freeze([{ token: '1234' }]),
-
-  get headers() {},
 
   hasResourceRole(resource, role) {},
 
   installKeycloak(parameters) {},
 
   initKeycloak() {},
-
-  hasRealmRole(role) {},
-
-  _parseRedirectUrl(router, transition) {},
-
-  loadUserProfile() {},
-
-  login(redirectUri) {},
-
-  logout(redirectUri) {},
 
   checkTransition(transition) {},
 
