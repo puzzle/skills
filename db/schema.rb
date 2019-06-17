49ab1bb5662e67d4dc97ae64f0068a3280964d45
Robin Steiner <robin95@bluewin.ch>
2019-06-17 09:02:03 +0200

    Add migration and rename config variables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_060802) do
+ActiveRecord::Schema.define(version: 2019_06_17_065814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,15 +229,6 @@ ActiveRecord::Schema.define(version: 2019_04_04_060802) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_skills_on_category_id"
-  end
-
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "ldap_uid"
-    t.string "api_token"
-    t.integer "failed_login_attempts", default: 0
-    t.datetime "last_failed_login_attempt_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "categories", "categories", column: "parent_id"
