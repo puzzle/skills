252a7a4b0841fb12014581f4907a93c670236ffe
Robin Steiner <robin95@bluewin.ch>
2019-07-29 11:40:01 +0200

    Adapt env variable naming

--- config/initializers/keycloak.rb
+++ config/initializers/keycloak.rb
@@ -1,6 +1,6 @@
 Keycloak.configure do |config|
-  config.server_url = ENV['KEYCLOAK_SERVER_URL']
-  config.realm_id   = ENV['KEYCLOAK_REALM_ID']
+  config.server_url = ENV['RAILS_KEYCLOAK_SERVER_URL']
+  config.realm_id   = ENV['RAILS_KEYCLOAK_REALM_ID']
   config.logger     = Rails.logger
   if Rails.env.test? && ENV['FRONTEND_TESTS'].present?
     config.skip_paths = {
