de20aa64ed3c09227e70202735a8ea96413e4e0c
sgilgen <gilgen@puzzle.ch>
2019-10-24 09:04:14 +0200

    Format Files

--- app/controllers/env_settings_controller.rb
+++ app/controllers/env_settings_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class EnvSettingsController < ApplicationController
+
   def index
     render json: values
   end
@@ -7,19 +10,19 @@ class EnvSettingsController < ApplicationController
 
   def values
     {
-        sentry: ENV['SENTRY_DSN_FRONTEND'],
-        rails_port: ENV['RAILS_PORT'],
-        helplink: ENV['HELPLINK'],
-        keycloak: keycloak_info
+      sentry: ENV['SENTRY_DSN_FRONTEND'],
+      rails_port: ENV['RAILS_PORT'],
+      helplink: ENV['HELPLINK'],
+      keycloak: keycloak_info
     }
   end
 
   def keycloak_info
     {
-        url: ENV['EMBER_KEYCLOAK_SERVER_URL'],
-        realm: ENV['EMBER_KEYCLOAK_REALM_NAME'],
-        clientId: ENV['EMBER_KEYCLOAK_CLIENT_ID'],
-        secret: ENV['EMBER_KEYCLOAK_SECRET']
+      url: ENV['EMBER_KEYCLOAK_SERVER_URL'],
+      realm: ENV['EMBER_KEYCLOAK_REALM_NAME'],
+      clientId: ENV['EMBER_KEYCLOAK_CLIENT_ID'],
+      secret: ENV['EMBER_KEYCLOAK_SECRET']
     }
   end
 end
--- spec/controllers/env_settings_controller_spec.rb
+++ spec/controllers/env_settings_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-describe EnvSettingsController, type: :controller do
-  describe 'GET index' do
+describe EnvSettingsController do
+  context 'GET index' do
     before do
+
       ENV['SENTRY_DSN_FRONTEND'] = 'fake-dsn'
       ENV['EMBER_KEYCLOAK_SECRET'] = 'fake-secret'
       ENV['EMBER_KEYCLOAK_CLIENT_ID'] = 'fake-id'
@@ -11,16 +12,21 @@ describe EnvSettingsController, type: :controller do
       ENV['HELPLINK'] = 'fake-link'
 
     end
-    it 'returns env_settigns' do
+    
+    it 'returns env_settings' do
+
       get :index
+
       settings = json
       settings_keycloak = settings['keycloak']
+
       expect(settings['sentry']).to eq('fake-dsn')
       expect(settings['helplink']).to eq('fake-link')
       expect(settings['rails_port']).to eq('fake-port')
       expect(settings_keycloak['secret']).to eq('fake-secret')
       expect(settings_keycloak['clientId']).to eq('fake-id')
       expect(settings_keycloak['realm']).to eq('fake-realm')
+
     end
   end
 end
