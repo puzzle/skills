a2b889895464077118d89c0ec5b0e3350898cfdf
Nils Rauch <rauch@puzzle.ch>
2020-01-31 09:40:06 +0100

    wip 3.5 upgrade fixes

--- frontend/app/services/selected-person.js
+++ frontend/app/services/selected-person.js
@@ -1,7 +1,6 @@
 import Service from "@ember/service";
 import { computed } from "@ember/object";
 
-// zu selected-person umbenennen
 export default Service.extend({
   personId: null,
   selectedSubRoute: null,
