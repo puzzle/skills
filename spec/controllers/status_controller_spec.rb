a3acb17f785ecb9fd884e3eb59c54b6af9bb55ac
Robin Steiner <robin95@bluewin.ch>
2020-02-07 12:35:35 +0100

    Add status controller test

--- spec/controllers/status_controller_spec.rb
+++ spec/controllers/status_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe StatusController, type: :controller do
+  describe 'test_health' do
+    endpoint_test :health
+  end
 
+  describe 'test_readiness' do
+    endpoint_test :readiness
+  end
+
+  private
+
+  def endpoint_test(endpoint, status = :success, matcher = /ok/)
+    get endpoint
+    assert_response status
+    assert_match matcher, response.body.to_s
+  end
 end
