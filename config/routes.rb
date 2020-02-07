6bb3c1e8d3af8f244a42d4402efc9fe9207f834f
Robin Steiner <32168247+Robin481@users.noreply.github.com>
2020-02-07 13:36:46 +0100

    Feature/52997 status checks (#323)
    * Add status controller
    
    * Catch specific PG connection error
    
    * Add status controller test
    
    * Fix rubocop issues
    
    * Fix status controller test
    
    * Add scope and raise better
    
    * Change raise order

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  scope 'status' do
+    get 'health', to: 'status#health'
+    get 'readiness', to: 'status#readiness'
+  end
+
   scope '/api' do
 
 
