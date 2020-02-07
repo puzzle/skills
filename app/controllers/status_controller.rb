a4bcc486844e9d26cce5327367cc32801decb6a3
Robin Steiner <robin95@bluewin.ch>
2020-02-07 10:54:01 +0100

    Add status controller

---
+++ app/controllers/status_controller.rb
@@ -0,0 +1,31 @@
+class StatusController < ApplicationController
+
+  # Web Server Okay?
+  def health
+    render json: { status: :ok }
+  end
+
+  # Are we ready to serve requests?
+  def readiness
+    ready, status, message = assess_readiness
+    http_code = ready ? :ok : :internal_server_error
+
+    render json: { status: status, message: message}, status: http_code
+  end
+
+  private
+
+  def assess_readiness
+    return [true, :ok, 'OK'] if can_query_database?
+
+    [false, :service_unavailable, 'ERROR: Can not connect to the database']
+  end
+
+  def can_query_database?
+    ActiveRecord::Base.connection.execute('SELECT 1')
+    true
+  rescue Mysql2::Error
+    false
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  get 'status/health', to: 'status#health'
+  get 'status/readiness', to: 'status#readiness'
+
   scope '/api' do
 
 
