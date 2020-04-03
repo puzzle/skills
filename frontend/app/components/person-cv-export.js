dbf6430a18adab8fa81e7fc8fc65b6469c88a56d
Michael Dooley <mdooley-ch@live.com>
2020-04-03 18:25:37 +0200

    modal view in frontend now works

--- frontend/app/components/person-cv-export.js
+++ frontend/app/components/person-cv-export.js
@@ -2,34 +2,31 @@ import Component from "@ember/component";
 import { inject } from "@ember/service";
 
 export default Component.extend({
-  store: inject(),
-  intl: inject(),
+  download: inject(),
+  router: inject(),
 
   init() {
     this._super(...arguments);
   },
 
-  didInsertElement() {
-    //We need global jquery here because Bootstrap renders the modal outside of the component
-    /* eslint-disable ember/no-global-jquery, no-undef, ember/jquery-ember-run  */
-    $("#person-cv-export").on("hide.bs.modal", () => {
-      //this.abort();
-    });
-    /* eslint-enable no-global-jquery, no-undef, jquery-ember-run  */
-  },
-
   actions: {
-    abortNew() {
-      /*this try catch is necessary because bootstrap modal does not work in acceptance tests,
-      meaning, this would throw an error no matter if the actual feature works and the test
-      would fail.*/
-      /* eslint-disable no-undef  */
-      try {
-        $("#person-cv-export").modal("hide");
-      } catch (error) {
-        Ember.Logger.warn(error.stack);
-      }
-      /* eslint-enable no-undef  */
+    startExport(personId, e) {
+      e.preventDefault();
+
+      let url =
+        `/api/people/` +
+        this.get("router.currentURL").split("/")[2] +
+        `.odt?anon=false`; //TODO: This is ugly
+      this.get("download").file(url);
+    },
+
+    startAnonymizedExport(personId, e) {
+      e.preventDefault();
+      let url =
+        `/api/people/` +
+        this.get("router.currentURL").split("/")[2] +
+        `.odt?anon=true`; //TODO: This is ugly
+      this.get("download").file(url);
     }
   }
 });
