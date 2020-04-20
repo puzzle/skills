7318079f08b6d73c885fccd43c1c3f9737c9baec
DominikFischli <dominikfischli@bluewin.ch>
2020-04-20 16:50:33 +0200

    removed unused attribute

--- frontend/app/models/person.js
+++ frontend/app/models/person.js
@@ -3,7 +3,6 @@ import { computed } from "@ember/object";
 
 const Person = DS.Model.extend({
   name: DS.attr("string"),
-  foundIn: DS.attr("string"),
   birthdate: DS.attr("date"),
   picturePath: DS.attr("string"),
   location: DS.attr("string"),
