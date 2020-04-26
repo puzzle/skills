f54365225bdf04395642391d7831b99b69fdd59d
DominikFischli <dominikfischli@bluewin.ch>
2020-04-26 18:11:26 +0200

    added jump-to helper

---
+++ frontend/app/helpers/person-jump-to-helper.js
@@ -0,0 +1,33 @@
+import { helper } from "@ember/component/helper";
+import $ from "jquery";
+import Ember from "ember";
+
+export function personJumpTo([query, foundIn]) {
+  Ember.run.later(() => markAndJump(query), 550);
+}
+
+function markAndJump(query) {
+  const elements = $(".cv-search-searchable");
+  const regex = new RegExp(query, "ig");
+  for (const element of elements) {
+    if (element.textContent.search(regex) !== -1) {
+      mark(element, regex);
+      jumpToMarked();
+      return;
+    }
+  }
+}
+
+function mark(element, regex) {
+  const text = element.textContent;
+  const markedText = text.match(regex)[0];
+  $(element).html(text.replace(regex, '<span class="mark"></span>'));
+  $(".mark").text(markedText);
+}
+
+function jumpToMarked() {
+  const topOfMarked = $(".mark").offset().top - 176;
+  window.scroll({ top: topOfMarked, behavior: "smooth" });
+}
+
+export default helper(personJumpTo);
