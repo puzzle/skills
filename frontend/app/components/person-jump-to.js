4cd653c1fda20bde36a167971a6e273213a8a367
DominikFischli <dominikfischli@bluewin.ch>
2020-04-27 10:49:19 +0200

    refactored into commponent

--- frontend/app/components/person-jump-to.js
+++ frontend/app/components/person-jump-to.js
@@ -1,14 +1,23 @@
-import { helper } from "@ember/component/helper";
+import classic from "ember-classic-decorator";
+import Component from "@ember/component";
 import $ from "jquery";
 import { later } from "@ember/runloop";
 
-export function personJumpTo([query, foundIn]) {
-  later(() => markAndJump(query), 550);
+@classic
+export default class PersonJumpTo extends Component {
+  didRender() {
+    later(() => personJumpTo(this.query), 550);
+  }
 }
 
-function markAndJump(query) {
+function personJumpTo(query) {
   const elements = $(".cv-search-searchable");
   const regex = new RegExp(query, "ig");
+
+  searchElements(elements, regex);
+}
+
+function searchElements(elements, regex) {
   for (const element of elements) {
     if (element.textContent.search(regex) !== -1) {
       mark(element, regex);
@@ -20,14 +29,12 @@ function markAndJump(query) {
 
 function mark(element, regex) {
   const text = element.textContent;
-  const markedText = text.match(regex)[0];
+  const textToMark = text.match(regex)[0];
   $(element).html(text.replace(regex, '<span class="mark"></span>'));
-  $(".mark").text(markedText);
+  $(".mark").text(textToMark);
 }
 
 function jumpToMarked() {
   const topOfMarked = $(".mark").offset().top - 176;
   window.scroll({ top: topOfMarked, behavior: "smooth" });
 }
-
-export default helper(personJumpTo);
