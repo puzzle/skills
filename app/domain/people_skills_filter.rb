c36a96fb55ef1d734ae94214723eabeb7acef0e3
DominikFischli <dominikfischli@bluewin.ch>
2020-05-18 00:45:21 +0200

    Renamed function, escaped query

--- app/domain/people_skills_filter.rb
+++ app/domain/people_skills_filter.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class PeopleSkillsFilter
-  attr_reader :entries, :rated, :level, :skill_id
+  attr_reader :entries, :rated, :levels, :skill_ids, :level_for_skill
 
   def initialize(entries, rated, level = nil, skill_id = nil)
     @entries = entries
     @rated = rated
     @levels = level.nil? ? nil : level.to_s.split(',')
     @skill_ids = skill_id.nil? ? nil : skill_id.to_s.split(',')
+    unless skill_ids.nil?
+      @level_for_skill = skill_ids.zip(levels).map { |ls| { skill: ls[0], level: ls[1] } }
+    end
   end
 
   def scope
@@ -27,8 +30,8 @@ class PeopleSkillsFilter
   end
 
   def filter_by_level(entries)
-    return entries if @levels.nil?
-    if @skill_ids.length == @levels.length
+    return entries if levels.nil?
+    if skill_ids.length == levels.length
       return level_filter_results(entries)
     else
       raise ArgumentError, 'Amount of Skill_ids and levels provided do not match'
@@ -36,22 +39,19 @@ class PeopleSkillsFilter
   end
 
   def level_filter_results(entries)
-    person_ids = []
-    entries.where(buildstring).group(:person_id).count.each_entry do |key, value|
-      person_ids.push(key) if value == @skill_ids.length
-    end
+    filtered_entries = filter_entries_by_skill_and_level(entries)
+    skills_per_person = filtered_entries.group(:person_id).count
+    person_ids = skills_per_person.map { |id, count| id if count == @skill_ids.length }
     entries.where(person_id: person_ids, skill_id: @skill_ids)
   end
 
-  def buildstring
-    result = +''
-    @skill_ids.each_with_index do |skill, index|
-      result.concat('(skill_id=')
-            .concat(skill.to_s)
-            .concat(' and level >=')
-            .concat(@levels[index].to_s)
-            .concat(') or ')
+  def filter_entries_by_skill_and_level(entries)
+    base = entries.clone
+    l_s = level_for_skill.clone.pop
+    base = entries.where('skill_id = ? and level >= ?', l_s[:skill], l_s[:level])
+    level_for_skill.each do |ls|
+      base = base.or(entries.where('skill_id = ? and level >= ?', ls[:skill], ls[:level]))
     end
-    result.delete_suffix(' or ')
+    base
   end
 end
