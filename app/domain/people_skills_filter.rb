aec0a3ade59afc3ef73a052aff15752448040c19
DominikFischli <dominikfischli@bluewin.ch>
2020-05-18 15:57:28 +0200

    polished code for readability

--- app/domain/people_skills_filter.rb
+++ app/domain/people_skills_filter.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 class PeopleSkillsFilter
-  attr_reader :entries, :rated, :levels, :skill_ids, :level_for_skill
+  attr_reader :entries, :rated, :levels_for_skills
 
-  def initialize(entries, rated, level = nil, skill_id = nil)
+  def initialize(entries, rated, levels = [], skill_ids = [])
     @entries = entries
     @rated = rated
-    @levels = level.nil? ? nil : level.to_s.split(',')
-    @skill_ids = skill_id.nil? ? nil : skill_id.to_s.split(',')
-    unless skill_ids.nil?
-      @level_for_skill = skill_ids.zip(levels).map { |ls| { skill: ls[0], level: ls[1] } }
-    end
+    @levels_for_skills = []
+    set_levels_for_skills(levels, skill_ids)
   end
 
   def scope
@@ -18,6 +15,19 @@ class PeopleSkillsFilter
   end
 
   private
+
+  def set_levels_for_skills(levels, skill_ids)
+    return if levels.blank? || skill_ids.blank?
+
+    levels = levels.to_s.split(',')
+    skill_ids = skill_ids.to_s.split(',')
+
+    if levels.length != skill_ids.length
+      raise ArgumentError, 'For each Skill there must be a level present'
+    end
+
+    @levels_for_skills = skill_ids.zip(levels).map { |ls| { skill: ls[0], level: ls[1] } }
+  end
 
   def filter_by_rated
     if rated == 'true'
@@ -30,28 +40,32 @@ class PeopleSkillsFilter
   end
 
   def filter_by_level(entries)
-    return entries if levels.nil?
-    if skill_ids.length == levels.length
-      return level_filter_results(entries)
-    else
-      raise ArgumentError, 'Amount of Skill_ids and levels provided do not match'
-    end
-  end
+    return entries if levels_for_skills.empty?
 
-  def level_filter_results(entries)
-    filtered_entries = filter_entries_by_skill_and_level(entries)
+    filtered_entries = filter_for_skills_and_levels(entries)
     skills_per_person = filtered_entries.group(:person_id).count
-    person_ids = skills_per_person.map { |id, count| id if count == @skill_ids.length }
-    entries.where(person_id: person_ids, skill_id: @skill_ids)
+    person_ids = persons_with_required_skill(skills_per_person)
+
+    entries.where(person_id: person_ids, skill_id: skill_ids)
   end
 
-  def filter_entries_by_skill_and_level(entries)
-    base = entries.clone
-    l_s = level_for_skill.clone.pop
-    base = entries.where('skill_id = ? and level >= ?', l_s[:skill], l_s[:level])
-    level_for_skill.each do |ls|
-      base = base.or(entries.where('skill_id = ? and level >= ?', ls[:skill], ls[:level]))
+  def filter_for_skills_and_levels(entries)
+    result = PeopleSkill.none
+    levels_for_skills.each do |ls|
+      result = result.or(filter_for_level_and_skill(entries, ls[:skill], ls[:level]))
     end
-    base
+    result
+  end
+
+  def filter_for_level_and_skill(entries, skill, level)
+    entries.where('skill_id = ? and level >= ?', skill, level)
+  end
+
+  def persons_with_required_skill(skills_per_person)
+    skills_per_person.map { |id, count| id if count == skill_ids.length }
+  end
+
+  def skill_ids
+    levels_for_skills.map { |ls| ls[:skill] }
   end
 end
