e3919421be057b554f86194013c9c032084b38ae
Robin Steiner <robin95@bluewin.ch>
2020-06-26 16:05:23 +0200

    Fix ember build another way

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -6,11 +6,6 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    "ember-cli-uglify": {
-      uglify: {
-        compress: false // Workaround for build issue
-      }
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
