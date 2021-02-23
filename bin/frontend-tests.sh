ddc7eeabfd1e55f19101f2cee24fd795ec3210c8
Ruggero Botteon <botteon@puzzle.ch>
2021-02-23 13:24:40 +0100

    #408: add log

--- bin/frontend-tests.sh
+++ bin/frontend-tests.sh
@@ -13,16 +13,20 @@ esac
 
 rails server -e test -d -p $RAILS_PORT -P $PID_FILE
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
+echo 1$PWD
 cd frontend
+echo 2$PWD
 if [[ $SERVER == true ]]; then
+  echo 3$PWD
   ember test --server
 else
+  echo 4$PWD
   yarn test
 fi
 rc=$?
+echo 5$PWD
 cd ..
-
+echo 6$PWD
 kill -INT $(cat $PID_FILE)
 
 exit $rc
