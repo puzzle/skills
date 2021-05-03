ff5c71705bdffecf3c8a81dcb75e201e929f368b
wmvong <wing.vong@students.unibe.ch>
2021-05-03 12:14:59 +0200

    test passes

--- frontend/tests/acceptance/person-jump-to-test.js
+++ frontend/tests/acceptance/person-jump-to-test.js
@@ -1,126 +1,43 @@
 import { module, test } from "qunit";
 import page from "frontend/tests/pages/cv-search";
 import setupApplicationTest from "frontend/tests/helpers/setup-application-test";
-import { currentURL, triggerEvent } from "@ember/test-helpers";
+import { click, currentURL, triggerEvent } from "@ember/test-helpers";
 
 module("Acceptance | person jump to", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("jump to Bachelor of Communications", async function(assert) {
-    //assert.expect(3);
+  test("jump to BSc in Cleaning", async function(assert) {
+    assert.expect(5);
 
     await page.indexPage.visit();
     assert.equal(currentURL(), "/cv_search");
 
-    await page.indexPage.searchInput("Bachelor of Communications");
+    await page.indexPage.searchInput("BSc in Cleaning");
     await triggerEvent("input", "keyup");
-    assert.equal(currentURL(), "/cv_search?q=Bachelor%20of%20Communications");
+    assert.equal(currentURL(), "/cv_search?q=BSc%20in%20Cleaning");
+
+    const names = page.indexPage.people.peopleNames
+      .toArray()
+      .map(name => name.text);
+    const foundIns = page.indexPage.people.peopleFoundInLink
+      .toArray()
+      .map(foundIn => foundIn.text);
+    assert.ok(names.includes("Bob Anderson"));
+    assert.ok(foundIns.includes("Titel"));
+
+    const button = document.querySelector(".cv-search-found-in-link");
+    await click(button);
 
     let store = this.owner.__container__.lookup("service:store");
-    let jorah = store
+    let bob = store
       .peekAll("person")
-      .filter(person => person.get("name") == "Jorah Mormont")
+      .filter(person => person.get("name") == "Bob Anderson")
       .get("firstObject");
-    await jorah;
-
-    //await click(".cv-search-found-in-link");
+    await bob;
 
     assert.equal(
       currentURL(),
-      "/people/" + jorah.get("id") + "?query=Bachelor%20of%20Communications"
+      "/people/" + bob.get("id") + "?query=BSc%20in%20Cleaning"
     );
   });
-
-  /*
-  test("jump to Gregor", async function(assert) {
-    assert.expect(3);
-
-    await page.indexPage.visit();
-    assert.equal(currentURL(), "/cv_search");
-
-    await page.indexPage.searchInput("Gregor");
-    await triggerEvent("input", "keyup");
-    assert.equal(currentURL(), "/cv_search?q=Gregor");
-
-    await click(".cv-search-found-in-link");
-
-    let store = this.owner.__container__.lookup("service:store");
-    let junit = store
-      .peekAll("skill")
-      .filter(skill => skill.get("title") == "JUnit")
-      .get("firstObject");
-    await junit;
-    assert.equal(currentURL(), "/people/" + junit.get("id") + "?query=Gregor");
-  });
-
-  test("jump to agriculturist", async function(assert) {
-    assert.expect(3);
-
-    await page.indexPage.visit();
-    assert.equal(currentURL(), "/cv_search");
-
-    await page.indexPage.searchInput("agriculturist");
-    await triggerEvent("input", "keyup");
-    assert.equal(currentURL(), "/cv_search?q=agriculturist");
-
-    await click(".cv-search-found-in-link");
-
-    let store = this.owner.__container__.lookup("service:store");
-    let junit = store
-      .peekAll("skill")
-      .filter(skill => skill.get("title") == "JUnit")
-      .get("firstObject");
-    await junit;
-    assert.equal(
-      currentURL(),
-      "/people/" + junit.get("id") + "?query=agriculturist"
-    );
-  });
-
-  test("jump to judge", async function(assert) {
-    assert.expect(3);
-
-    await page.indexPage.visit();
-    assert.equal(currentURL(), "/cv_search");
-
-    await page.indexPage.searchInput("judge");
-    await triggerEvent("input", "keyup");
-    assert.equal(currentURL(), "/cv_search?q=judge");
-
-    await click(".cv-search-found-in-link");
-
-    let store = this.owner.__container__.lookup("service:store");
-    let junit = store
-      .peekAll("skill")
-      .filter(skill => skill.get("title") == "JUnit")
-      .get("firstObject");
-    await junit;
-    assert.equal(currentURL(), "/people/" + junit.get("id") + "?query=judge");
-  });
-
-  test("jump to Vitakinesis", async function(assert) {
-    //assert.expect(3);
-
-    await page.indexPage.visit();
-    assert.equal(currentURL(), "/cv_search");
-
-    await page.indexPage.searchInput("Vitakinesis");
-    await triggerEvent("input", "keyup");
-    assert.equal(currentURL(), "/cv_search?q=Vitakinesis");
-
-    await click(".cv-search-found-in-link");
-
-    let store = this.owner.__container__.lookup("service:store");
-    let junit = store
-      .peekAll("skill")
-      .filter(skill => skill.get("title") == "JUnit")
-      .get("firstObject");
-    await junit;
-
-    assert.equal(
-      currentURL(),
-      "/people/" + junit.get("id") + "?query=Vitakinesis"
-    );
-  });
-  */
 });
