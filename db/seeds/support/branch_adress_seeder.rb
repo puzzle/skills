b8b7432ddc040cce18e5ceacec4db07b511455a8
Niklas Jäggi <jaeggi@puzzle.ch>
2023-02-13 13:20:07 +0100

    implement branch adress seeder

---
+++ db/seeds/support/branch_adress_seeder.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+class BranchAdressSeeder
+  def seed_branch_adresses(branch_short_names, branch_adress_informations)
+    branch_short_names.each_with_index do |branch_short_name, index|
+      seed_branch_adress(branch_short_name, branch_adress_informations[index.to_i])
+    end
+  end
+
+  private
+
+  def seed_branch_adress(branch_short_name, adress_information)
+    BranchAdress.seed_once(:adress_information) do |p|
+      p.short_name = branch_short_name.to_s
+      p.adress_information = adress_information.to_s
+      p.country = 'CH'
+    end
+  end
+end
