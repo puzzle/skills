e398b4c7b82cd2dd5304e443a0449a000c153314
Lias Kleisa <kleisa@puzzle.ch>
2023-02-15 14:12:40 +0100

    Gray out create and edit buttons when non admin member

--- frontend/app/components/skills-list-row-show.js
+++ frontend/app/components/skills-list-row-show.js
@@ -1,10 +1,18 @@
 import classic from "ember-classic-decorator";
 import { tagName } from "@ember-decorators/component";
 import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
 @classic
 @tagName("tr")
 export default class SkillsListRowShow extends Component {
+  @service("keycloak-session")
+  session;
+  init() {
+    super.init(...arguments);
+    let session = this.get("session");
+    this.set("isAdmin", session.hasResourceRole("ADMIN"));
+  }
   click() {
     this.sendAction("toggleSkill", this.get("skill"));
   }
