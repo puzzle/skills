808bbdc955b469ee8e68a389a0b9868900ecf3cf
Niklas Jäggi <jaeggi@puzzle.ch>
2023-02-16 10:40:37 +0100

    implement feedback

--- db/seeds/support/branch_adress_seeder.rb
+++ db/seeds/support/branch_adress_seeder.rb
@@ -2,7 +2,7 @@
 class BranchAdressSeeder
   def seed_branch_adresses(branch_short_names, branch_adress_informations)
     branch_short_names.each_with_index do |branch_short_name, index|
-      seed_branch_adress(branch_short_name, branch_adress_informations[index.to_i])
+      seed_branch_adress(branch_short_name, branch_adress_informations[index])
     end
   end
 
@@ -10,8 +10,8 @@ class BranchAdressSeeder
 
   def seed_branch_adress(branch_short_name, adress_information)
     BranchAdress.seed_once(:adress_information) do |b|
-      b.short_name = branch_short_name.to_s
-      b.adress_information = adress_information.to_s
+      b.short_name = branch_short_name
+      b.adress_information = adress_information
       b.country = 'CH'
     end
   end
