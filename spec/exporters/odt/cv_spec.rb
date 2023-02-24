6f03ad9674e3621cfc65452d5d9fe8afb1b14d44
Lias Kleisa <kleisa@puzzle.ch>
2023-02-24 09:55:02 +0100

    Write rails tests for filter peopleskills by level

--- spec/exporters/odt/cv_spec.rb
+++ spec/exporters/odt/cv_spec.rb
@@ -29,6 +29,29 @@ describe Odt::Cv do
       expect(nationalities).to eq('Australien')
     end
 
+    it 'get one skill hash when filter on level 3' do
+      skills = Odt::Cv.new(people(:bob), {'skillsByLevel' => 'true'}).send(:skills_by_level_value, 3)
+
+      expect(skills).to eq([{:category=>"Software-Engineering", :competence=>"Rails"}])
+    end
+
+    it 'get empty skill hash when filter on level 5' do
+      skills = Odt::Cv.new(people(:bob), {'skillsByLevel' => 'true'}).send(:skills_by_level_value, 5)
+
+      expect(skills).to eq([])
+    end
+
+    it 'get PeopleSkill by level 2' do
+      skills = Odt::Cv.new(people(:bob), {'skillsByLevel' => 'true'}).send(:skills_by_level, 2)
+      people_skill = PeopleSkill.find 996949822
+      expect(skills).to eq([people_skill])
+    end
+
+    it 'get no PeopleSkill by level 5' do
+      skills = Odt::Cv.new(people(:bob), {'skillsByLevel' => 'true'}).send(:skills_by_level, 5)
+      expect(skills).to eq([])
+    end
+
     it 'formats competence notes' do
       notes = Odt::Cv.new(people(:bob), {'anon' => 'false'}).send(:competence_notes_list)[:competence]
       expect(notes).to eq('Java\n Ruby')
