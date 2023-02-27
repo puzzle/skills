a128c8c45199863971fa1c1859afac5d1a55bb84
Lias Kleisa <kleisa@puzzle.ch>
2023-02-27 16:00:48 +0100

    Write frontend test for email validation

--- frontend/tests/integration/components/person-edit-test.js
+++ frontend/tests/integration/components/person-edit-test.js
@@ -75,4 +75,47 @@ module("Integration | Component | person-edit", function(hooks) {
         .includes("Birthdate can't be blank")
     );
   });
+
+  test("it renders person-edit with validation error at empty email", async function(assert) {
+    let person = run(() =>
+      this.owner.lookup("service:store").createRecord("person")
+    );
+    person.name = "Hans Rudolf";
+    person.title = "Construction Consultant";
+    person.location = "Bern";
+    person.birthdate = new Date(2019, 1, 19);
+    person.shortname = "HR";
+    this.set("person", person);
+
+    await render(hbs`{{person-edit person=person}}`);
+    this.$("button")[0].click();
+    await settled();
+    assert.ok(
+      this.$("#validation-error")
+        .text()
+        .includes("Email kann nicht leer sein")
+    );
+  });
+
+  test("it renders person-edit with validation error at invalid email", async function(assert) {
+    let person = run(() =>
+      this.owner.lookup("service:store").createRecord("person")
+    );
+    person.name = "Hans Rudolf";
+    person.email = "hans";
+    person.title = "Construction Consultant";
+    person.location = "Bern";
+    person.birthdate = new Date(2019, 1, 19);
+    person.shortname = "HR";
+    this.set("person", person);
+
+    await render(hbs`{{person-edit person=person}}`);
+    this.$("button")[0].click();
+    await settled();
+    assert.ok(
+      this.$("#validation-error")
+        .text()
+        .includes("Gib eine gültige Email Adresse ein")
+    );
+  });
 });
