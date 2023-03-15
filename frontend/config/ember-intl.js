be544cd3f49e604f393425b47e2f42dfe60f8211
Lias Kleisa <kleisa@puzzle.ch>
2023-03-15 13:02:37 +0100

    Upgrade @ember/optional-features and remove unused config

--- frontend/config/ember-intl.js
+++ frontend/config/ember-intl.js
@@ -3,40 +3,6 @@
 module.exports = function(/* env */) {
   return {
     /**
-     * The locales that our application supports.
-     *
-     * This is optional and is automatically set if project stores translations
-     * where ember-intl is able to look them up (<project root>/translations/).
-     *
-     * If the project relies on side-loading translations, then you must explicitly
-     * list out the locales. i.e: ['en-us', 'en-gb', 'fr-fr']
-     *
-     * @property locales
-     * @type {Array?}
-     * @default "null"
-     */
-    locales: null,
-
-    /**
-     * autoPolyfill, when true will automatically inject the IntlJS polyfill
-     * into index.html
-     *
-     * @property autoPolyfill
-     * @type {Boolean}
-     * @default "false"
-     */
-    autoPolyfill: false,
-
-    /**
-     * disablePolyfill prevents the polyfill from being bundled in the asset folder of the build
-     *
-     * @property disablePolyfill
-     * @type {Boolean}
-     * @default "false"
-     */
-    disablePolyfill: false,
-
-    /**
      * prevents the translations from being bundled with the application code.
      * This enables asynchronously loading the translations for the active locale
      * by fetching them from the asset folder of the build.
