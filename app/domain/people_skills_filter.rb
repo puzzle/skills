3fecf6c82301421f97bc73e969617b0ec64df145
Niklas Jäggi <jaeggi@puzzle.ch>
2023-03-20 13:27:42 +0100

    improve backend and merge level and interest filter into one array

--- app/domain/people_skills_filter.rb
+++ app/domain/people_skills_filter.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class PeopleSkillsFilter
-  attr_reader :entries, :rated, :levels_for_skills, :interests_for_skills
+  attr_reader :entries, :rated, :levels_and_interests_for_skills
 
   def initialize(entries, rated, levels = [], interests = [], skill_ids = [])
     @entries = entries
     @rated = rated
-    @levels_for_skills = []
-    set_levels_for_skills(levels, skill_ids)
-    @interests_for_skills = []
-    set_interests_for_skills(interests, skill_ids)
+    @levels_and_interests_for_skills = []
+    set_levels_and_interests_for_skills(levels, interests, skill_ids)
   end
 
   def scope
@@ -18,30 +16,18 @@ class PeopleSkillsFilter
 
   private
 
-  def set_levels_for_skills(levels, skill_ids)
-    return if levels.blank? || skill_ids.blank?
+  def set_levels_and_interests_for_skills(levels, interests, skill_ids)
+    return if levels.blank? || skill_ids.blank? || interests.blank?
 
     levels = levels.to_s.split(',')
-    skill_ids = skill_ids.to_s.split(',')
-
-    if levels.length != skill_ids.length
-      raise ArgumentError, 'For each Skill there must be a level present'
-    end
-
-    @levels_for_skills = skill_ids.zip(levels).map { |ls| { skill: ls[0], level: ls[1] } }
-  end
-
-  def set_interests_for_skills(interests, skill_ids)
-    return if interests.blank? || skill_ids.blank?
-
     interests = interests.to_s.split(',')
     skill_ids = skill_ids.to_s.split(',')
 
-    if interests.length != skill_ids.length
-      raise ArgumentError, 'For each Skill there must be a interest present'
+    if levels.length != skill_ids.length || interests.length != skill_ids.length
+      raise ArgumentError, 'For each Skill there must be a level and interest present'
     end
 
-    @interests_for_skills = skill_ids.zip(interests).map { |ls| { skill: ls[0], interest: ls[1] } }
+    @levels_and_interests_for_skills = skill_ids.zip(levels, interests).map { |ls| { skill: ls[0], level: ls[1], interest: ls[2] } }
   end
 
   def filter_by_rated
@@ -56,7 +42,7 @@ class PeopleSkillsFilter
   end
 
   def filter_by_level_and_interest(entries)
-    return entries if levels_for_skills.empty? || interests_for_skills.empty?
+    return entries if levels_and_interests_for_skills.empty?
 
     filtered_entries = filter_for_skills_and_levels_and_interests(entries)
     skills_per_person = filtered_entries.group(:person_id).count
@@ -67,8 +53,8 @@ class PeopleSkillsFilter
 
   def filter_for_skills_and_levels_and_interests(entries)
     result = PeopleSkill.none
-    levels_for_skills.each do |ls|
-      result = result.or(filter_for_level_and_skill_and_interest(entries, ls[:skill], ls[:level], interests_for_skills.find { |h| h[:skill] == ls[:skill]}[:interest]))
+    levels_and_interests_for_skills.each do |ls|
+      result = result.or(filter_for_level_and_skill_and_interest(entries, ls[:skill], ls[:level], ls[:interest]))
     end
     result
   end
@@ -82,6 +68,6 @@ class PeopleSkillsFilter
   end
 
   def skill_ids
-    levels_for_skills.map { |ls| ls[:skill] }
+    levels_and_interests_for_skills.map { |ls| ls[:skill] }
   end
 end
