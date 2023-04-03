d8bc1722f063e90663c8d9e32f2422b74fdb2d68
Niklas Jäggi <jaeggi@puzzle.ch>
2023-04-03 14:09:12 +0200

    fix profile display skills by implementing varibale that differs between skill list and single skill

--- frontend/app/components/people-skill-list-show.js
+++ frontend/app/components/people-skill-list-show.js
@@ -1,5 +1,4 @@
 import classic from "ember-classic-decorator";
-import { observes } from "@ember-decorators/object";
 import { action, computed } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@ember/component";
@@ -10,13 +9,13 @@ export default class PeopleSkillListShow extends Component {
 
   init() {
     super.init(...arguments);
-    this.set("levelValue", this.get("skill.level"));
-    if (!this.get("skill.level")) {
+    this.set("levelValue", this.get("level"));
+    if (!this.get("level")) {
       this.set("levelValue", 1);
     }
   }
 
-  @computed("skill.level")
+  @computed("level")
   get levelName() {
     const levelNames = [
       "Nicht bewertet",
@@ -26,12 +25,12 @@ export default class PeopleSkillListShow extends Component {
       "Senior",
       "Expert"
     ];
-    return levelNames[this.get("skill.level")];
+    return levelNames[this.get("level")];
   }
 
   @action
   adjustSliderStylingOnReset() {
-    if (!this.get("skill.level")) {
+    if (!this.get("level")) {
       this.sliderHandle = this.$(".slider-handle:first");
       this.sliderHandle.removeClass("slider-handle");
       this.$(".in-selection").removeClass("in-selection");
