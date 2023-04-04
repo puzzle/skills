35702033ae50f21aa3ce86bf70061828909c2ccd
Niklas Jäggi <jaeggi@puzzle.ch>
2023-04-04 09:52:52 +0200

    clean up backend

--- app/domain/people_skills_filter.rb
+++ app/domain/people_skills_filter.rb
@@ -23,11 +23,16 @@ class PeopleSkillsFilter
     interests = interests.to_s.split(',')
     skill_ids = skill_ids.to_s.split(',')
 
-    if levels.length != skill_ids.length || interests.length != skill_ids.length
-      raise ArgumentError, 'For each Skill there must be a level and interest present'
-    end
+    check_query_param_length(levels, interests, skill_ids)
 
     @levels_and_interests_for_skills = skill_ids.zip(levels, interests).map { |ls| { skill: ls[0], level: ls[1], interest: ls[2] } }
+  end
+
+  def check_query_param_length(levels, interests, skill_ids)
+    # & is the save navigation operator and checks for nil
+    if levels&.length != skill_ids&.length || interests&.length != skill_ids&.length
+      raise ArgumentError, 'For each Skill there must be a level and interest present'
+    end
   end
 
   def filter_by_rated
@@ -49,8 +54,15 @@ class PeopleSkillsFilter
     person_ids = persons_with_required_skill(skills_per_person)
 
     # include skill and person to access name and title in query
-    data = entries.includes(:skill, :person).where(person_id: person_ids, skill_id: skill_ids)
-                  .group_by(&:person_id).map do |person_id, skills|
+    data = find_person_skills(entries, person_ids)
+
+    # Serialize Data and convert to json before returning
+    JSON.generate(PeopleSearchSkillSerializer.serialize(data))
+  end
+
+  def find_person_skills(entries, person_ids)
+    entries.includes(:skill, :person).where(person_id: person_ids, skill_id: skill_ids)
+           .group_by(&:person_id).map do |person_id, skills|
       skills_array = skills.map do |skill|
         { people_skill_id: skill.id, skill_id: skill.skill_id, title: skill.skill.title, level: skill.level, interest: skill.interest, certificate: skill.certificate, core_competence: skill.core_competence }
       end
@@ -58,15 +70,12 @@ class PeopleSkillsFilter
 
       { person_id: person_id, name: person[:name], skills: skills_array }
     end
-
-    # Serialize Data and convert to json before returning
-    JSON.generate(PeopleSearchSkillSerializer.serialize(data))
   end
 
   def filter_for_skills_and_levels_and_interests(entries)
     result = PeopleSkill.none
-    levels_and_interests_for_skills.each do |ls|
-      result = result.or(filter_for_level_and_skill_and_interest(entries, ls[:skill], ls[:level], ls[:interest]))
+    levels_and_interests_for_skills.each do |search_param|
+      result = result.or(filter_for_level_and_skill_and_interest(entries, search_param[:skill], search_param[:level], search_param[:interest]))
     end
     result
   end
