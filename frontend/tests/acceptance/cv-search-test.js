08bead72bd0bb74823c5a7b498b2dd2ebaf1e85b
megli2 <egli@puzzle.ch>
2023-04-05 13:22:19 +0200

    add method to delete all cookies after each test

--- frontend/tests/acceptance/cv-search-test.js
+++ frontend/tests/acceptance/cv-search-test.js
@@ -10,9 +10,7 @@ module("Acceptance | cv search", function(hooks) {
 
   test("search person by title MA in sending silly memes", async function(assert) {
     assert.expect(6);
-
     await page.indexPage.visit();
-
     assert.equal(currentURL(), "/cv_search");
 
     await page.indexPage.searchInput("MA in sending silly memes");
@@ -31,6 +29,7 @@ module("Acceptance | cv search", function(hooks) {
     assert.ok(foundIns.includes(t("person.title")));
     assert.notOk(names.includes("Alice Mante"));
     assert.notOk(names.includes("Bob Anderson"));
+    deleteAllCookies();
   });
 
   test("search person by competence and project technology Java", async function(assert) {
@@ -43,7 +42,7 @@ module("Acceptance | cv search", function(hooks) {
     await page.indexPage.searchInput("Java");
     await triggerEvent("input", "keyup");
 
-    assert.equal(currentURL(), "/cv_search?q=Java");
+    assert.equal("/cv_search?q=Java", currentURL());
     const names = page.indexPage.people.peopleNames
       .toArray()
       .map(name => name.text);
@@ -55,6 +54,7 @@ module("Acceptance | cv search", function(hooks) {
     assert.ok(foundIn.includes(t("person.projects")));
     assert.ok(foundIn.includes(t("person.personCompetences")));
     assert.notOk(names.includes("ken"));
+    deleteAllCookies();
   });
 
   test("search person by activity description Ascom", async function(assert) {
@@ -78,11 +78,11 @@ module("Acceptance | cv search", function(hooks) {
     assert.ok(foundIn.includes(t("person.activities")));
     assert.notOk(names.includes("Bob Anderson"));
     assert.notOk(names.includes("ken"));
+    deleteAllCookies();
   });
 
   test("search person by education location Uni Bern", async function(assert) {
     assert.expect(6);
-
     await page.indexPage.visit();
 
     assert.equal(currentURL(), "/cv_search");
@@ -101,11 +101,11 @@ module("Acceptance | cv search", function(hooks) {
     assert.ok(foundIn.includes(t("person.educations")));
     assert.notOk(names.includes("Alice Mante"));
     assert.notOk(names.includes("ken"));
+    deleteAllCookies();
   });
 
   test("search person by advanced_training description was nice", async function(assert) {
     assert.expect(6);
-
     await page.indexPage.visit();
 
     assert.equal(currentURL(), "/cv_search");
@@ -124,6 +124,7 @@ module("Acceptance | cv search", function(hooks) {
     assert.ok(foundIn.includes(t("person.advancedTrainings")));
     assert.notOk(names.includes("Bob Anderson"));
     assert.notOk(names.includes("ken"));
+    deleteAllCookies();
   });
 
   test("save cookie after input in search field was given", async function(assert) {
@@ -131,7 +132,6 @@ module("Acceptance | cv search", function(hooks) {
 
     //Reset Cookie and type value into search field
     await page.indexPage.visit();
-    document.cookie = undefined;
     await page.indexPage.searchInput("java");
     await triggerEvent("input", "keyup");
 
@@ -139,6 +139,7 @@ module("Acceptance | cv search", function(hooks) {
     await page.indexPage.visitPeople();
     await page.indexPage.visit();
     assert.equal(this.element.querySelector("input").value, "java");
+    deleteAllCookies();
   });
 
   test("cookie still works if another cookie is created", async function(assert) {
@@ -146,7 +147,6 @@ module("Acceptance | cv search", function(hooks) {
 
     //Reset Cookies and add two new ones: One manually and one which is created by search field
     await page.indexPage.visit();
-    document.cookie = undefined;
     document.cookie = "skills=nice";
     await page.indexPage.searchInput("ruby");
     await triggerEvent("input", "keyup");
@@ -155,5 +155,16 @@ module("Acceptance | cv search", function(hooks) {
     await page.indexPage.visitPeople();
     await page.indexPage.visit();
     assert.equal(this.element.querySelector("input").value, "ruby");
+    deleteAllCookies();
   });
+
+  function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
 });
