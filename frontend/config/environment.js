4f4f656171abfc2fb777321c82a00db31471597d
Yanick Minder <minder@puzzle.ch>
2023-05-09 14:13:29 +0200

    merge both person-form acceptance tests together and remove autoboot property

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -54,7 +54,6 @@ module.exports = function(environment) {
   if (environment === "test") {
     // Testem prefers this...
     ENV.locationType = "none";
-    ENV.APP.autoboot = false;
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
