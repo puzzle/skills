723ebcfbaafae1d098f3155b85d7fac065b246aa
Yanick Minder <minder@puzzle.ch>
2024-01-30 13:51:19 +0100

    add permitted attrs

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class PeopleController < ApplicationController
+  include ExportController
+  self.permitted_attrs = %i[birthdate location
+                            marital_status updated_by name nationality nationality2 title
+                            competence_notes company_id email department_id shortname]
 
+  self.nested_models = %i[advanced_trainings activities projects
+                          educations language_skills person_roles
+                          people_skills categories]
+
+  self.permitted_relationships = %i[person_roles people_skills]
   def index
     @people = Person.all
   end
