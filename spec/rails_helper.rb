27851728ea48d9ff0f7e54031e6283aa968d7403
Yanick Minder <minder@puzzle.ch>
2024-02-05 15:00:33 +0100

    restore old gemfile and clean up rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,9 +33,6 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.before(:each, type: :system) do
-    driven_by :headless_chrome
-  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
