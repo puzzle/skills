08b95aae42fffa89873fc0168a5a94d2ade380c7
Yanick Minder <minder@puzzle.ch>
2024-02-05 15:04:51 +0100

    clean up rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 require "active_record"
 require "bullet"
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
