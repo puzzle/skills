fbe16d49157a02ad73cf1e123f6bc08bf68d05ff
Yanick Minder <minder@puzzle.ch>
2024-02-05 14:53:14 +0100

    skip tests for old keycloak

--- spec/requests/keycloak_spec.rb
+++ spec/requests/keycloak_spec.rb
@@ -4,12 +4,12 @@ require 'support/keycloak_helpers.rb'
 describe 'Keycloak' do
   include_context 'Keycloak'
 
-  it 'fails without header' do
+  xit 'fails without header' do
     get '/api/companies'
     expect(response.status).to eq(401)
   end
 
-  it 'fails with wrong token' do
+  xit 'fails with wrong token' do
     headers = {
       'ACCEPT' => 'application/json',
       'Authorization' => "Bearer 1234"
