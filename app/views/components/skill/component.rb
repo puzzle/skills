130efec1f23f50f3b5a959be152a3340a063b5d4
Yanick Minder <minder@puzzle.ch>
2024-02-06 15:33:36 +0100

    component tests

--- app/views/components/skill/component.rb
+++ app/views/components/skill/component.rb
@@ -4,38 +4,29 @@ class Skill::Component < ApplicationViewComponent
   with_collection_parameter :skill
   renders_one :header, "HeaderComponent"
 
-  # def initialize(**args)
-  #   super(**args, :skill_iteration)
-  # end
+  class HeaderComponent < ViewComponent::Base
+    def call
+      content_tag(:table, class: 'table w-50') do
+        concat(content_tag(:thead) do
+          concat(content_tag(:tr) do
+            content_tag :div, content
+          end)
+        end)
 
-  def initialize(skill:, skill_iteration:)
-    @skill = skill
-    @iteration = skill_iteration
-  end
-
-  #   def haml_tag_if(condition, *args, &block)
-  #     if condition
-  #       puts *args
-  #       puts &block
-  #
-  #       # content_tag(:div, yield, &block.to_sym)
-  #       pry
-  #       tag.send(*args, &block)
-  #     else
-  #       yield
-  #     end
-  #   end
-  # end
-  # puts *args[0].to_sym
-  # puts block.display
-  # content_tag(*args[0].to_sym, '', eval(*args[1].to_s))
-    def haml_tag_if(condition, wrapper_tag, wrapper_options = {}, &block)
-      if condition
-        wrapper_content = capture_haml(&block)
-        content_tag(wrapper_tag, wrapper_content, wrapper_options)
-
-      else
-        capture_haml(&block)
+        concat(content_tag(:tbody) do
+          entries.each_with_index do |skill, i|
+            concat(content_tag(:tr) do
+              concat(content_tag(:th, 2, scope: 'row'))
+              concat(content_tag(:td, "tesasd"))
+              concat(content_tag(:td, skill.people.count))
+              concat(content_tag(:td, skill.category.title))
+              concat(content_tag(:td, skill.category.parent.title))
+              concat(content_tag(:td, skill.default_set))
+              concat(content_tag(:td, skill.radar))
+            end)
+          end
+        end)
       end
     end
+  end
 end
