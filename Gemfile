6247e976c17921c240e4dc1ae70b02e8ac30ad80
Yanick Minder <minder@puzzle.ch>
2024-02-06 09:07:08 +0100

    run rubocop

--- Gemfile
+++ Gemfile
@@ -74,5 +74,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-gem "view_component", "~> 3.10"
-gem "view_component-contrib", "~> 0.2.2"
+gem 'view_component', '~> 3.10'
+gem 'view_component-contrib', '~> 0.2.2'
--- lib/generators/view_component/view_component_generator.rb
+++ lib/generators/view_component/view_component_generator.rb
@@ -2,48 +2,52 @@
 
 # Based on https://github.com/github/view_component/blob/master/lib/rails/generators/component/component_generator.rb
 class ViewComponentGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path("templates", __dir__)
+  source_root File.expand_path('templates', __dir__)
 
   class_option :skip_test, type: :boolean, default: false
   class_option :skip_system_test, type: :boolean, default: false
   class_option :skip_preview, type: :boolean, default: false
 
-  argument :attributes, type: :array, default: [], banner: "attribute"
+  argument :attributes, type: :array, default: [], banner: 'attribute'
 
   def create_component_file
-    template "component.rb", File.join("app/views/components", class_path, file_name, "component.rb")
+    template 'component.rb',
+             File.join('app/views/components', class_path, file_name, 'component.rb')
   end
 
   def create_template_file
-    template "component.html.haml", File.join("app/views/components", class_path, file_name, "component.html.haml")
+    template 'component.html.haml',
+             File.join('app/views/components', class_path, file_name, 'component.html.haml')
   end
 
   def create_test_file
     return if options[:skip_test]
 
-    template "component_spec.rb", File.join("spec/views/components", class_path, "#{file_name}_spec.rb")
+    template 'component_spec.rb',
+             File.join('spec/views/components', class_path, "#{file_name}_spec.rb")
   end
 
   def create_system_test_file
     return if options[:skip_system_test]
 
-    template "component_system_spec.rb", File.join("spec/system/views/components", class_path, "#{file_name}_spec.rb")
+    template 'component_system_spec.rb',
+             File.join('spec/system/views/components', class_path, "#{file_name}_spec.rb")
   end
 
   def create_preview_file
     return if options[:skip_preview]
 
-    template "preview.rb", File.join("app/views/components", class_path, file_name, "preview.rb")
+    template 'preview.rb', File.join('app/views/components', class_path, file_name, 'preview.rb')
   end
 
   private
 
   def parent_class
-    "ApplicationViewComponent"
+    'ApplicationViewComponent'
   end
 
   def preview_parent_class
-    "ApplicationViewComponentPreview"
+    'ApplicationViewComponentPreview'
   end
 
   def initialize_signature
