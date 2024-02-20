7d49266cb995974f6107980edfa77286680a54fe
Jannik Pulfer <pulfer@puzzle.ch>
2024-02-20 09:53:40 +0100

    replace @entry with @person after rebase and remove nested_models and permitted_relationships from people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,15 +3,9 @@
 class PeopleController < CrudController
   include ExportController
 
-  # self.permitted_attrs = %i[birthdate location
-  #                           marital_status updated_by name nationality nationality2 title
-  #                           competence_notes company_id email department_id shortname]
-
-  # self.nested_models = %i[advanced_trainings activities projects
-  #                         educations language_skills person_roles
-  #                         people_skills categories]
-
-  # self.permitted_relationships = %i[person_roles people_skills]
+  self.permitted_attrs = %i[birthdate location
+                            marital_status updated_by name nationality nationality2 title
+                            competence_notes company_id email department_id shortname]
 
   def show
     if format_odt?
