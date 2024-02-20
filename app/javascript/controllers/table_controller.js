16f37e34fd731176e9eb51163fe9ea77db37e1e5
megli2 <egli@puzzle.ch>
2024-02-20 11:56:53 +0100

    use different approach to make table row editable

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -23,4 +23,39 @@ export default class extends Controller {
     this.saveButtonTarget.style.display = actionButtonDisplay;
     this.editButtonTarget.style.display = editButtonDisplay;
   }
+
+  saveData({params}) {
+    const skill = params.skill;
+    const url = "skills/" + skill.id;
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    let updateSkill = {
+      "title": this.rowTarget.cells[0].textContent,
+      "radar": skill.radar,
+      "portfolio": skill.portfolio,
+      "default_set": skill.default_set
+    }
+    // Define the request options
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify(updateSkill)
+    };
+    this.doRequest(options, url);
+  }
+
+  doRequest(options, url) {
+    // Send the request using Fetch API
+    fetch(url, options)
+        .then(response => {
+          console.log("Problem")
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();  // Parse response JSON
+        })
+  }
 }
