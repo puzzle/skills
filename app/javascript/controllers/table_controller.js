376f2b23ce758cb5882eaaca0245de4a994ca743
megli2 <egli@puzzle.ch>
2024-02-21 11:34:09 +0100

    use turbo-frame to edit row instead of bootstrap property

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -2,60 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="table"
 export default class extends Controller {
-  static targets = ["row", "editButton" ,"saveButton", "cancelButton"]
+  static targets = ["childSelect"];
   connect() {
 
   }
 
-  editRow() {
-    this.triggerButtonDisplay("none", "block");
-    this.rowTarget.contentEditable = "true";
-    this.rowTarget.focus();
-  }
-
-  cancelRow() {
-    this.triggerButtonDisplay("block", "none");
-    this.rowTarget.contentEditable = "false";
-  }
-
-  triggerButtonDisplay(editButtonDisplay, actionButtonDisplay) {
-    this.cancelButtonTarget.style.display = actionButtonDisplay;
-    this.saveButtonTarget.style.display = actionButtonDisplay;
-    this.editButtonTarget.style.display = editButtonDisplay;
-  }
-
-  saveData({params}) {
-    const skill = params.skill;
-    const url = "skills/" + skill.id;
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-    let updateSkill = {
-      "title": this.rowTarget.cells[0].textContent,
-      "radar": skill.radar,
-      "portfolio": skill.portfolio,
-      "default_set": skill.default_set
-    }
-    // Define the request options
-    const options = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        'X-CSRF-Token': csrfToken
-      },
-      body: JSON.stringify(updateSkill)
-    };
-    this.doRequest(options, url);
-  }
-
-  doRequest(options, url) {
-    // Send the request using Fetch API
-    fetch(url, options)
-        .then(response => {
-          console.log("Problem")
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();  // Parse response JSON
-        })
+  adjustChildren() {
+    console.log("Adjust options");
   }
 }
