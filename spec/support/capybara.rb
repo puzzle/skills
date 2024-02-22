195bb804a0b51a9fa76818d4d696faf3dfb34769
Yanick Minder <minder@puzzle.ch>
2024-02-22 10:31:07 +0100

    fix pipeline

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,33 +1,24 @@
 is_ci = ENV.fetch('IS_CI', false)
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[--headless --disable-gpu] },
-      'goog:loggingPrefs': {
-          browser: 'ALL'
-      }
-    )
-
-  options = ::Selenium::WebDriver::Chrome::Options.new
-
+  options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
-  options.add_argument('--window-size=1400,1400')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+
 
   Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      desired_capabilities: capabilities,
-      options: options
-    )
+    app,
+    browser: :chrome,
+    options: options
+  )
 end
-
-Capybara.default_driver = :headless_chrome
 
 RSpec.configure do |config|
   if is_ci
     config.before(:each, type: :feature) do
-      driven_by :headless_chrome
+      Capybara.current_driver = :headless_chrome
     end
   end
 end
