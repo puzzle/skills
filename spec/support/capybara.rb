b7cb796bfd952a3749eb917f72d006144bc2ef4b
Yanick Minder <minder@puzzle.ch>
2024-02-22 11:43:03 +0100

    fix pipeline

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,10 @@ is_ci = ENV.fetch('IS_CI', false)
 Capybara.register_driver :headless do |app|
   options = Selenium::WebDriver::Firefox::Options.new
   options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+
 
 
   Capybara::Selenium::Driver.new(
