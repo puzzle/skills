9505e566d60627c6de62ea1c29688f388aa2c115
Yanick Minder <minder@puzzle.ch>
2024-02-22 11:37:07 +0100

    fix pipeliebn

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,24 +1,24 @@
 is_ci = ENV.fetch('IS_CI', false)
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :headless do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
   options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--disable-dev-shm-usage')
 
 
   Capybara::Selenium::Driver.new(
     app,
-    browser: :chrome,
+    browser: :firefox,
     options: options
   )
 end
 
+Capybara.default_max_wait_time = 10
+
 RSpec.configure do |config|
-  if is_ci
+  if true
     config.before(:each, type: :feature) do
-      Capybara.current_driver = :headless_chrome
+      Capybara.default_driver = Capybara.javascript_driver = :headless
+      Capybara.current_driver = :headless
     end
   end
 end
