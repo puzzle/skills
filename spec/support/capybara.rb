3b8f448e115eb5a4b4222f2c56dbac4f90de7816
Yanick Minder <minder@puzzle.ch>
2024-02-22 14:50:09 +0100

    fix typo

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,7 @@
 is_ci = ENV.fetch('IS_CI', false)
 
 RSpec.configure do |config|
-  if id_ci
+  if is_ci
     config.before(:each, type: :feature) do
       Capybara.default_driver = Capybara.javascript_driver = :selenium_chrome_headless
       Capybara.current_driver = :selenium_chrome_headless
