35aee846b18e397ae9cf7d3cc1363e47711559bd
megli2 <egli@puzzle.ch>
2024-02-23 10:38:24 +0100

    implement selection change when category parent was selected

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -2,12 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="table"
 export default class extends Controller {
-  static targets = ["childSelect"];
-  connect() {
-
-  }
+  static targets = ["childSelect", "parentSelect"];
 
   adjustChildren() {
-    console.log("Adjust options");
+    fetch('api/categories/' + this.parentSelectTarget.value)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(response => {
+            //Clear select field
+            let length = this.childSelectTarget.options.length;
+            for(let i = length-1; i >= 0; i--) {
+                this.childSelectTarget.options[i] = null;
+            }
+
+            //Add new Options
+            const categoryArray = response.included;
+            for(let category of categoryArray) {
+                let opt = document.createElement("option");
+                opt.value = category.attributes.id;
+                opt.textContent = category.attributes.title;
+                this.childSelectTarget.append(opt);
+            }
+        })
+        .catch(error => {
+          console.error('Fetch error:', error);
+        });
   }
 }
