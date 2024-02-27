daa7aadcac62f7b25106ae43d1e657ad4bc0bf53
Yanick Minder <minder@puzzle.ch>
2024-02-27 15:57:07 +0100

    clean up pr

--- db/schema.rb
+++ db/schema.rb
@@ -136,13 +136,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_26_110212) do
     t.string "email"
     t.integer "department_id"
     t.string "shortname"
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["company_id"], name: "index_people_on_company_id"
-    t.index ["email"], name: "index_people_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true
   end
 
   create_table "people_skills", force: :cascade do |t|
