b98e535f75ade9fa637e00018ace96a422aa4289
Yanick Minder <minder@puzzle.ch>
2024-02-27 16:00:31 +0100

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_26_110212) do
+ActiveRecord::Schema[7.0].define(version: 2021_03_08_101205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
