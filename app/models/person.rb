db04371a03e7e78acb89767c636a0372ed233bcf
Yanick Minder <minder@puzzle.ch>
2024-02-29 12:45:31 +0100

    prepare to change omniauth lib

--- app/models/person.rb
+++ app/models/person.rb
@@ -98,7 +98,6 @@ class Person < ApplicationRecord
   class << self
     def from_omniauth(auth)
       roles = auth.extra.raw_info.pitc.roles
-      require 'pry'; binding.pry # rubocop:disable Style/Semicolon,Lint/Debugger
       where(email: auth.info.email).first_or_create do |person|
         person.email = auth.info.email
         person.name = auth.info.name
