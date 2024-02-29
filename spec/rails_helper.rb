8ffdfc5ecd81fba30bac5bf32b59ef0860d3f838
Yanick Minder <minder@puzzle.ch>
2024-02-29 09:32:50 +0100

    fix auth for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,10 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.include Warden::Test::Helpers
+  Warden.test_mode!
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
