0bd65fa904129dbefcfa4682772145e7633612e6
Jannik Pulfer <pulfer@puzzle.ch>
2024-03-01 16:05:35 +0100

    make image instantly update on file upload by using a stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,14 @@
 
 import { application } from "./application"
 
-import DropdownLinksController from "./dropdown_controller"
-application.register("dropdown", DropdownLinksController)
-import RemoteModalController from "./remote_modal_controller"
-application.register("remote-modal", RemoteModalController)
+import DropdownController from "./dropdown_controller"
+application.register("dropdown", DropdownController)
+
+import ImageUploadController from "./image_upload_controller"
+application.register("image-upload", ImageUploadController)
+
 import NationalityTwoController from "./nationality_two_controller"
 application.register("nationality-two", NationalityTwoController)
+
+import RemoteModalController from "./remote_modal_controller"
+application.register("remote-modal", RemoteModalController)
