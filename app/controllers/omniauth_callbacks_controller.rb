48be77fea1536cd32861a19c4d70bfe515786334
Yanick Minder <minder@puzzle.ch>
2024-03-01 10:16:17 +0100

    skip before action only for keycloak openid

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: :keycloak_openid
 
   def keycloak_openid
     omniauth_auth = request.env['omniauth.auth']
