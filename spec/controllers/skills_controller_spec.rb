f309af05d955e249779b37dca143594025286749
megli2 <egli@puzzle.ch>
2024-03-06 09:48:06 +0100

    fix text by adjusting name of query parameter and rework styles

--- spec/controllers/skills_controller_spec.rb
+++ spec/controllers/skills_controller_spec.rb
@@ -7,7 +7,7 @@ describe SkillsController do
     it 'should switch category' do
       edited_skill = {**Skill.first.as_json.symbolize_keys,
                       category_parent: Category.where(title: "System-Engineering")[0].id}
-      patch :update, params: {id: edited_skill[:id], skill: edited_skill, validateOnly: true}
+      patch :update, params: {id: edited_skill[:id], skill: edited_skill, validate_only: true}
       expect(response.body).to have_select("skill_category_id", with_options: [Category.where(title: "System-Engineering")[0].children[0].title])
     end
   end
