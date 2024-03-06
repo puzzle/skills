8b402ed0e9da1d678fdac7660ad52a995501c0b6
megli2 <egli@puzzle.ch>
2024-03-06 11:48:19 +0100

    rework tests by using different way to select option and adjusting ids

--- spec/features/skills_controller_spec.rb
+++ spec/features/skills_controller_spec.rb
@@ -3,11 +3,8 @@ require 'rails_helper'
 describe :skills do
   def fill_out_form
     fill_in 'skill_title', with: 'new Title'
-    parent_select = first('#skill_category_parent')
-    parent_select.select "System-Engineering"
-
-    child_select = page.all('#skill_category_id')
-    child_select.select "Linux-Engineering"
+    select 'System-Engineering', from: 'skill_category_parent'
+    select 'Linux-Engineering', from: 'skill_category_id'
 
     check 'skill_default_set'
     select 'adopt', from: 'skill_radar'
@@ -20,7 +17,7 @@ describe :skills do
       within "#skill_#{Skill.first.id}" do
         page.first('.edit-button').click
         expect(page).to have_field('skill_title', with: Skill.first.title)
-        expect(page).to have_select('skill_category_id', selected: Skill.first.category.parent.title)
+        expect(page).to have_select('skill_category_parent', selected: Skill.first.category.parent.title)
         expect(page).to have_select('skill_category_id', selected: Skill.first.category.title)
         expect(page).to have_field('skill_default_set', checked: true)
         expect(page).to have_select('skill_radar', options: Settings.radar, selected: Skill.first.radar)
@@ -32,11 +29,10 @@ describe :skills do
       visit skills_path
       page.first('.edit-button').click
       fill_out_form
-      within 'div.h-100.d-flex.justify-content-center.align-items-center' do
-        save_button = page.find("input[type='image']")
-        save_button.click
-      end
+      save_button = page.find("input[type='image']")
+      save_button.click
       expect(page).to have_css("div.row.border.border-top.table-light.tableform-hover.table-row", text: 'new Title', wait: 2)
+
       edited_skill = Skill.where(title: 'new Title')[0]
       expect(edited_skill.category.title).to eq('Linux-Engineering')
       expect(edited_skill.category.parent.title).to eq('System-Engineering')
@@ -48,12 +44,9 @@ describe :skills do
     it 'can cancel edited skill' do
       visit skills_path
       page.first('.edit-button').click
-      page.first('.edit-button').click
       fill_out_form
-      within 'div.h-100.d-flex.justify-content-center.align-items-center' do
-          save_button = page.find("img.pointer")
-          save_button.click
-      end
+      save_button = page.first("img.pointer")
+      save_button.click
 
       skill = Skill.first
       expect(skill.title).to eq('Rails')
