a9cae8ac8d4677c101dd373eaeb157ca91360c42
Jannik Pulfer <pulfer@puzzle.ch>
2024-03-12 10:39:36 +0100

    make people controller test also test if nationality is saved correctly

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -7,13 +7,17 @@ describe PeopleController do
 
     it 'should save nationality2 as nil if checkbox is unchecked and save it if it is checked' do
       expect(person["nationality2"]).not_to be(nil)
-
       put :update, params: {id: person["id"], person: person, has_nationality2: {checked: "0"}}
-
       response.code.should == "302"
       response.should redirect_to(person_path(person["id"]))
-
       expect(people(:bob).reload.nationality2).to be(nil)
+
+      edited_person = person
+      edited_person["nationality2"] = "DE"
+      put :update, params: {id: person["id"], person: edited_person, has_nationality2: {checked: "1"}}
+      response.code.should == "302"
+      response.should redirect_to(person_path(person["id"]))
+      expect(people(:bob).reload.nationality2).to eql("DE")
     end
   end
 end
