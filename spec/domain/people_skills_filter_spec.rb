0ad5134c6a9eae6a1a985fb5438279cb33b5940b
megli2 <egli@puzzle.ch>
2024-03-15 14:08:49 +0100

    finish test implementation

--- spec/domain/people_skills_filter_spec.rb
+++ spec/domain/people_skills_filter_spec.rb
@@ -9,9 +9,9 @@ describe PeopleSkillsFilter do
       levels = filteredPeopleSkills.pluck(:level)
       interests = filteredPeopleSkills.pluck(:interest)
 
-      expect(filteredPeopleSkills.count).to eq(14)
-      expect(levels).to eq([3, 1, 0, 1, 5, 4, 5, 5, 5, 5, 1, 1, 1, 1])
-      expect(interests).to eq([5, 4, 0, 2, 4, 5, 3, 2, 2, 4, 5, 3, 2, 4])
+      expect(filteredPeopleSkills.count).to eq(15)
+      expect(levels).to eq([3, 1, 0, nil, 1, 5, 4, 5, 5, 5, 5, 1, 1, 1, 1])
+      expect(interests).to eq([5, 4, 0, nil, 2, 4, 5, 3, 2, 2, 4, 5, 3, 2, 4])
     end
 
     it 'filters rated people skills' do
--- spec/features/people_show_spec.rb
+++ spec/features/people_show_spec.rb
@@ -6,11 +6,20 @@ describe 'People skills Show', type: :feature, js: true do
     visit root_path
   end
 
-  it 'Should render elements correctly' do
+  it 'Should only display valid amount of members' do
       visit skills_path
-      click_link('Bash')
-      expect(page).to have_text('Skill: Bash (1 Members)')
-      expect(page).to have_xpath("//input[@value=5]")
-      expect(page).to have_xpath("//input[@value=2]")
+      click_link('Rails')
+      expect(page).to have_text('Skill: Rails (5 Members)')
+      expect(page).to have_selector('.skill-row', count: 5)
+  end
+
+  it 'Should render elements correctly' do
+    visit skills_path
+    click_link('Bash')
+    expect(page).to have_text('Skill: Bash (1 Members)')
+    expect(page).to have_xpath("//input[@value=5]")
+    expect(page).to have_xpath("//input[@value=2]")
+    expect(page.first(".certificate")).to be_checked
+    expect(page.first(".core-competence")).not_to be_checked
   end
 end
