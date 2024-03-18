c97f235414ca169d099a4b674be1960df8751424
Yanick Minder <minder@puzzle.ch>
2024-03-18 12:01:21 +0100

    selects has the ability to set value null in db and gets displayed properly

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -6,9 +6,24 @@ export default class extends Controller {
 
   static targets = [ "hideable" ]
 
+  connect() {
+    this.hideableTargets.forEach((el) => {
+      el.querySelectorAll('select').forEach((select) => {
+        let is_active = /^true$/i.test(select.dataset.activeDefault);
+        select.disabled = is_active;
+        select.hidden = is_active;
+      });
+    });
+  }
+
   toggleTargets() {
     this.hideableTargets.forEach((el) => {
-      el.hidden = !el.hidden
+      el.hidden = !el.hidden;
+      
+      el.querySelectorAll('select').forEach((select) => {
+        select.hidden = !select.hidden;
+        select.disabled = !select.disabled;
+      });
     });
   }
 }
