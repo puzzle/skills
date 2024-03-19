01d41dcaac40815cee6b38520a229c93296748f0
megli2 <egli@puzzle.ch>
2024-03-19 10:44:11 +0100

    write feature specs

---
+++ spec/features/core_competences_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe "Core competences" do
+  before(:each) do
+    sign_in auth_users(:user), scope: :auth_user
+    visit root_path
+  end
+
+  it 'should display core competences' do
+    visit person_path(people(:bob))
+    expect(page).to have_selector('.core-competence', count: 1)
+    expect(page).to have_text('Software-Engineering')
+    expect(page).to have_text('Rails')
+  end
+
+  it 'should display competence notes and edit link correctly' do
+    visit person_path(people(:alice))
+    expect(page).to have_text('LaTex\n Puppet\n Bash')
+    expect(page).to have_selector('#edit-link')
+  end
+
+  it 'should update competence notes' do
+    visit person_path(people(:alice))
+    page.find('#edit-link').click
+    expect(page).to have_selector('.form-control')
+
+    fill_in 'competence_notes', with: 'Hello World here'
+    page.find('#save').click
+    expect(page).to have_text('Hello World here')
+  end
+
+  it 'should not update competence notes when clicking cancel button' do
+    visit person_path(people(:alice))
+    page.find('#edit-link').click
+    expect(page).to have_selector('.form-control')
+
+    fill_in 'competence_notes', with: 'Hello World here'
+    page.find('#cancel').click
+    expect(page).to have_text('LaTex\n Puppet\n Bash')
+  end
+end
