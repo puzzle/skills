4faccc3680caace363c16772344d0896f0a72be0
megli2 <egli@puzzle.ch>
2024-03-19 11:43:49 +0100

    use default functionality of crud controller to update competence notes

--- app/controllers/people/competence_notes_controller.rb
+++ app/controllers/people/competence_notes_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class People::CompetenceNotesController < CrudController
-  def update
-    @person.update!(competence_notes: params[:competence_notes])
-    render 'people/competence_notes/show'
-  end
 
-  def model_class
+  def self.model_class
     Person
   end
 end
