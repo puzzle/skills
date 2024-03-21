dcbdc6976f722b1e1c1dd853d91eff5570154fcb
Yanick Minder <minder@puzzle.ch>
2024-03-21 13:46:53 +0100

    clean up daterange picker

--- app/controllers/advanced_trainings_controller.rb
+++ app/controllers/advanced_trainings_controller.rb
@@ -4,6 +4,7 @@ class AdvancedTrainingsController < PersonRelationsController
   self.permitted_attrs = %i[description year_to month_to year_from month_from person_id]
   self.nilified_attrs_if_missing = [:year_to, :month_to]
 
+
   def create
     super(:location => person_path(person))
   end
@@ -14,13 +15,15 @@ class AdvancedTrainingsController < PersonRelationsController
 
   private
 
-  def entry
+  def entry # rubocop:disable Metrics/AbcSize
     relation = super
-    relation.month_from ||= 1
-    relation.year_from ||= Time.zone.today.year - 1
+    if relation.id.nil?
+      relation.month_from ||= 1
+      relation.year_from ||= Time.zone.today.year - 1
 
-    relation.month_to ||= Time.zone.today.month
-    relation.year_to ||= Time.zone.today.year
+      relation.month_to ||= Time.zone.today.month
+      relation.year_to ||= Time.zone.today.year
+    end
     relation
   end
 end
