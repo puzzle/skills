b59b354ae16f753bad0bf8e07f1dc5ca065af9a1
Yanick Minder <minder@puzzle.ch>
2024-03-21 10:23:35 +0100

    fix error with create

--- app/controllers/person_relations_controller.rb
+++ app/controllers/person_relations_controller.rb
@@ -3,14 +3,13 @@
 class PersonRelationsController < CrudController
 
   def fetch_entries
-    model_class.where(person_id: params['person_id'])
+    model_class.where(person_id: params[:person_id])
   end
 
   private
 
   def entry
-    relation = super
-    relation.person = Person.find(params['person_id'])
-    relation
+    instance_variable_set(:@person, Person.find(params[:person_id]))
+    super
   end
 end
