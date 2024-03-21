e79abbbc8cfbbb1a86abb72fdba7213465fb6f0b
Yanick Minder <minder@puzzle.ch>
2024-03-21 12:48:41 +0100

    add create funtionnality

--- app/controllers/person_relations_controller.rb
+++ app/controllers/person_relations_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class PersonRelationsController < CrudController
-
-  def fetch_entries
-    model_class.where(person_id: params[:person_id])
-  end
-
   private
 
   def entry
-    instance_variable_set(:@person, Person.find(params[:person_id]))
-    super
+    relation = super
+    relation.person = person
+    relation
+  end
+
+  def person
+    @person ||= Person.find(params[:person_id])
   end
 end
