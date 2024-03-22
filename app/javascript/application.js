f1257df02fb27b509f5eba05b51a3ed28a01e0e1
Robin Steiner <robin95@bluewin.ch>
2024-03-22 14:48:59 +0100

    Move puzzle-shell to cdn in turn stopping css breakage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,2 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "@puzzleitc/puzzle-shell";
-import "@puzzleitc/puzzle-shell/style.css";
--- spec/features/people_show_spec.rb
+++ spec/features/people_show_spec.rb
@@ -10,10 +10,8 @@ describe 'People skills Show', type: :feature, js: true do
       visit skills_path
       click_link('Rails')
 
-      # We used visible false in this test because the modal is not accessible in any different way
-      # follow-up ticket for this: #628
-      expect(page).to have_selector('.modal-title', visible: false, text: 'Skill: Rails (4 Members)')
-      expect(page).to have_selector('.skill-row', visible: false, count: 4)
+      expect(page).to have_selector('.modal-title', text: 'Skill: Rails (4 Members)')
+      expect(page).to have_selector('.skill-row', count: 4)
   end
 
   it 'Should render elements correctly' do
