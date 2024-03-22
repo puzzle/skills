2a5181f370b520ed97a1af16afdb3ebcc65e0be1
Yanick Minder <minder@puzzle.ch>
2024-03-22 11:53:05 +0100

    Ahhhhhh finally working \n add button to set same date

--- app/javascript/controllers/date_picker_controller.js
+++ app/javascript/controllers/date_picker_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="date-picker"
 export default class extends Controller {
 
-
   static targets = [ "hideable" ]
+  sameDate = false
 
   connect() {
     this.hideableTargets.forEach((el) => {
@@ -17,12 +17,34 @@ export default class extends Controller {
 
   toggleTargets() {
     this.hideableTargets.forEach((el) => {
+      if(this.sameDate){
+        this.sameDate = false;
+        el.querySelectorAll('select').forEach((select) => {
+          select.disabled = true;
+        });
+        return;
+      }
       el.hidden = !el.hidden;
-      
       el.querySelectorAll('select').forEach((select) => {
-        select.hidden = !select.hidden;
         select.disabled = !select.disabled;
       });
     });
+  }
+
+  sameDateToggle(){
+    this.sameDate = !this.sameDate;
+    let endDatePicker = document.querySelector("#end_date_picker")
+    endDatePicker.hidden = this.sameDate;
+    if (!this.sameDate) return;
+    endDatePicker.querySelectorAll('select').forEach((select) => {
+      select.disabled = false;
+    });
+  }
+
+  changeDate(event) {
+    if(this.sameDate){
+      let targetId = event.target.id.replace("from", "to");
+      document.querySelector(`#${targetId}`).value = event.target.value
+    }
   }
 }
