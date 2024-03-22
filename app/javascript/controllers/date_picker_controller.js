a35bdd803434fc351a602573920c4362a858f95b
Yanick Minder <minder@puzzle.ch>
2024-03-22 09:50:58 +0100

    rename visibility controller to date-range_controller

--- app/javascript/controllers/date_picker_controller.js
+++ app/javascript/controllers/date_picker_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="visibility"
+// Connects to data-controller="date-picker"
 export default class extends Controller {
 
 
