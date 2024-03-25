a6d06923b09773383d8f04cb27d61d1b6464178f
Yanick Minder <minder@puzzle.ch>
2024-03-25 10:36:56 +0100

    refactor

--- app/controllers/advanced_trainings_controller.rb
+++ app/controllers/advanced_trainings_controller.rb
@@ -4,15 +4,6 @@ class AdvancedTrainingsController < PersonRelationsController
   self.permitted_attrs = %i[description year_to month_to year_from month_from person_id]
   self.nilified_attrs_if_missing = [:year_to, :month_to]
 
-
-  def create
-    super(:location => person_path(person))
-  end
-
-  def update
-    super(:location => person_path(person))
-  end
-
   private
 
   def entry # rubocop:disable Metrics/AbcSize
