254e38c9706642d13b79afaf3adc155192d5d28d
megli2 <egli@puzzle.ch>
2024-03-25 16:33:08 +0100

    refactor code

--- app/exporters/odt/cv.rb
+++ app/exporters/odt/cv.rb
@@ -58,7 +58,17 @@ module Odt
     end
 
     def format_competences_list(sort_by_level, skills, grouped_by_level_skills)
-      sort_by_level ? grouped_by_level_skills.join("\r\n \r\n") : skills.pluck(:title).join(', ')
+      sort_by_level ? grouped_by_level_skills.join("\r\n") : skills.pluck(:title).join(', ')
+    end
+
+    def skills_by_level_string(grouped_people_skills_by_level)
+      grouped_people_skills_by_level.map do |key, value|
+        group_string= "#{stage_by_given_level(key)}: \r\n"
+        value.each do |people_skill|
+          group_string << "- #{people_skill.skill.title} \r\n"
+        end
+        group_string
+      end
     end
 
     def location
@@ -131,14 +141,12 @@ module Odt
         skills = Skill.joins(:category)
                       .where(categories: { parent_id: parent_c.id }, id: competences_ids)
 
-        # skills = skills.map {|skill| skill.people_skills.where(person_id: person.id).group_by {|ps| ps.level}}
-        grouped_by_level_skills = skills.map do |skill|
-          grouped_skills = skill.people_skills.where(person_id: person.id).group_by {|ps| ps.level}
-          grouped_skills.keys.map { |grouped_skill| "#{stage_by_given_level(grouped_skill)}: \r\n - #{skill.title}"}
-        end
+        grouped_people_skills_by_level= skills.map {|skill| skill.people_skills.find_by(person_id: person.id)}
+                                     .group_by(&:level)
+        mapped_string = skills_by_level_string(grouped_people_skills_by_level)
 
         next if skills.blank?
-        { category: parent_c.title, competence: format_competences_list(sort_by_level, skills, grouped_by_level_skills)}
+        { category: parent_c.title, competence: format_competences_list(sort_by_level, skills, mapped_string)}
       end.compact
     end
 
