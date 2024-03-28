12354d376d4030777725b86b265e44cfb560c6b2
Yanick Minder <minder@puzzle.ch>
2024-03-28 13:20:38 +0100

    merge

--- spec/domain/skills_filter_spec.rb
+++ spec/domain/skills_filter_spec.rb
@@ -16,15 +16,14 @@ describe SkillsFilter do
 
     it 'filters skills by category' do
       filter = SkillsFilter.new(Skill.all, 458108788, nil, nil)
-      filtered_skills = filter.scope
-
+      filtered_skills = filter.scope.pluck(:title)
       expect(filtered_skills.count).to eq(4)
-      expect(filtered_skills[0].title).to eq('Rails')
-      expect(filtered_skills[1].title).to eq('JUnit')
-      expect(filtered_skills[2].title).to eq('cunit')
-      expect(filtered_skills[3].title).to eq('ember')
+      expect(filtered_skills).to include('Rails')
+      expect(filtered_skills).to include('JUnit')
+      expect(filtered_skills).to include('cunit')
+      expect(filtered_skills).to include('ember')
     end
-    
+
     it 'filters skills by title' do
       filter = SkillsFilter.new(Skill.all, nil, 'Rails', nil)
       filtered_skills = filter.scope
@@ -32,7 +31,7 @@ describe SkillsFilter do
       expect(filtered_skills.count).to eq(1)
       expect(filtered_skills[0].title).to eq('Rails')
     end
-    
+
     it 'filters skills by default set' do
       filter = SkillsFilter.new(Skill.all, nil, nil, 'new')
       filtered_skills = filter.scope
@@ -40,7 +39,7 @@ describe SkillsFilter do
       expect(filtered_skills.count).to eq(1)
       expect(filtered_skills[0].title).to eq('Bash')
     end
-    
+
     it 'filters skills by category, default set and title' do
       filter = SkillsFilter.new(Skill.all, 458108788, 'u', 'false')
       filtered_skills = filter.scope
