52b2ba9b7736dbf66a2efb93747890eb0618e0eb
megli2 <egli@puzzle.ch>
2024-03-28 15:19:32 +0100

    refactor logic to work with turbo

--- app/controllers/people/people_skills_controller.rb
+++ app/controllers/people/people_skills_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
-class PeopleSkillsController < CrudController
+class People::PeopleSkillsController < CrudController
   include ParamConverters
   self.permitted_attrs = [:level, :interest, :certificate, :core_competence]
+
+  def index
+    @person = Person.find(params[:id])
+    super
+  end
 
 end
