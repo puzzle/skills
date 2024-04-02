9b04a828d4259c2c24f275e9712a06136c9d24d2
Yanick Minder <minder@puzzle.ch>
2024-04-02 13:55:41 +0200

    Clean up

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -3,12 +3,19 @@
 module DateHelper
   def date_range_label(date_range)
     now = "#{months[date_range.month_from || 0]} #{date_range.year_from} "
-    now += "- #{t('date_range_picker.today')}" if date_range&.till_today?
-    now += "- #{months[date_range&.month_to || 0]} #{date_range.year_to}" unless date_range&.till_today? # rubocop:disable Layout/LineLength
+    now += date_range&.till_today? ? "- #{today_label}" : date_range_end_label(date_range)
     now
   end
 
   def months
     t('date.month_names')
+  end
+
+  def today_label
+    t('date_range_picker.today')
+  end
+
+  def date_range_end_label(date_range)
+    "- #{months[date_range&.month_to || 0]} #{date_range.year_to}"
   end
 end
