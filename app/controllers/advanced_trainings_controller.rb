7ca897178d21b66abc5891293a34f243be50a0be
Yanick Minder <minder@puzzle.ch>
2024-04-02 10:51:32 +0200

    Clean up

--- app/controllers/advanced_trainings_controller.rb
+++ app/controllers/advanced_trainings_controller.rb
@@ -2,6 +2,4 @@
 
 class AdvancedTrainingsController < PersonRelationsController
   self.permitted_attrs = %i[description year_to month_to year_from month_from person_id]
-  self.nilified_attrs_if_missing = [:year_to, :month_to]
-
 end
