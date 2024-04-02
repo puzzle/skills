02dfbeb0f2a66303d86a65651231e6086fccd18c
Yanick Minder <minder@puzzle.ch>
2024-04-02 15:44:15 +0200

    clean up

--- app/helpers/select_helper.rb
+++ app/helpers/select_helper.rb
@@ -8,14 +8,15 @@ module SelectHelper
   end
 
   def months_with_nil
-    months.each_with_index.map do |month, index|
-      [month || '-', index.zero? ? nil : index]
-    end
+    month_list = months.compact.each_with_index.map { |month, index| [month, index + 1] }
+    add_default_option(month_list, { text: '-' })
   end
 
-  def last_100_years_disabled_hidden_nil
-    years = (100.years.ago.year..Time.zone.today.year).to_a
-    years.unshift(['', { value: nil, hidden: true }])
-    years
+  def last_100_years
+    (100.years.ago.year..Time.zone.today.year).to_a
+  end
+
+  def add_default_option(collection, option = {})
+    collection.unshift([option[:text], option[:value], option])
   end
 end
