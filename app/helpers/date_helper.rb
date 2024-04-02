eb5084800e554860662b18a1c05fabc156e16ffd
Yanick Minder <minder@puzzle.ch>
2024-04-02 16:12:53 +0200

    refactor

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -12,11 +12,11 @@ module DateHelper
 
   def date_range_end_label(model_with_dates)
     if model_with_dates&.till_today?
-      "- #{t('date_range_picker.today')}"
-    elsif model_with_dates.same_year? && model_with_dates.same_month?
-      ''
-    else
-      "- #{months[model_with_dates&.month_to || 0]} #{model_with_dates&.year_to}"
+      return "- #{t('date_range_picker.today')}"
+    elsif !(model_with_dates.same_year? && model_with_dates.same_month?)
+      return "- #{months[model_with_dates&.month_to || 0]} #{model_with_dates&.year_to}"
     end
+
+    ''
   end
 end
