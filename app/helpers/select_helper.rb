653877e55ba31c3517e01dd201d8fc122a4db473
Yanick Minder <minder@puzzle.ch>
2024-04-02 09:47:05 +0200

    Clean up mess

--- app/helpers/select_helper.rb
+++ app/helpers/select_helper.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
 module SelectHelper
-  def generate_select_options_with_default(person)
-    selected = person ? person.id : ''
-    prompt = person ? false : true
+  def select_when_availabale(obj)
+    selected = obj ? obj.id : ''
+    prompt = obj ? false : true
     { :selected => selected, :prompt => prompt, :disabled => '' }
+  end
+
+  def months_with_nil
+    months = translated_months.compact_blank.each_with_index.map do |month, index|
+      [month, index + 1]
+    end
+    months.unshift(['-', nil])
+    months
+  end
+
+  def last_100_years_disabled_hidden_nil
+    years = (100.years.ago.year..Time.zone.today.year).to_a
+    years.unshift(['', { value: nil, hidden: true }])
+    years
   end
 end
--- app/javascript/controllers/date_picker_controller.js
+++ app/javascript/controllers/date_picker_controller.js
@@ -4,51 +4,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = [ "hideable" ]
-  sameDate = false
 
   connect() {
-    this.hideableTargets.forEach((el) => {
-      el.querySelectorAll('select').forEach((select) => {
-        select.disabled = el.hidden;
-        select.hidden = el.hidden;
-      });
-    });
   }
 
   toggleTargets() {
     this.hideableTargets.forEach((el) => {
-      if(this.sameDate){
-        this.sameDate = false;
-        this.setSelects(el, "disabled", true);
-        return;
-      }
       el.hidden = !el.hidden;
-      el.querySelectorAll('select').forEach((select) => {
-        select.disabled = !select.disabled;
-        select.hidden = !select.hidden;
-      });
+      this.setSelects(el, "value", "");
     });
   }
 
-  sameDateToggle(){
-    this.sameDate = !this.sameDate;
-    let endDatePicker = document.querySelector("#end_date_picker")
-    endDatePicker.hidden = this.sameDate;
-    if (!this.sameDate) return;
-    this.setSelects(endDatePicker, "disabled", false);
-    this.setSelects(endDatePicker, "hidden", false);
-  }
 
-
-  // should be working 
-  changeDate(event) {
-    if(this.sameDate){
-      let targetId = event.target.id.replace("from", "to");
-      document.querySelector(`#${targetId}`).value = event.target.value
-    }
-  }
-
-  // should be working 
   setSelects(parent, property, value){
     parent.querySelectorAll('select').forEach((select) => {
       select[property] = value;
