0bd61b441bc732f99e499b4c10988e4f592aaae8
Manuel <moeri@puzzle.ch>
2024-04-05 09:53:37 +0200

    Implement first version of search

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -4,7 +4,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {}
 
-    handleChange(event) {
-        window.location.href = event.target.dataset.value + event.target.value;
-    }
+  filterOptions(event) {
+    const searchText = event.target.value.toLowerCase();
+    const options = this.element.querySelectorAll(".form-select option");
+    options.forEach(option => {
+      const optionText = option.textContent.toLowerCase();
+      if (optionText.includes(searchText) && searchText.length >= 2) {
+          option.style.display = "block";
+          console.log("Option: " + optionText);
+      } else {
+          option.style.display = "none";
+      }
+    });
+  }
+
+  handleChange(event) {
+      window.location.href = event.target.dataset.value + event.target.value;
+  }
 }
