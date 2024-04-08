3e57d2982d6dbdfa906cb0af7f5227d424e38cb6
megli2 <egli@puzzle.ch>
2024-04-08 13:43:22 +0200

    delete unnecessary ressources

--- app/controllers/people_skills_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class PeopleSkillsController < CrudController
-  include ParamConverters
-  self.permitted_attrs = [:level, :interest, :certificate, :core_competence]
-end
