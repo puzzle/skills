5cf88b71a89717f166838c7e5d19b0ea7fcbbff3
megli2 <egli@puzzle.ch>
2024-04-09 14:07:47 +0200

    beginn with people_skill implementation

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,14 @@ Rails.application.routes.draw do
       delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_auth_user_session
     end
 
+  resources :people do
+    resources :advanced_trainings
+  end
+
+  resources :skills
+  resources :people_skills
+
+
   # Status
   scope 'status' do
     get 'health', to: 'status#health'
