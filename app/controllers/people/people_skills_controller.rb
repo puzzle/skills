bda54551b5416282dfa0c3787dda4f9c5c931b5c
megli2 <egli@puzzle.ch>
2024-04-09 10:10:50 +0200

    implement before action to set person

--- app/controllers/people/people_skills_controller.rb
+++ app/controllers/people/people_skills_controller.rb
@@ -5,16 +5,7 @@ class People::PeopleSkillsController < CrudController
   self.permitted_attrs = [{ people_skills_attributes: [:id, :certificate, :level,
                                                        :interest, :core_competence, :_destroy] }]
   helper_method :people_skills_of_category
-
-  def index
-    @person = Person.find(params[:id])
-    super
-  end
-
-  def edit
-    @person = Person.find(params[:id])
-    super
-  end
+  before_action :set_person
 
   def self.model_class
     Person
@@ -28,6 +19,10 @@ class People::PeopleSkillsController < CrudController
 
   def people_skills_of_category(category)
     @person.people_skills.where(skill_id: category.skills.pluck(:id))
+  end
+
+  def set_person
+    @person = Person.find(params[:id])
   end
 
 end
