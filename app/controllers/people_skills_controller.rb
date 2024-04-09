228539c14d17dae114d2d2d81d4a8dbad4275e92
megli2 <egli@puzzle.ch>
2024-04-09 14:49:45 +0200

    implement template to search for people with skills

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -7,20 +7,18 @@ class PeopleSkillsController < CrudController
   def entries
     return [] if params[:skill_id].blank?
 
+  def fetch_entries
     base = PeopleSkill.includes(:person, skill: [
-                                  :category,
-                                  :people, { people_skills: :person }
-                                ])
-    PeopleSkillsFilter.new(
-      base, true, params[:level], params[:interest], params[:skill_id]
+      :category,
+      :people, { people_skills: :person }
+    ])
+    people_skills = PeopleSkillsFilter.new(
+      base, params[:rated], params[:level], params[:interest], params[:skill_id]
     ).scope
-  end
+    if params.key?(:person_id)
+      return people_skills.where(person_id: params[:person_id])
+    end
 
-  def search_level
-    params[:level] ? params[:level].to_i : 1
-  end
-
-  def search_interest
-    params[:interest] ? params[:interest].to_i : 1
+    people_skills
   end
 end
