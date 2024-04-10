02d9c40c358d2fe530f78b9f2874acb2f20a2ad6
megli2 <egli@puzzle.ch>
2024-04-10 11:40:28 +0200

    use helper methods to retrieve level and interest instead of instance variables

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -2,16 +2,10 @@
 
 class PeopleSkillsController < CrudController
   include ParamConverters
-  helper_method :filtered_entries
-
-  def index
-    @level = params[:level] ? Integer(params[:level]) : 1
-    @interest = params[:interest] ? Integer(params[:interest]) : 1
-    super
-  end
+  helper_method :filtered_entries, :search_level, :search_interest
 
   def filtered_entries
-    return [] if params[:skill_id].nil?
+    return [] if params[:skill_id].nil? || params[:skill_id] == ""
 
     base = PeopleSkill.includes(:person, skill: [
                                   :category,
@@ -20,5 +14,13 @@ class PeopleSkillsController < CrudController
     PeopleSkillsFilter.new(
       base, true, params[:level], params[:interest], params[:skill_id]
     ).scope
+  end
+
+  def search_level
+    params[:level] ? Integer(params[:level]) : 1
+  end
+
+  def search_interest
+    params[:interest] ? Integer(params[:interest]) : 1
   end
 end
