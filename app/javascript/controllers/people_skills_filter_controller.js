91ec16e863dbf275ebd16a79a7e0ae121f34bc03
megli2 <egli@puzzle.ch>
2024-04-15 10:39:15 +0200

    finish template for multi-search

--- app/javascript/controllers/people_skills_filter_controller.js
+++ app/javascript/controllers/people_skills_filter_controller.js
@@ -3,22 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="people-skills-filter"
 export default class extends Controller {
     static targets = ["filter", "add"]
-    filterCounter = 3;
 
     connect() {
-        console.log("Fuck my life")
+        for(let target of this.filterTargets) {
+            target.style.display = "none";
+        }
     }
 
     hide({params}) {
         this.filterTargets[params.id - 1].style.display = "none";
-        this.filterCounter--;
+        this.addTarget.style.display = "flex";
     }
 
     show() {
-        this.filterTargets[this.filterCounter].style.display = "block";
-        this.filterCounter++;
-        if(this.filterCounter === 3) {
-            this.addTarget.style.display = "none";
+        for(let target of this.filterTargets) {
+            if(target.style.display === "none") {
+                target.style.display = "block";
+                if(target === this.filterTargets[this.filterTargets.length - 1]) {
+                    this.addTarget.style.display = "none";
+                }
+                return;
+            }
         }
     }
 }
