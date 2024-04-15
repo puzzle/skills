22be4a39b84cd8939f227f2cb1bca53f07767924
megli2 <egli@puzzle.ch>
2024-04-15 09:12:20 +0200

    implement basic concept for show and hide

--- app/javascript/controllers/people_skills_filter_controller.js
+++ app/javascript/controllers/people_skills_filter_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="people-skills-filter"
 export default class extends Controller {
-    static targets = ["filter"]
+    static targets = ["filter", "add"]
+    filterCounter = 3;
 
     connect() {
         console.log("Fuck my life")
@@ -10,5 +11,14 @@ export default class extends Controller {
 
     hide({params}) {
         this.filterTargets[params.id - 1].style.display = "none";
+        this.filterCounter--;
+    }
+
+    show() {
+        this.filterTargets[this.filterCounter].style.display = "block";
+        this.filterCounter++;
+        if(this.filterCounter === 3) {
+            this.addTarget.style.display = "none";
+        }
     }
 }
