7cef678c6d2c9e89f2c9c3d82a7f7c9de298f43f
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-16 13:29:50 +0200

    Replace hard coded date formating with date range label helper and remove unneeded self.nesting from controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,5 +2,4 @@
 
 class ActivitiesController < People::PersonRelationsController
   self.permitted_attrs = %i[description role month_to year_to month_from year_from person_id]
-  self.nesting = Person
 end
