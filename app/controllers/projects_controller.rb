129d68147948ea6e9778b4a4c28dd661e80c708a
Yanick Minder <minder@puzzle.ch>
2024-04-17 10:32:59 +0200

    add default structure

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ProjectsController < PersonRelationsController
+  self.permitted_attrs = %i[description title role technology
+                            month_from year_from month_to year_to person_id]
+
+  self.nested_models = %i[project_technologies]
+end
