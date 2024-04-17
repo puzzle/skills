e89408c088d7e6ad212c7d0ce4505dbce2049621
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-17 10:22:14 +0200

    Revert unwanted changes in capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,10 @@
 is_ci = ENV.fetch('IS_CI', false)
 
 RSpec.configure do |config|
-  if true
+  if is_ci
     config.before(:each, type: :feature) do
-      Capybara.default_driver = Capybara.javascript_driver = :selenium
-      Capybara.current_driver = :selenium
+      Capybara.default_driver = Capybara.javascript_driver = :selenium_chrome_headless
+      Capybara.current_driver = :selenium_chrome_headless
     end
   end
 end
