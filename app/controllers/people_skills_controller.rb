c646c336b4dd9e603dd0ce3b095c45df35ea44cb
megli2 <egli@puzzle.ch>
2024-04-18 13:43:55 +0200

    make rubocop happy

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -7,13 +7,15 @@ class PeopleSkillsController < CrudController
 
   def entries
     return [] if params[:skill_id].blank?
+
     extracted_queries = extract_query_params
     base = PeopleSkill.includes(:person, skill: [
                                   :category,
                                   :people, { people_skills: :person }
                                 ])
     PeopleSkillsFilter.new(
-      base, true, extracted_queries[:levels], extracted_queries[:interests], extracted_queries[:skill_ids]
+      base, true, extracted_queries[:levels], extracted_queries[:interests],
+      extracted_queries[:skill_ids]
     ).scope
   end
 
@@ -21,14 +23,17 @@ class PeopleSkillsController < CrudController
     params[:skill_id].present? ? params[:skill_id].length : 1
   end
 
+  # rubocop:disable Metrics/AbcSize
   def extract_query_params
-    query_hash = {:skill_ids => [], :levels => [], :interests => []}
+    query_hash = { :skill_ids => [], :levels => [], :interests => [] }
     params[:skill_id].map.with_index do |skill, index|
       next unless skill != ''
+
       query_hash[:skill_ids] << skill.to_i
       query_hash[:levels] << params[:level][index].to_i
       query_hash[:interests] << params[:interest].values[index].to_i
     end
     query_hash
   end
+  # rubocop:enable Metrics/AbcSize
 end
