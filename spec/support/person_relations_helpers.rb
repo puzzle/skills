36be196c3a3352e846da307b33c1835a201479de
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-18 09:01:14 +0200

    Rename method in person relations helpers to match naming conventions

--- spec/support/person_relations_helpers.rb
+++ spec/support/person_relations_helpers.rb
@@ -2,7 +2,7 @@ module PersonRelationsHelpers
   #obj_class -> The class of the specific person_relation you want to open the create form for
   def open_create_form(obj_class)
     element_id = dom_id(obj_class.new)
-    link = findByTurboFrameId(element_id)
+    link = find_by_turbo_frame_id(element_id)
     scroll_to(link, align: :center)
     link.click
     n_children = find("##{element_id}").all("*").count
@@ -12,14 +12,14 @@ module PersonRelationsHelpers
   #element -> The element for which you want to open the edit form (e.g. person.activities.first)
   def open_edit_form(element)
     element_id = dom_id(element)
-    html_element = findByTurboFrameId(element_id)
+    html_element = find_by_turbo_frame_id(element_id)
     scroll_to(html_element, align: :center)
     html_element.click
     n_children = find("##{element_id}").all("*").count
     expect(n_children).to be > 0
   end
 
-  def findByTurboFrameId(id)
+  def find_by_turbo_frame_id(id)
     find("a[data-turbo-frame='#{id}']")
   end
 end
