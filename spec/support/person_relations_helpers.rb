be6ce5a66b14169ace321c0e519d285de6b80baf
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-18 07:54:44 +0200

    Rewrite comments in person_relations_helper

--- spec/support/person_relations_helpers.rb
+++ spec/support/person_relations_helpers.rb
@@ -1,6 +1,5 @@
 module PersonRelationsHelpers
-  #link_name -> The link text that is displayed on the view
-  #form_field_id -> The id of an element that is only in the form (used to determine when the form has finished loading)
+  #obj_class -> The class of the specific person_relation you want to open the create form for
   def open_create_form(obj_class)
     element_id = dom_id(obj_class.new)
     link = findByTurboFrameId(element_id)
@@ -10,6 +9,7 @@ module PersonRelationsHelpers
     expect(n_children).to be > 0
   end
 
+  #element -> The element for which you want to open the edit form (e.g. person.activities.first)
   def open_edit_form(element)
     element_id = dom_id(element)
     html_element = findByTurboFrameId(element_id)
