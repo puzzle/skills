3fcbac9d63cfbae1c01fc047b8f20860eee82ec2
megli2 <egli@puzzle.ch>
2024-04-18 10:48:37 +0200

    implement stimulus controller to remove row from form

--- app/javascript/controllers/people_skills_filter_controller.js
+++ app/javascript/controllers/people_skills_filter_controller.js
@@ -2,32 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="people-skills-filter"
 export default class extends Controller {
-    static targets = ["filter", "add"]
+    static targets = ["filter", "form"]
 
-    connect() {
-        for(let target of this.filterTargets) {
-            target.style.display = "none";
-        }
-    }
-
-    hide({params}) {
-        this.filterTargets[params.id].style.display = "none";
-        this.addTarget.style.display = "flex";
-    }
-
-    show() {
-        for(let target of this.filterTargets) {
-            if(target.style.display === "none") {
-                target.style.display = "block";
-                if(this.countHidden() === this.filterTargets.length) {
-                    this.addTarget.style.display = "none";
-                }
-                return;
-            }
-        }
-    }
-
-    countHidden() {
-        return this.filterTargets.filter(x => x.style.display === "block").length;
+    remove({params}) {
+        this.filterTargets[params.id].remove();
+        this.formTarget.submit();
     }
 }
