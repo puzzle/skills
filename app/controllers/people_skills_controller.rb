fe339a0a3b0d0a801e4ba50fb724b8e8d16f24cb
megli2 <egli@puzzle.ch>
2024-04-18 08:23:04 +0200

    implement method to add query params manually to url

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -3,7 +3,7 @@
 class PeopleSkillsController < CrudController
   include ParamConverters
 
-  helper_method :search_skill, :search_level, :search_interest, :row_count
+  helper_method :search_skill, :search_level, :search_interest, :row_count, :query_params
 
   def entries
     return [] if params[:skill_id].blank?
@@ -31,11 +31,32 @@ class PeopleSkillsController < CrudController
   end
 
   def row_count
+    query_params
     params[:skill_id].present? ? params[:skill_id].length : 1
   end
 
   def query_params
-    params[:skill_id].to_s
+    skill_ids = map_array_to_query("skill_id")
+    levels = map_array_to_query("level")
+    interests = map_hash_to_query("interest")
+    return "" if skill_ids.nil?
+    (skill_ids + levels + interests).join("&")
+  end
+
+  def map_array_to_query(query_name)
+    if params["#{query_name}"].present?
+      params["#{query_name}"].map do |skill|
+        "#{query_name}[]=#{skill}"
+      end
+    end
+  end
+
+  def map_hash_to_query(query_name)
+    if params["#{query_name}"].present?
+      params["#{query_name}"].values.map.with_index do |skill, index|
+        "#{query_name}[#{index}]=#{skill}"
+      end
+    end
   end
 
 end
