179079948a6def045996a783060e24eb62200cb9
megli2 <egli@puzzle.ch>
2024-04-19 16:23:34 +0200

    remove waits and add additional feature test

--- spec/features/people_skill_search_spec.rb
+++ spec/features/people_skill_search_spec.rb
@@ -48,7 +48,7 @@ describe :people_skills do
       expect(page).to have_selector("#filter-row-4")
 
       # set skills in filters
-      page.all('select', wait: 5)[0].select('JUnit')
+      page.all('select')[0].select('JUnit')
       page.all('select')[1].select('Rails')
       page.all('select')[2].select('ember')
       page.all('select')[3].select('Bash')
@@ -78,7 +78,7 @@ describe :people_skills do
       expect(page).to have_selector("#filter-row-4")
 
       # set skills in filters
-      page.all('select', wait: 5)[0].select('JUnit')
+      page.all('select')[0].select('JUnit')
       page.all('select')[1].select('Rails')
       page.all('select')[2].select('ember')
       page.all('select')[3].select('Bash')
@@ -87,6 +87,30 @@ describe :people_skills do
       # set level in filters
       page.find('#row4_star-label5').click(x: 10, y: 10)
       expect(page).to have_text("Keine Resultate")
+    end
+
+    it 'Should be able to remove filter row and switch results accordingly' do
+      visit(people_skills_path)
+      page.find('#add-row-button').click
+      expect(page).to have_selector("#filter-row-1")
+
+      # set skills in filters
+      page.all('select')[0].select('ember')
+      expect(page).to have_text("Alice Mante")
+      expect(page).to have_text("Wally Allround")
+      expect(page).to have_text("Hope Sunday")
+
+      # add skill filter
+      page.all('select')[1].select('cunit')
+      expect(page).to have_text("Wally Allround")
+      expect(page).to have_text("Hope Sunday")
+      expect(page).to_not have_text("Alice Mante")
+
+      # remove skill filter
+      page.find('#remove-row-1').click
+      expect(page).to have_text("Alice Mante")
+      expect(page).to have_text("Wally Allround")
+      expect(page).to have_text("Hope Sunday")
     end
 
   end
