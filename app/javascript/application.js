bed9471f5536d2f5754c520a15722107a51133a2
Manuel <moeri@puzzle.ch>
2024-04-19 15:20:36 +0200

    Implement dropdown with choices.js and fix layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "choices"
+
--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import Choices from 'choices.js';
 
 // Connects to data-controller="dropdown-links"
 export default class extends Controller {
-  connect() {}
+  connect() {
+    const element = document.getElementById('dropdown');
+    const choices = new Choices(element, {});
+  }
 
-    handleChange(event) {
-        window.location.href = event.target.dataset.value + event.target.value;
-    }
+  handleChange(event) {
+      window.location.href = event.target.dataset.value + event.target.value;
+  }
 }
