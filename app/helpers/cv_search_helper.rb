c9c2f736a96edc02d3ff2ff0313089499565c937
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-23 13:59:52 +0200

    Move logic that translates the found_in value and the one that creates a person link with search query from cv search view to a new helper

---
+++ app/helpers/cv_search_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module CvSearchHelper
+  def translate_found_in(result)
+    I18n.t("cv_search.#{result[:found_in].split("#")[0].underscore}")
+  end
+
+  def person_path_with_query(result)
+    "#{person_path(result[:person][:id])}?q=#{params[:q]}"
+  end
+end
