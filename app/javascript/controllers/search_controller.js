9c22ab5c5259916e0a4d2ed9337264b1277ac8a8
Jannik Pulfer <pulfer@puzzle.ch>
2024-04-23 14:15:11 +0200

    Populate value from query-params in cv search field on reload

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,6 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+    connect() {
+        const params = new URLSearchParams(window.location.search);
+        if(params.has("q")) {
+            document.getElementById("cv_search_field").value = params.get("q");
+        }
+    }
+
     timeout;
     submitWithTimeout(e) {
         const form = e.target.parentElement;
