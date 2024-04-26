53c88f26c6f67700db119ed35b70e94d0472ea0f
megli2 <egli@puzzle.ch>
2024-04-26 08:56:27 +0200

    cleanup code by using params class to extract params

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -11,39 +11,25 @@ class PeopleSkillsController < CrudController
     super
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def entries
-    return [] if params[:skill_id].blank?
+    return [] if @converted_params.skills.empty?
 
-    extracted_queries = extract_query_params
-    return [] if extracted_queries[:skill_ids].empty?
+    skills = @converted_params.skills.select { |skill| skill.present? }.map(&:to_i)
+    levels = @converted_params.levels.take(skills.length).map(&:to_i)
+    interests = @converted_params.interests.take(skills.length).map(&:to_i)
 
     base = PeopleSkill.includes(:person, skill: [
                                   :category,
                                   :people, { people_skills: :person }
                                 ])
     PeopleSkillsFilter.new(
-      base, true, extracted_queries[:levels], extracted_queries[:interests],
-      extracted_queries[:skill_ids]
+      base, true, levels, interests, skills
     ).scope
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
   def row_count
     params[:skill_id].present? ? params[:skill_id].length : 1
   end
-
-  # rubocop:disable Metrics/AbcSize
-  def extract_query_params
-    query_hash = { :skill_ids => [], :levels => [], :interests => [] }
-    params[:skill_id].map.with_index do |skill, index|
-      next unless skill != ''
-
-      query_hash[:skill_ids] << skill.to_i
-      query_hash[:levels] << params[:level][index].to_i
-      query_hash[:interests] << params[:interest].values[index].to_i
-    end
-    query_hash
-  end
-  # rubocop:enable Metrics/AbcSize
 end
