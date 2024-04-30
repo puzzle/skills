d77066f77455c0c617204b1e0f96c7e4621a6a6d
megli2 <egli@puzzle.ch>
2024-04-30 10:55:00 +0200

    implement scroll behaviour

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -2,12 +2,33 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-    connect() {
-        console.log("Connected with scroll")
-    }
+    static targets = ["listItem", "scrollItem"]
+    currentSelectedIndex = 0;
 
     scrollToElement({params}) {
-        let element = document.getElementById(params.id);
-        element.scrollIntoView();
+        document.getElementById(params.id).scrollIntoView();
+    }
+
+    scrollEvent() {
+        for(let i = 0; i < this.scrollItemTargets.length; i++) {
+            if(this.isElementInViewport(this.scrollItemTargets[i])) {
+                if(this.currentSelectedIndex !== i) {
+                    this.listItemTargets[i].classList.add("skills-selected");
+                    this.listItemTargets[this.currentSelectedIndex].classList.remove("skills-selected");
+                    this.currentSelectedIndex = i;
+                }
+                break;
+            }
+        }
+    }
+
+    isElementInViewport(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
 }
