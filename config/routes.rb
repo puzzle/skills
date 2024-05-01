fdde4ea98fc0cf25a4ecbf169522f18942d80be1
megli2 <egli@puzzle.ch>
2024-05-01 09:43:19 +0200

    change name of route

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :skills
   resources :people_skills do
     collection do
-      get 'filter', to: "people_skills/filter_form#index"
+      get 'filter_form', to: "people_skills/filter_form#index"
     end
   end
 
