72e854e481ebb8bb0a0d11eedce0c88b1ce87f44
Yanick Minder <minder@puzzle.ch>
2024-05-03 14:38:09 +0200

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :find_profile_by_keycloak_user
 
-  # def authenticate_auth_user!
-  #   require 'pry'; binding.pry
-  #   return super unless helpers.development?
+  def authenticate_auth_user!
+    super
 
-  #   admin = AuthUser.find_by(email: 'admin@skills.ch')
-  #   request.env['warden'].set_user(admin, :scope => :auth_user)
-  # end
+    # admin = AuthUser.find_by(email: 'admin@skills.ch')
+    # request.env['warden'].set_user(admin, :scope => :auth_user)
+  end
 
   def render_unauthorized
     return false if helpers.admin?
