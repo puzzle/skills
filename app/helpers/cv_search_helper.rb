266e6d4ae8d7067979f70a2a2f312b82c242263c
Yanick Minder <minder@puzzle.ch>
2024-05-28 10:15:54 +0200

    clean up cv_search

--- app/helpers/cv_search_helper.rb
+++ app/helpers/cv_search_helper.rb
@@ -4,8 +4,4 @@ module CvSearchHelper
   def translate_found_in(result)
     I18n.t("cv_search.#{result[:found_in].split('#')[0].underscore}")
   end
-
-  def person_path_with_query(result)
-    "#{person_path(result[:person][:id])}?q=#{params[:q]}"
-  end
 end
