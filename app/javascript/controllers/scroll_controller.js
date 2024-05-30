0395542b95fdf8854b620e5ff4ecd47b510999c4
Yanick Minder <minder@puzzle.ch>
2024-05-30 14:33:36 +0200

    Fix scroll bug

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
     offsetY = this.parentTarget.getBoundingClientRect().top;
 
     connect() {
+        window.scrollTo({ top: 0, behavior: 'instant'});
         document.addEventListener("scroll", () => {
             this.highlight();
         });
