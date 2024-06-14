ca1971766b259e2c522ba2379a253d37c744d453
Jannik Pulfer <pulfer@puzzle.ch>
2024-06-14 10:44:03 +0200

    Bring back highlighting of search results when clicking on results of cv search

--- app/helpers/cv_search_helper.rb
+++ app/helpers/cv_search_helper.rb
@@ -4,4 +4,8 @@ module CvSearchHelper
   def translate_found_in(result)
     I18n.t("cv_search.#{result[:found_in].split('#')[0].underscore}")
   end
+
+  def found_in_skills?(result)
+    result[:found_in].include?('skills')
+  end
 end
