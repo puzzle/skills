cf98207f259144fd2b455e597bd97562eb19fb42
Jannik Pulfer <pulfer@puzzle.ch>
2024-06-21 13:27:55 +0200

    Fix people search domain specs after making search skills a default param

--- spec/domain/people_search_spec.rb
+++ spec/domain/people_search_spec.rb
@@ -5,21 +5,21 @@ describe PeopleSearch do
   context 'search' do
     it 'finds in which association the search term has been found' do
         search_term = 'duckduck'
-        people = PeopleSearch.new(search_term, false).entries
+        people = PeopleSearch.new(search_term).entries
         person = people[0]
         expect(person[:found_in]).to eq('projects')
       end
 
       it 'finds in which person attribute the search term has been found' do
         search_term = 'Alice'
-        people = PeopleSearch.new(search_term, false).entries
+        people = PeopleSearch.new(search_term).entries
         person = people[0]
         expect(person[:found_in]).to eq('name')
       end
 
       it 'returns nothing for nonsense search term' do
         search_term = 'lkdffjskljfkl'
-        people = PeopleSearch.new(search_term, false).entries
+        people = PeopleSearch.new(search_term).entries
         expect(people).to be_empty
       end
     end
