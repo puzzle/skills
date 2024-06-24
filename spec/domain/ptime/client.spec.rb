3c6a1679437a2ced8482a85ebe096e54e4ecc3d3
Jannik Pulfer <pulfer@puzzle.ch>
2024-06-24 16:03:11 +0200

    Fix first client spec

--- spec/domain/ptime/client.spec.rb
+++ spec/domain/ptime/client.spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
+ENV["PTIME_BASE_URL"] = "www.api.com"
+ENV["PTIME_API_USERNAME"] = "test username"
+ENV["PTIME_API_PASSWORD"] = "test password"
+
 describe Ptime::Client do
     before(:each) do
-        ENV["PTIME_BASE_URL"] = "www.api.com"
-        ENV["PTIME_API_USERNAME"] = ""
-        ENV["PTIME_API_PASSWORD"] = ""
-        stub_request(:get, "www.api.com").
+        stub_request(:get, "#{ENV["PTIME_BASE_URL"]}/api/v1/employees").
             to_return(body: "abc".to_json, headers: { 'pagination-total-count': 200, 'pagination-per-page': 20, 'pagination-current-page': 5, 'pagination-total-pages': 10, 'content-type': "application/vnd.api+json; charset=utf-8" }, status: 200)
     end
 
-    it 'should test' do
-        Ptime::Client.new.get("");
+    it 'should be able to fetch employee data' do
+        fetched_employees = Ptime::Client.new.get("employees");
     end
 end
