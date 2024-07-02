e85a027780f1b580f3d19cc323d52d666a9918d6
Yanick Minder <minder@puzzle.ch>
2024-07-02 11:01:02 +0200

    add translation for sv_export

--- config/initializers/skills_form_builder.rb
+++ config/initializers/skills_form_builder.rb
@@ -2,8 +2,7 @@ class SkillsFormBuilder < ActionView::Helpers::FormBuilder
 
   def cancel(value= nil, options = {})
     value, options = nil, value if value.is_a?(Hash)
-
-    options[:class] ||= "btn btn-secondary"
+    options[:class] ||= "btn btn-outline-secondary"
     options[:id] ||= "cancel-button"
     path ||= value || @template.polymorphic_path(self.object)
     button_text = options.delete(:text) || I18n.t('helpers.cancel')
