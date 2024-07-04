5dd9a5d7d1f4e5f21523e0aef2770383f1e1d849
Yanick Minder <minder@puzzle.ch>
2024-07-04 17:31:04 +0200

    start adding custom scanner

---
+++ lib/custom_translation_matchers/dry_crud_ti_scanner.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require_relative '../../app/helpers/i18n_helper'
+require_relative '../../app/helpers/actions_helper'
+
+module CustomTranslationMatchers
+  class DryCrudTiScanner < I18n::Tasks::Scanners::AstMatchers::BaseMatcher
+    include I18nHelper
+
+
+
+    def convert_to_key_occurrences(send_node, _method_name, location: send_node.loc)
+      human_attribute_name_to_key_occurences(send_node: send_node, location: location)
+    end
+
+    private
+
+    def human_attribute_name_to_key_occurences(send_node:, location:) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+      methods_check = %i[action_link
+                         action_icon
+                         show_action_link
+                         edit_action_link
+                         destroy_action_link
+                         index_action_link
+                         add_action_link
+                         add_action_link_modal
+                         export_action_link
+                         close_action_link
+                         cancel_action_link]
+      children = Array(send_node&.children)
+      receiver = children[0]
+      method_name = children[1]
+      require 'pry'; binding.pry # rubocop:disable Style/Semicolon,Lint/Debugger
+      return unless methods_check.include? :action_link
+
+      # !send_node.loc.expression.to_json.start_with?('app/helpers')
+
+      # puts ti('hi')
+      require 'pry'; binding.pry # rubocop:disable Style/Semicolon,Lint/Debugger
+
+      value = children[2]
+      string = Array(children[2]).first
+
+
+      key = 'activerecord.attributes.tet'
+      [
+        key,
+        I18n::Tasks::Scanners::Results::Occurrence.from_range(
+          raw_key: key,
+          range: location.expression
+        )
+      ]
+    end
+
+    def underscore(value)
+      value = value.dup.to_s
+      value.gsub!(/(.)([A-Z])/, '\1_\2')
+      value.downcase!
+    end
+  end
+end
