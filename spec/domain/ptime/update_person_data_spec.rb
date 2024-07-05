ec2bb14a1862498f8e4ece72539d0df2767f91d8
Jannik Pulfer <pulfer@puzzle.ch>
2024-07-05 15:52:33 +0200

    Write spec that tests ensures an error is raised when the person is not found in the ptime api

--- spec/domain/ptime/update_person_data_spec.rb
+++ spec/domain/ptime/update_person_data_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+ptime_base_test_url = "www.ptime.example.com"
+ptime_api_test_username = "test username"
+ptime_api_test_password = "test password"
+ENV["PTIME_BASE_URL"] = ptime_base_test_url
+ENV["PTIME_API_USERNAME"] = ptime_api_test_username
+ENV["PTIME_API_PASSWORD"] = ptime_api_test_password
+
 describe Ptime::UpdatePersonData do
   it 'should raise error when no ptime_employee_id is passed to new action' do
     expect{ Ptime::UpdatePersonData.new.create_person(nil) }.to raise_error(RuntimeError, 'No ptime_employee_id provided')
@@ -21,5 +28,17 @@ describe Ptime::UpdatePersonData do
 
     new_person = Ptime::UpdatePersonData.new.create_person(person_wally.ptime_employee_id)
     expect(person_wally.attributes.except(*%w[created_at updated_at])).to eql(new_person.attributes.except(*%w[created_at updated_at]))
+  end
+
+  it 'should raise error when person is not found in ptime api' do
+    stub_request(:get, "#{ptime_base_test_url}/api/v1/employees/50").
+      to_return(headers: { 'content-type': "application/vnd.api+json; charset=utf-8" }, status: 404)
+                                                                    .with(basic_auth: [ptime_api_test_username, ptime_api_test_password])
+
+    person_wally = people(:wally)
+    person_wally.ptime_employee_id = 50
+    person_wally.save!
+
+    expect{ Ptime::UpdatePersonData.new.update_person_data(person_wally) }.to raise_error(RuntimeError, 'Ptime_employee with id 50 not found')
   end
 end
