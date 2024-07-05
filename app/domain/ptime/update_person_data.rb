b4d0f788b9ad68ec595429a90595c1a268283a9e
Jannik Pulfer <pulfer@puzzle.ch>
2024-07-05 11:50:58 +0200

    Fix assigning and updating of nationalities and write test for creating a person

--- app/domain/ptime/update_person_data.rb
+++ app/domain/ptime/update_person_data.rb
@@ -15,13 +15,14 @@ module Ptime
     def update_person_data(person)
       attribute_mapping = { full_name: :name, shortname: :shortname, email: :email, marital_status:
                             :marital_status, graduation: :title, birthdate: :birthdate,
-                            location: :location, nationality: :nationality }.freeze
+                            location: :location }.freeze
 
       begin
         return unless person.ptime_employee_id
 
         ptime_employee = Ptime::Client.new.get("employees/#{person.ptime_employee_id}")['data']
       rescue RestClient::NotFound
+        person.destroy! if person.created_at == person.updated_at
         raise "Ptime_employee with id #{person.ptime_employee_id} not found"
       rescue StandardError
         nil
@@ -33,6 +34,10 @@ module Ptime
         end
         ptime_employee_employed = ptime_employee['attributes']['is_employed']
         person.company = Company.find_by(name: ptime_employee_employed ? 'Firma' : 'Ex-Mitarbeiter')
+        ptime_employee_nationalities = ptime_employee['attributes']['nationalities']
+        person.nationality = ptime_employee_nationalities[0]
+        person.nationality2 = ptime_employee_nationalities[1]
+        person.destroy! if !person.valid? && person.created_at == person.updated_at
         person.save!
       end
     end
--- spec/features/update_person_data_spec.rb
+++ spec/features/update_person_data_spec.rb
@@ -81,45 +81,99 @@ employees = {
 }
 
 describe Ptime::UpdatePersonData do
-  it 'should update person when visited' do
-    updated_wally = {
-      'id': 50,
-      'type': 'employee',
-      'attributes': {
-        'shortname': 'CAL',
-        'firstname': 'Changed Wally',
-        'lastname': 'Allround',
-        'fullname': 'Changed Wally Allround',
-        'email': 'changedwally@example.com',
-        'marital_status': 'single',
-        'nationalities': [
-          'US'
-        ],
-        'graduation': 'Quarter-Stack Developer',
-        'department_shortname': 'SYS',
-        'employment_roles': [],
-        is_employed: false,
-        birthdate: '1.1.2000',
-        location: 'Bern',
-        nationality: 'CH'
-    }}
+  describe 'Update or create person', type: :feature, js: true do
+    before(:each) do
+      sign_in auth_users(:user), scope: :auth_user
+    end
 
-    stub_request(:get, "#{ptime_base_test_url}/api/v1/employees?per_page=1000").
-      to_return(body: updated_wally.to_json, headers: { 'content-type': "application/vnd.api+json; charset=utf-8" }, status: 200)
-                                                                               .with(basic_auth: [ptime_api_test_username, ptime_api_test_password])
+    it 'should update person when visited' do
+      updated_wally = {
+        'data': {
+          'id': 50,
+          'type': 'employee',
+          'attributes': {
+            'shortname': 'CAL',
+            'firstname': 'Changed Wally',
+            'lastname': 'Allround',
+            'full_name': 'Changed Wally Allround',
+            'email': 'changedwally@example.com',
+            'marital_status': 'single',
+            'nationalities': %w[DE DK],
+            'graduation': 'Quarter-Stack Developer',
+            'department_shortname': 'SYS',
+            'employment_roles': [],
+            'is_employed': false,
+            'birthdate': '1.1.2000',
+            'location': 'Basel',
+          }}
+        }
 
-    person_wally = people(:wally)
-    expect(person_wally.name).to eq('Wally Allround')
-    expect(person_wally.shortname).to eq('WAL')
-    visit person_path(person_wally)
-    person_wally.reload
-    expect(person_wally.name).to eq('Changed Wally Allround')
-    expect(person_wally.shortname).to eq('CAL')
-    expect(person_wally.email).to eq('changedwally@example.com')
-    expect(person_wally.marital_status).to eq('single')
-    expect(person_wally.title).to eq('Quarter-Stack Developer')
-    expect(person_wally.birthdate).to eq('1.1.2000')
-    expect(person_wally.location).to eq('Bern')
-    expect(person_wally.nationality).to eq('CH')
+
+      stub_request(:get, "#{ptime_base_test_url}/api/v1/employees/50").
+        to_return(body: updated_wally.to_json, headers: { 'content-type': "application/vnd.api+json; charset=utf-8" }, status: 200)
+                                                                                 .with(basic_auth: [ptime_api_test_username, ptime_api_test_password])
+
+      Company.create!(name: "Ex-Mitarbeiter")
+      person_wally = people(:wally)
+      person_wally.ptime_employee_id = 50
+      person_wally.save!
+
+      expect(person_wally.name).to eq('Wally Allround')
+      expect(person_wally.email).to eq('wally@example.com')
+      visit person_path(person_wally)
+      person_wally.reload
+      expect(person_wally.name).to eq('Changed Wally Allround')
+      expect(person_wally.shortname).to eq('CAL')
+      expect(person_wally.email).to eq('changedwally@example.com')
+      expect(person_wally.marital_status).to eq('single')
+      expect(person_wally.title).to eq('Quarter-Stack Developer')
+      expect(person_wally.birthdate).to eq('1.1.2000')
+      expect(person_wally.location).to eq('Basel')
+      expect(person_wally.nationality).to eq('DE')
+      expect(person_wally.nationality2).to eq('DK')
+    end
+
+    it 'should create person when visited' do
+      updated_wally = {
+        'data': {
+          'id': 50,
+          'type': 'employee',
+          'attributes': {
+            'shortname': 'CAL',
+            'firstname': 'Changed Wally',
+            'lastname': 'Allround',
+            'full_name': 'Changed Wally Allround',
+            'email': 'changedwally@example.com',
+            'marital_status': 'single',
+            'nationalities': %w[DE EG],
+            'graduation': 'Quarter-Stack Developer',
+            'department_shortname': 'SYS',
+            'employment_roles': [],
+            'is_employed': true,
+            'birthdate': '1.1.2000',
+            'location': 'Basel',
+          }}
+      }
+
+      stub_request(:get, "#{ptime_base_test_url}/api/v1/employees/50").
+        to_return(body: updated_wally.to_json, headers: { 'content-type': "application/vnd.api+json; charset=utf-8" }, status: 200)
+                                                                      .with(basic_auth: [ptime_api_test_username, ptime_api_test_password])
+      Company.create!(name: "Ex-Mitarbeiter")
+
+      person_wally = people(:wally)
+      person_wally.destroy!
+      expect(Person.find_by(name: "Wally Allround")).to be_nil
+      visit new_person_path(ptime_employee_id: 50)
+      new_wally = Person.find_by(name: "Changed Wally Allround")
+      expect(new_wally.name).to eq('Changed Wally Allround')
+      expect(new_wally.shortname).to eq('CAL')
+      expect(new_wally.email).to eq('changedwally@example.com')
+      expect(new_wally.marital_status).to eq('single')
+      expect(new_wally.title).to eq('Quarter-Stack Developer')
+      expect(new_wally.birthdate).to eq('1.1.2000')
+      expect(new_wally.location).to eq('Basel')
+      expect(new_wally.nationality).to eq('DE')
+      expect(new_wally.nationality2).to eq('EG')
+    end
   end
 end
