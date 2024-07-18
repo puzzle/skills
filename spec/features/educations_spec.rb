bf771aa302f8549b8ec23044ab38b68c1e4054a4
Manuel <moeri@puzzle.ch>
2024-07-18 14:39:44 +0200

    Generalize window resizing of capybara

--- spec/features/educations_spec.rb
+++ spec/features/educations_spec.rb
@@ -4,7 +4,6 @@ describe 'Educations', type: :feature, js:true do
   let(:person) { people(:bob) }
 
   before(:each) do
-    Capybara.page.driver.browser.manage.window.maximize
     set_env_variables_and_stub_request
     sign_in auth_users(:admin)
     visit person_path(person)
