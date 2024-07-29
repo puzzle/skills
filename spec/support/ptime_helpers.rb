57ad58af26df975ec57c904db0f8ce97450ad944
Yanick Minder <minder@puzzle.ch>
2024-07-29 10:41:58 +0200

    add client test

--- spec/support/ptime_helpers.rb
+++ spec/support/ptime_helpers.rb
@@ -14,13 +14,13 @@ module PtimeHelpers
         fixture_data("all_ptime_employees")[:data]
     end
 
-    def stub_ptime_request(return_body, path =nil)
+    def stub_ptime_request(return_body, path =nil, status = 200)
         path ||= "employees?per_page=1000"
         url = "http://#{ENV["PTIME_BASE_URL"]}/api/v1/#{path}"
         content_type = "application/vnd.api+json; charset=utf-8"
 
         stub_request(:get, url)
-            .to_return(body: return_body, headers: { 'content-type': content_type }, status: 200)
+            .to_return(body: return_body, headers: { 'content-type': content_type }, status: status)
             .with(basic_auth: [ENV["PTIME_API_USERNAME"], ENV["PTIME_API_PASSWORD"]])
     end
 
