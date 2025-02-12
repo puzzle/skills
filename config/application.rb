835b2ce88ea75e3bdb18c2be58da986b3d747477
Jannik Pulfer <pulfer@puzzle.ch>
2025-02-12 15:22:04 +0100

    Delete new framework defaults file and load defaults of v 7.2

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 module Skills
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     I18n.available_locales = [:de, :en, :fr, :it]
