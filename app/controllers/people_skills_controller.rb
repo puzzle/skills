d0354360f2110f2dbf6db27eeacc58d75794d334
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-02-24 08:27:58 +0100

    write new helper method for no_match translation to clean up view

--- app/controllers/people_skills_controller.rb
+++ app/controllers/people_skills_controller.rb
@@ -3,7 +3,7 @@
 class PeopleSkillsController < CrudController
   include ParamConverters
 
-  helper_method :filter_params, :find_skill_title_by_id
+  helper_method :filter_params, :find_skill_title_by_id, :get_no_match_translation
 
   def entries
     return [] if filter_params.skill_ids.empty?
@@ -27,12 +27,28 @@ class PeopleSkillsController < CrudController
     PeopleSkills::FilterParams.new(params)
   end
 
+  def extract_required_search_values
+    filtered_array = filter_params.skill_ids.zip(filter_params.levels, filter_params.interests)
+    filtered_array.reject { |arr| arr.first == '' }.transpose
+  end
+
   def find_skill_title_by_id(id)
     Skill.find(id).title
   end
 
-  def extract_required_search_values
-    filtered_array = filter_params.skill_ids.zip(filter_params.levels, filter_params.interests)
-    filtered_array.reject { |arr| arr.first == '' }.transpose
+  def get_no_match_translation(index)
+    if index == 0
+      if index == filter_params.rows_count - 1
+        "search.no_match.only_skill"
+      else
+        "search.no_match.first_skill"
+      end
+    else
+      if index == filter_params.rows_count - 1
+        "search.no_match.last_skill"
+      else
+        "search.no_match.skill"
+      end
+    end
   end
 end
