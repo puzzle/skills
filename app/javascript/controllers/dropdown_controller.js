2c36576049277f23b3e7fe4222ec5798c9e4e6f9
Jannik Pulfer <pulfer@puzzle.ch>
2025-02-26 16:03:12 +0100

    Disable link of selected element in slim select dropdown to allow opening of dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
     if (!this.hasDropdownTarget)
       return;
     
-    new SlimSelect({
+    const slimSelectDropdown = new SlimSelect({
       select: this.dropdownTarget,
       events: {
         beforeChange: (newVal) => {
@@ -17,11 +17,22 @@ export default class extends Controller {
           // Check if dropdown element is a link
           if(newVal.html.startsWith("<a")) {
             Turbo.visit(newVal.value);
+
+            // Make currently selected element does not follow link when clicked on, so opening the dropdown is possible
+            if(slimSelectDropdown.getSelected()[0]) {
+              document.querySelector('.ss-main .dropdown-option-link').href = "javascript:void(0)";
+            }
+
             return false;
           }
         }
       },
     });
+
+    // Make currently selected element does not follow link when clicked on, so opening the dropdown is possible
+    if(slimSelectDropdown.getSelected()[0]?.startsWith("/")) {
+      document.querySelector('.ss-main .dropdown-option-link').href = "javascript:void(0)";
+    }
   }
 
   handleChange(event) {
