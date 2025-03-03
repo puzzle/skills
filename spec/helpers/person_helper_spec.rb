ee1492720cc9b3e8b9e0902a80455f84680a0f94
Manuel <moeri@puzzle.ch>
2025-03-03 15:48:55 +0100

    Fix tests

--- spec/helpers/person_helper_spec.rb
+++ spec/helpers/person_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PersonHelper, type: :helper do
 
     it 'should send request to ptime api' do
       allow(Skills).to receive(:ptime_available?).and_return(true)
-      skills_people = helper.fetch_ptime_or_skills_data
+      skills_people = helper.fetch_people_data
       expected = [
         ["Longmax Smith", "/de/people/new?ptime_employee_id=33"],
         ["Alice Mante", "/de/people/new?ptime_employee_id=21"],
@@ -17,7 +17,7 @@ RSpec.describe PersonHelper, type: :helper do
     it 'should return people from skills database if last request was right now' do
       allow(Skills).to receive(:ptime_available?).and_return(false)
 
-      skills_people = helper.fetch_ptime_or_skills_data
+      skills_people = helper.fetch_people_data
       expected = [
         ["Bob Anderson", "/de/people/902541635"],
         ["Alice Mante", "/de/people/663665735"],
@@ -40,7 +40,7 @@ RSpec.describe PersonHelper, type: :helper do
       longmax.update!(ptime_employee_id: 33)
       alice.update!(ptime_employee_id: 21)
 
-      dropdown_data = build_dropdown_data(ptime_employees_data)
+      dropdown_data = build_people_dropdown(ptime_employees_data)
       expected = [
         ["Longmax Smith", "/de/people/169654640"],
         ["Alice Mante", "/de/people/663665735"],
