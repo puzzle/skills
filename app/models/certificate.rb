6013f0d24f5373022924e805fdc4b17c8e467a47
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-05 13:56:20 +0100

    Add length limit to certificate fields and add model tests for the validators

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -2,6 +2,9 @@ class Certificate < ApplicationRecord
   validates :name, :points_value, :description,
             :exam_duration, :type_of_exam, :study_time, presence: true
 
+  validates :name, :provider, length: { maximum: 100 }
+  validates :description, :notes, length: { maximum: 250 }
+
   def to_s
     name
   end
--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe Certificate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Certificate do
+  fixtures :certificates
+
+  context 'validations' do
+    it 'checks whether required attribute values are present' do
+      certificate = Certificate.new
+      certificate.valid?
+
+      expect(certificate.errors[:name].first).to eq('muss ausgefüllt werden')
+      expect(certificate.errors[:points_value].first).to eq('muss ausgefüllt werden')
+      expect(certificate.errors[:description].first).to eq('muss ausgefüllt werden')
+      expect(certificate.errors[:exam_duration].first).to eq('muss ausgefüllt werden')
+      expect(certificate.errors[:type_of_exam].first).to eq('muss ausgefüllt werden')
+      expect(certificate.errors[:study_time].first).to eq('muss ausgefüllt werden')
+    end
+
+    it 'checks validation maximum length for attribute' do
+      certificate = certificates('aws-certification')
+      certificate.name = SecureRandom.hex(101)
+      certificate.provider = SecureRandom.hex(101)
+      certificate.description = SecureRandom.hex(251)
+      certificate.notes = SecureRandom.hex(251)
+      certificate.valid?
+
+      expect(certificate.errors[:name].first).to eq('ist zu lang (mehr als 100 Zeichen)')
+      expect(certificate.errors[:provider].first).to eq('ist zu lang (mehr als 100 Zeichen)')
+      expect(certificate.errors[:description].first).to eq('ist zu lang (mehr als 250 Zeichen)')
+      expect(certificate.errors[:notes].first).to eq('ist zu lang (mehr als 250 Zeichen)')
+    end
+  end
 end
