b9021d1315a8eb7bb0c1688c869d6a3f6de20429
Manuel <moeri@puzzle.ch>
2025-03-06 14:05:29 +0100

    Fix tests for now, but make rubocop mad

--- spec/helpers/person_helper_spec.rb
+++ spec/helpers/person_helper_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe PersonHelper, type: :helper do
       allow(Skills).to receive(:ptime_available?).and_return(true)
       skills_people = helper.fetch_people_data
       expected = [
-        ["Longmax Smith", "people/new?ptime_employee_id=33"],
-        ["Alice Mante", "people/new?ptime_employee_id=21"],
-        ["Charlie Ford", "people/new?ptime_employee_id=45"]
+        ["Longmax Smith", "/de/people/new?ptime_employee_id=33"],
+        ["Alice Mante", "/de/people/new?ptime_employee_id=21"],
+        ["Charlie Ford", "/de/people/new?ptime_employee_id=45"]
       ]
       expect(skills_people).to eq(expected)
     end
@@ -19,13 +19,13 @@ RSpec.describe PersonHelper, type: :helper do
 
       skills_people = helper.fetch_people_data
       expected = [
-        ["Bob Anderson", "people/902541635"],
-        ["Alice Mante", "people/663665735"],
-        ["ken", "people/155397742"],
-        ["Charlie Ford", "people/786122151"],
-        ["Wally Allround", "people/790004949"],
-        ["Hope Sunday", "people/247095502"],
-        ["Longmax Smith", "people/169654640"]
+        ["Bob Anderson", "/de/people/902541635", { class: "p-0", "data-html": "<a href='/de/people/902541635' class='dropdown-option-link'>Bob Anderson</a>" }],
+        ["Alice Mante", "/de/people/663665735", { class: "p-0", "data-html": "<a href='/de/people/663665735' class='dropdown-option-link'>Alice Mante</a>" }],
+        ["ken", "/de/people/155397742", { class: "p-0", "data-html": "<a href='/de/people/155397742' class='dropdown-option-link'>ken</a>" }],
+        ["Charlie Ford", "/de/people/786122151", { class: "p-0", "data-html": "<a href='/de/people/786122151' class='dropdown-option-link'>Charlie Ford</a>" }],
+        ["Wally Allround", "/de/people/790004949", { class: "p-0", "data-html": "<a href='/de/people/790004949' class='dropdown-option-link'>Wally Allround</a>" }],
+        ["Hope Sunday", "/de/people/247095502", { class: "p-0", "data-html": "<a href='/de/people/247095502' class='dropdown-option-link'>Hope Sunday</a>" }],
+        ["Longmax Smith", "/de/people/169654640", { class: "p-0", "data-html": "<a href='/de/people/169654640' class='dropdown-option-link'>Longmax Smith</a>" }]
       ]
       expect(skills_people).to eq(expected)
     end
