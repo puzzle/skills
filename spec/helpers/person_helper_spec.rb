ddaf23b56d0b41d138dbd3c1d30e1ae928b428d6
Manuel <moeri@puzzle.ch>
2025-03-10 13:40:52 +0100

    Fix helper tests to check for the correct path

--- spec/helpers/person_helper_spec.rb
+++ spec/helpers/person_helper_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe PersonHelper, type: :helper do
       allow(Skills).to receive(:use_ptime_sync?).and_return(true)
       skills_people = helper.fetch_people_data
       expected = [
-        ["Longmax Smith", "/de/people/new?ptime_employee_id=33"],
-        ["Alice Mante", "/de/people/new?ptime_employee_id=21"],
-        ["Charlie Ford", "/de/people/new?ptime_employee_id=45"]
+        ["Longmax Smith", "/people/new?ptime_employee_id=33", { class: "p-0", "data-html": "<a href='/people/new?ptime_employee_id=33' class='dropdown-option-link'>Longmax Smith</a>" }],
+        ["Alice Mante", "/people/new?ptime_employee_id=21", { class: "p-0", "data-html": "<a href='/people/new?ptime_employee_id=21' class='dropdown-option-link'>Alice Mante</a>" }],
+        ["Charlie Ford", "/people/new?ptime_employee_id=45", { class: "p-0", "data-html": "<a href='/people/new?ptime_employee_id=45' class='dropdown-option-link'>Charlie Ford</a>" }]
       ]
       expect(skills_people).to eq(expected)
     end
@@ -19,34 +19,15 @@ RSpec.describe PersonHelper, type: :helper do
 
       skills_people = helper.fetch_people_data
       expected = [
-        ["Bob Anderson", "/de/people/902541635", { class: "p-0", "data-html": "<a href='/de/people/902541635' class='dropdown-option-link'>Bob Anderson</a>" }],
-        ["Alice Mante", "/de/people/663665735", { class: "p-0", "data-html": "<a href='/de/people/663665735' class='dropdown-option-link'>Alice Mante</a>" }],
-        ["ken", "/de/people/155397742", { class: "p-0", "data-html": "<a href='/de/people/155397742' class='dropdown-option-link'>ken</a>" }],
-        ["Charlie Ford", "/de/people/786122151", { class: "p-0", "data-html": "<a href='/de/people/786122151' class='dropdown-option-link'>Charlie Ford</a>" }],
-        ["Wally Allround", "/de/people/790004949", { class: "p-0", "data-html": "<a href='/de/people/790004949' class='dropdown-option-link'>Wally Allround</a>" }],
-        ["Hope Sunday", "/de/people/247095502", { class: "p-0", "data-html": "<a href='/de/people/247095502' class='dropdown-option-link'>Hope Sunday</a>" }],
-        ["Longmax Smith", "/de/people/169654640", { class: "p-0", "data-html": "<a href='/de/people/169654640' class='dropdown-option-link'>Longmax Smith</a>" }]
+        ["Bob Anderson", "/people/902541635", { class: "p-0", "data-html": "<a href='/people/902541635' class='dropdown-option-link'>Bob Anderson</a>" }],
+        ["Alice Mante", "/people/663665735", { class: "p-0", "data-html": "<a href='/people/663665735' class='dropdown-option-link'>Alice Mante</a>" }],
+        ["ken", "/people/155397742", { class: "p-0", "data-html": "<a href='/people/155397742' class='dropdown-option-link'>ken</a>" }],
+        ["Charlie Ford", "/people/786122151", { class: "p-0", "data-html": "<a href='/people/786122151' class='dropdown-option-link'>Charlie Ford</a>" }],
+        ["Wally Allround", "/people/790004949", { class: "p-0", "data-html": "<a href='/people/790004949' class='dropdown-option-link'>Wally Allround</a>" }],
+        ["Hope Sunday", "/people/247095502", { class: "p-0", "data-html": "<a href='/people/247095502' class='dropdown-option-link'>Hope Sunday</a>" }],
+        ["Longmax Smith", "/people/169654640", { class: "p-0", "data-html": "<a href='/people/169654640' class='dropdown-option-link'>Longmax Smith</a>" }]
       ]
       expect(skills_people).to eq(expected)
-    end
-  end
-
-  describe '#build_dropdown_data' do
-    it 'should build correct dropdown data' do
-      longmax = people(:longmax)
-      alice = people(:alice)
-      people(:charlie)
-
-      longmax.update!(ptime_employee_id: 33)
-      alice.update!(ptime_employee_id: 21)
-
-      dropdown_data = build_people_dropdown(ptime_employees_data)
-      expected = [
-        ["Longmax Smith", "/de/people/169654640"],
-        ["Alice Mante", "/de/people/663665735"],
-        ["Charlie Ford", "/de/people/new?ptime_employee_id=45"]]
-      expect(dropdown_data).to eq(expected)
-
     end
   end
 end
