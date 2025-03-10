980c152f5d7abe4e89a79bf96e3c40ee06a4c648
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-10 08:49:44 +0100

    Show certificate overview after creating one and fix styling

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -3,4 +3,9 @@ class CertificatesController < CrudController
                             provider exam_duration type_of_exam
                             study_time notes]
   before_action :render_unauthorized_not_conf_admin
+
+  def create(**options, &)
+    super(:location => certificates_path,
+          status: :unprocessable_entity)
+  end
 end
