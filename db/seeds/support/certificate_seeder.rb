85ac4f8b190febc1c9ac23b67b8116fe013087eb
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-12 15:40:20 +0100

    Simplify rounding in certificate seeder

--- db/seeds/support/certificate_seeder.rb
+++ db/seeds/support/certificate_seeder.rb
@@ -6,7 +6,7 @@ class CertificateSeeder
         cert.points_value = [0, 0.5, 1, 1.5].sample
         cert.description = Faker::Company.bs.upcase_first
         cert.provider = Faker::Company.name
-        cert.exam_duration = Faker::Number.between(from: 60, to: 240)/10.floor*10
+        cert.exam_duration = Faker::Number.between(from: 60, to: 240).round(-1)
         cert.type_of_exam = ['Multiple choice', 'Theoretical', 'Practical', 'Case study', 'Written work'].sample
         cert.study_time = Faker::Number.between(from: 1, to: 10)
         cert.notes = Faker::Company.bs.upcase_first
