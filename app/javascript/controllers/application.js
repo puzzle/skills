bb2a17e7567fc2ba62cb937d2ee093c499677608
Miguel Lehmann <lehmann@puzzle.ch>
2025-03-13 11:04:17 +0100

    make tooltip configure simpler

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,9 +8,7 @@ application.debug = false
 window.Stimulus   = application
 
 // Configure Boostrap tooltip
-document.addEventListener("DOMContentLoaded", function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-});
 
 export { application }
