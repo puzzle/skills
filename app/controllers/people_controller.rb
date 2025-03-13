d2c696a0aeb5625805b3b777303a38c4f55962c2
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-03-13 08:47:44 +0100

    make switzerland the new default nationality

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -32,6 +32,7 @@ class PeopleController < CrudController
 
   def new
     super
+    @person.nationality = "CH"
     %w[DE EN FR].each do |language|
       @person.language_skills.push(LanguageSkill.new({ language: language }))
     end
