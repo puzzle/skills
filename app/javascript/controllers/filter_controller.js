d50dd246f2dc8a22cfe2fc56803832a9220d261f
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-21 14:30:52 +0100

    Add comment to clarify changing of default skill form action in stimulus controller

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -10,6 +10,9 @@ export default class extends Controller {
         const params = new URLSearchParams(formData);
         const newUrl = `${form.action}?${params.toString()}`;
 
+        /* When changing the rating filter, the action urls of the default skill forms don't reflect that change.
+        To prevent reverting the filter to its state when initially loading the page, we have to update the rating
+        param in the action urls of these forms. */
         const defaultSkillForms = document.querySelectorAll('#default-skills form');
         defaultSkillForms.forEach(f => f.action = newUrl)
 
