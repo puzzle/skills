33994e8bcfc8eac9eb4dcebd772be6090782ec10
Miguel Lehmann <lehmann@puzzle.ch>
2025-03-26 13:00:36 +0100

    test every field that gets disabled

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -212,6 +212,19 @@ describe :people do
       visit person_path(bob)
       click_link('Bearbeiten', href: edit_person_path(bob))
       page.find('#person_name').disabled?
+      page.find('#person_email').disabled?
+      page.find('#person_title').disabled?
+      page.find('#person_person_roles_attributes_0_role_id').disabled?
+      page.find('#person_person_roles_attributes_0_person_role_level_id').disabled?
+      page.find('#person_person_roles_attributes_0_percent').disabled?
+      page.find('#person_department_id').disabled?
+      page.find('#person_company_id').disabled?
+      page.find('#person_location').disabled?
+      page.find('#person_birthdate').disabled?
+      page.find('#nat-two-checkbox').disabled?
+      page.find('#person_nationality').disabled?
+      page.find('#person_marital_status').disabled?
+      page.find('#person_shortname').disabled?
     end
 
     it 'should have all edit fields' do
