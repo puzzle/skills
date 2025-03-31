23a6c7f1930403db4a35fcff9d6b8a248fddb6de
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-31 11:32:37 +0200

    Update unified skills controller to return new skill and begin to write controller and model specs for unified skills

--- app/controllers/unified_skills_controller.rb
+++ app/controllers/unified_skills_controller.rb
@@ -7,10 +7,12 @@ class UnifiedSkillsController < CrudController
       new_skill = Skill.create!(params.fetch(:skill).merge(default_set: false))
       old_skill1 = Skill.find(params.fetch(:old_skill_id1))
       old_skill2 = Skill.find(params.fetch(:old_skill_id2))
-      UnifiedSkill.create!(skill_1_attrs: old_skill1.attributes, skill_2_attrs: old_skill2.attributes, unified_skill_attrs: new_skill.attributes)
+      UnifiedSkill.create!(skill1_attrs: old_skill1.attributes, skill2_attrs: old_skill2.attributes, unified_skill_attrs: new_skill.attributes)
       PeopleSkill.where(skill_id: [old_skill1.id, old_skill2.id]).update!(skill_id: new_skill.id)
-      old_skill1.delete!
-      old_skill2.delete!
+      old_skill1.delete
+      old_skill2.delete
+
+      new_skill
     end
   end
 end
---
+++ spec/controllers/unified_skills_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe UnifiedSkillsController do
+  before(:each) do
+    sign_in(auth_users(:admin))
+  end
+
+  it 'should unify two skills' do
+    skill1 = Skill.first
+    skill2 = Skill.second
+    old_people_skills_count = PeopleSkill.where(skill_id: [skill1.id, skill2.id]).size
+
+    new_skill = {title: 'A unified skill', radar: 'adopt', portfolio: 'aktiv', category_id: Category.first.id}
+
+    post :create, params: {old_skill_id1: skill1.id, old_skill_id2: skill2.id, skill: new_skill}
+
+    expect(Skill.find_by(id: skill1.id)).to be_nil
+    expect(Skill.find_by(id: skill2.id)).to be_nil
+    expect(PeopleSkill.where(skill_id: [skill1.id, skill2.id])).to be_empty
+
+    merged_skill = Skill.find_by!(title: 'A unified skill')
+    expect(PeopleSkill.where(skill_id: merged_skill.id).size).to eql(old_people_skills_count)
+  end
+end
