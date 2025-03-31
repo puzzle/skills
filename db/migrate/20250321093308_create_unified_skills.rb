37d85f6f8fcd00e7b1961c698fa5fa77762cfb5e
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-31 09:22:45 +0200

    Rename numbered attribute names of unified skill to match naming convention and add logic that creates new unified skill and sets new skill on people skills in controller

--- db/migrate/20250321093308_create_unified_skills.rb
+++ db/migrate/20250321093308_create_unified_skills.rb
@@ -1,8 +1,8 @@
 class CreateUnifiedSkills < ActiveRecord::Migration[8.0]
   def change
     create_table :unified_skills do |t|
-      t.text :skill_1_attrs
-      t.text :skill_2_attrs
+      t.text :skill1_attrs
+      t.text :skill2_attrs
       t.text :unified_skill_attrs
 
       t.timestamps
