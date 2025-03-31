ed28cd45a6d7f757a877c37d3cd6a1e181af27d2
Jannik Pulfer <pulfer@puzzle.ch>
2025-03-31 11:32:37 +0200

    Update unified skills controller to return new skill and begin to write controller and model specs for unified skills

--- spec/models/unified_skill_spec.rb
+++ spec/models/unified_skill_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe UnifiedSkill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Certificate do
+  it 'should correctly serialize attributes' do
+    skill = Skill.first
+    unified_skill = UnifiedSkill.create!(skill1_attrs: skill.attributes, skill2_attrs: skill.attributes, unified_skill_attrs: skill.attributes)
+
+    expect(unified_skill.skill1_attrs.is_a? Hash).to eql(true)
+    expect(unified_skill.skill2_attrs.is_a? Hash).to eql(true)
+    expect(unified_skill.unified_skill_attrs.is_a? Hash).to eql(true)
+  end
 end
