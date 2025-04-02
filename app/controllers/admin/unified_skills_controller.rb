319912811b203d3af4d44ad9c31471369114cf8d
Jannik Pulfer <pulfer@puzzle.ch>
2025-04-02 16:18:12 +0200

    Implement new unified_skill_form class for handling the unified_skill form, rework the form with this model and move it to the new route, clean up code, update tests with new json structure

--- app/controllers/admin/unified_skills_controller.rb
+++ app/controllers/admin/unified_skills_controller.rb
@@ -1,18 +1,25 @@
 class Admin::UnifiedSkillsController < CrudController
   self.nesting = :admin
-  self.permitted_attrs = [:old_skill_id1, :old_skill_id2, { skill:
-                              [:id, :title, :radar, :portfolio, :default_set, :category_id] }]
-  before_action :render_unauthorized_not_admin
 
-  def index
-    @skill = Skill.find_by(id: params[:old_skill_id1]) || Skill.new
+  def self.model_class
+    UnifiedSkillForm
   end
 
-  def create
-    ActiveRecord::Base.transaction do
-      params.permit!
-      validate!
+  def build_entry
+    UnifiedSkillForm.new
+  end
 
+  self.permitted_attrs = [:old_skill_id1, :old_skill_id2, { new_skill: [:id, :title, :radar,
+                                                                        :portfolio, :default_set,
+                                                                        :category_id] }]
+  before_action :render_unauthorized_not_admin
+
+  def create
+    assign_attributes
+
+    return redirect_on_failure(location: new_admin_unified_skill_path) unless entry.valid?
+
+    ActiveRecord::Base.transaction do
       old_skill1 = Skill.find(old_skill_id1)
       old_skill2 = Skill.find(old_skill_id2)
 
@@ -23,7 +30,7 @@ class Admin::UnifiedSkillsController < CrudController
   private
 
   def merge_skills(old_skill1, old_skill2)
-    new_skill = Skill.create!(new_skill_values)
+    new_skill = Skill.create!(entry.new_skill)
     PeopleSkill.where(skill_id: [old_skill_id1, old_skill_id2]).update!(skill_id: new_skill.id)
     UnifiedSkill.create!(skill1_attrs: old_skill1.attributes, skill2_attrs: old_skill2.attributes,
                          unified_skill_attrs: new_skill.attributes)
@@ -32,43 +39,11 @@ class Admin::UnifiedSkillsController < CrudController
     old_skill2.delete
   end
 
-  def new_skill_values
-    params.fetch(:skill)
-  end
-
   def old_skill_id1
-    params.fetch(:old_skill_id1)
+    entry.old_skill_id1
   end
 
   def old_skill_id2
-    params.fetch(:old_skill_id2)
-  end
-
-  def validate!
-    validate_skill_ids_are_present!
-    validate_skills_are_unique!
-    validate_no_one_rated_both_skills!
-  end
-
-  def validate_skill_ids_are_present!
-    return true if old_skill_id1.present? && old_skill_id2.present?
-
-    raise 'Either one or both skill ids are missing'
-  end
-
-  def validate_skills_are_unique!
-    return true if old_skill_id1 != old_skill_id2
-
-    raise 'Merging a skill with itself is not possible'
-  end
-
-  def validate_no_one_rated_both_skills!
-    return true if PeopleSkill
-                   .where(skill_id: [old_skill_id1, old_skill_id2])
-                   .group(:person_id)
-                   .having('COUNT(skill_id) = 2')
-                   .none?
-
-    raise 'Merging these skills is not allowed since someone has rated both of them'
+    entry.old_skill_id2
   end
 end
