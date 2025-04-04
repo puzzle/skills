f674b334fac931e0bd1483dab98554b84f0e2af1
Jannik Pulfer <pulfer@puzzle.ch>
2025-04-04 11:16:49 +0200

    Add missing default set field to skill form, add correct error and success translations for all languages

--- app/helpers/unified_skills_helper.rb
+++ app/helpers/unified_skills_helper.rb
@@ -1,10 +1,11 @@
 module UnifiedSkillsHelper
+  include ParamConverters
   def old_skill1_select_options
-    options_for_select(skills_for_dropdown, selected: old_skill_id1, disabled: old_skill_id2)
+    options_for_select(skills_for_dropdown, selected: old_skill_id1)
   end
 
   def old_skill2_select_options
-    options_for_select(skills_for_dropdown, selected: old_skill_id2, disabled: old_skill_id1)
+    options_for_select(skills_for_dropdown, selected: old_skill_id2)
   end
 
   def old_skill_id1
