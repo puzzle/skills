85d2d8cdad4d14f5d3b22d546e75f6ff0fc0097f
Miguel <141239860+Miguel7373@users.noreply.github.com>
2025-04-07 10:22:38 +0200

    Feature/733 make attr readonly for ptime attrs (#846)
    * Squash branch
    
    * disable buttons and add tooltips to show why
    
    * make tooltip only appear if ptime sync is enabled
    
    * update bundle
    
    * make tests work and add swissgerman translation
    
    * add test for disabled fields
    
    * change location of i18n
    
    * normalize i18n-tasks
    
    * test every field that gets disabled
    
    * make the disable in helper functions
    
    * make helper way smaler and impement it into the original line
    
    * change disable of the role filds
    
    * make the contoller not update if ptime sync is not active and write tests for it
    
    * change negative test
    
    * rearrange tests
    
    * change naming and remove useless parameter
    
    * solve rubocop issue
    
    * update loading of attrs
    
    * disable rubocop
    
    * move permitted attrs
    
    * move permitted attrs
    
    ---------
    
    Co-authored-by: Manuel <moeri@puzzle.ch>

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -8,6 +8,23 @@ describe PeopleController do
     render_views
     let(:person) { people(:bob).attributes }
 
+    it 'should not be able to update name' do
+      allow(Skills).to receive(:use_ptime_sync?).and_return(true)
+      expect(person["name"]).to eql("Bob Anderson")
+      expect {
+        put :update, params: { id: person["id"], person: { name: 'Test' } }
+      }.to raise_error(ActionController::ParameterMissing)
+      expect(people(:bob).reload.name).to eql("Bob Anderson")
+    end
+
+    it 'should be able to update name' do
+      testname = 'Test'
+      expect(person["name"]).to eql('Bob Anderson')
+      put :update, params: {id: person["id"], person: { name: testname } }
+      expect(response).to redirect_to(person_path(person["id"]))
+      expect(people(:bob).reload.name).to eql(testname)
+    end
+
     it 'should save nationality2 as nil if checkbox is unchecked and save it if it is checked' do
       expect(person["nationality2"]).not_to be(nil)
       put :update, params: {id: person["id"], person: person, has_nationality2: {checked: "0"}}
