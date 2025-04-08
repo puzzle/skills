f605cebb2c034fc1b30438d070a4197ed95122b6
Miguel Lehmann <lehmann@puzzle.ch>
2025-04-08 10:18:25 +0200

    include puzzle shell header

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -24,10 +24,9 @@ module AuthHelper
   end
 
   def language_selector
-    languages = I18n.available_locales.map { |e| e.to_s }.map do |lang_code|
+    I18n.available_locales.map { |e| e.to_s }.map do |lang_code|
       [language(lang_code).capitalize, url_for(locale: lang_code, set_by_user: true)]
     end
-    options_for_select(languages, url_for(locale: I18n.locale, set_by_user: true))
   end
 
   def language(lang_code)
