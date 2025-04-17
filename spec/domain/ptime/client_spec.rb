532c3ae1ae812600543bdfdbee90350277d0ccb0
Manuel <moeri@puzzle.ch>
2025-04-17 11:08:15 +0200

    Prepare of ptime-sync with delayed job by reusing parts of the ptime-mapper-script branch
    Features taken over: Mapping of employees to people, logic of updating people, the request client & specs

---
+++ spec/domain/ptime/client_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Ptime::Client do
+  it 'should be able to fetch employee data' do
+    fetched_employees = Ptime::Client.new.request(:get, "employees", { per_page: 1000 })
+    expect(fetched_employees).to eq(ptime_employees_data)
+  end
+
+  it 'should raise PTimeClientError page is unreachable' do
+    stub_env_var("PTIME_BASE_URL", "irgend.oepp.is")
+    stub_ptime_request(ptime_employees.to_json, "employees?per_page=1000", 404)
+    expect {
+      Ptime::Client.new.request(:get, "employees", { per_page: 1000 })
+    }.to raise_error(PtimeExceptions::PTimeClientError)
+  end
+end
