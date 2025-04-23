a56fcdedf2789514e1a1b24db165176b606e0269
Jannik Pulfer <pulfer@puzzle.ch>
2025-04-23 15:19:06 +0200

    Include daemon gem and clean up people_employees script for better readability and performance

--- app/domain/ptime/people_employees.rb
+++ app/domain/ptime/people_employees.rb
@@ -3,72 +3,68 @@ module Ptime
     include PtimeHelper
 
     ATTRIBUTES_MAPPING = {
-      shortname: :shortname,
-      email: :email,
-      marital_status: :marital_status,
-      graduation: :title,
+      birthday: :birthdate,
       city: :location,
-      birthday: :birthdate
+      email: :email,
+      graduation: :title,
+      shortname: :shortname
     }.freeze
 
+    EMPLOYED_COMPANY = Company.find_by(name: 'Firma')
+    UNEMPLOYED_COMPANY = Company.find_by(name: 'Ex-Mitarbeiter')
+
     def update_people_data
-      fetch_data
-      @ptime_employees.each do |ptime_employee|
+      fetch_data_of_active_ptime_employees.each do |ptime_employee|
         ActiveRecord::Base.transaction do
-          person = Person.find_or_create_by(ptime_employee_id: ptime_employee[:id])
-          update_person_data(person, ptime_employee[:attributes])
+          @ptime_employee_attributes = ptime_employee[:attributes]
+          @person = Person.find_or_create_by(ptime_employee_id: ptime_employee[:id])
+          update_person_data
         end
       end
     end
 
-    def fetch_data
-      @ptime_employees = Ptime::Client.new.request(:get, 'employees',
-                                                   { per_page: MAX_NUMBER_OF_FETCHED_EMPLOYEES })
-                                      .filter do |ptime_employee|
+    def fetch_data_of_active_ptime_employees
+      Ptime::Client.new.request(
+        :get, 'employees',
+        { per_page: MAX_NUMBER_OF_FETCHED_EMPLOYEES }
+      ).filter do |ptime_employee|
         ptime_employee.dig(:attributes, :is_employed)
       end
-      cleanup_ptime_data
     end
 
-    def cleanup_ptime_data
-      @ptime_employees.each do |ptime_employee|
-        marital_status = ptime_employee[:attributes][:marital_status]
-        if marital_status.nil?
-          ptime_employee[:attributes][:marital_status] = 'single'
-        end
-      end
-    end
-
-    def update_person_data(person, ptime_employee_attributes)
-      raise 'Person has no ptime_employee_id' unless person.ptime_employee_id
-
-      ptime_employee_attributes.each do |key, value|
-        if ATTRIBUTES_MAPPING.key?(key.to_sym)
-          person[ATTRIBUTES_MAPPING[key.to_sym]] =
-            value.presence || '-'
-        end
-      end
-
-      set_additional_attributes(person, ptime_employee_attributes)
-      person.save!
-      set_person_roles(person, ptime_employee_attributes)
+    def update_person_data
+      update_mappable_attributes
+      update_non_mappable_attributes
+      @person.save!
+      update_person_roles
     end
 
     private
 
-    def set_additional_attributes(person, ptime_employee_attributes)
-      is_employed = ptime_employee_attributes[:is_employed]
-      person.company = Company.find_by(name: is_employed ? 'Firma' : 'Ex-Mitarbeiter')
-
-      person.nationality, person.nationality2 = ptime_employee_attributes[:nationalities]
-      person.name = employee_full_name(ptime_employee_attributes)
+    def update_mappable_attributes
+      @ptime_employee_attributes.each do |key, value|
+        if ATTRIBUTES_MAPPING.key?(key.to_sym)
+          @person[ATTRIBUTES_MAPPING[key.to_sym]] =
+            value.presence || '-'
+        end
+      end
     end
 
-    def set_person_roles(person, ptime_employee_attributes)
-      PersonRole.where(person_id: person.id).destroy_all
-      ptime_employee_attributes[:employment_roles].each do |role|
-        role_id = Role.find_or_create_by(name: sanitized_role_name(role[:name])).id
-        PersonRole.create!(person_id: person.id,
+    def update_non_mappable_attributes
+      is_employed = @ptime_employee_attributes[:is_employed]
+      @person.company = is_employed ? EMPLOYED_COMPANY : UNEMPLOYED_COMPANY
+      department = Department.find_or_create_by!(name: @ptime_employee_attributes[:department_name])
+      @person.department = department
+      @person.marital_status = @ptime_employee_attributes[:marital_status].presence || 'single'
+      @person.name = employee_full_name(@ptime_employee_attributes)
+      @person.nationality, @person.nationality2 = @ptime_employee_attributes[:nationalities]
+    end
+
+    def update_person_roles
+      PersonRole.where(person_id: @person.id).destroy_all
+      @ptime_employee_attributes[:employment_roles].each do |role|
+        role_id = Role.find_or_create_by!(name: sanitized_role_name(role[:name])).id
+        PersonRole.create!(person_id: @person.id,
                            role_id: role_id,
                            percent: role[:percent],
                            person_role_level_id: person_role_level_id_by_role(role))
