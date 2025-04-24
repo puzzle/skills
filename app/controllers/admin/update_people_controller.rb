78aba3d34ab0bd7690bc6f834a4555d0b4dc9192
Manuel <moeri@puzzle.ch>
2025-04-24 12:46:39 +0200

    Finish logic of manual sync button

--- app/controllers/admin/update_people_controller.rb
+++ app/controllers/admin/update_people_controller.rb
@@ -2,4 +2,8 @@ class Admin::UpdatePeopleController < CrudController
   self.nesting = :admin
   self.permitted_attrs = %i[name]
   before_action :render_unauthorized_not_admin
+
+  def manual_sync
+    NightlyUpdatePeopleDataPtimeJob.perform_now
+  end
 end
