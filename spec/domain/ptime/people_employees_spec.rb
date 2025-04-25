748d5fe3b6d11c783b4b15999961aaaffba572ed
Manuel <moeri@puzzle.ch>
2025-04-25 13:41:29 +0200

    Update fixtures and outsource logic in people_employees_script to satisfy tests and rubocop & write logic for updating company of unemployees

--- spec/domain/ptime/people_employees_spec.rb
+++ spec/domain/ptime/people_employees_spec.rb
@@ -1,7 +1,54 @@
 require 'rails_helper'
 
 describe Ptime::PeopleEmployees do
+  subject(:people_employees) { described_class.new }
+
   before(:each) do
     enable_ptime_sync
+
+    Company.create!(name: 'Firma')
+    Company.create!(name: 'Ex-Mitarbeiter')
+
+    PersonRoleLevel.create!(level: 'S3')
+    PersonRoleLevel.create!(level: 'S4')
+    PersonRoleLevel.create!(level: 'S5')
+  end
+
+  it 'creates and updates people from ptime data' do
+    expect { people_employees.update_people_data }.to change(Person, :count).by(1)
+
+    longmax = Person.find_by(name: 'Longmax Smith')
+    emmeline = Person.find_by(name: 'Emmeline Charlotte')
+    charlie = Person.find_by(name: 'Charlie Ford')
+
+    expect(longmax.email).to eq('longmax@example.com')
+    expect(longmax.company.name).to eq('Firma')
+    expect(longmax.marital_status).to eq('single')
+    expect(longmax.nationality).to eq('ZW')
+    expect(longmax.title).to eq('BSc in Architecture')
+    expect(longmax.location).to eq('Bern')
+    expect(longmax.birthdate.to_s).to eq('1990-03-10 00:00:00 UTC')
+    expect(longmax.department.name).to eq('/sys')
+    expect(longmax.roles.first.name).to eq('Architect')
+
+    longmax_person_role = PersonRole.find_by_person_id(longmax.id)
+    expect(longmax_person_role.percent).to eq(100.0)
+    expect(longmax_person_role.person_role_level_id).to eq(PersonRoleLevel.find_by_level('S4').id)
+
+    expect(emmeline.email).to eq('emmeline@example.com')
+    expect(emmeline.company.name).to eq('Firma')
+    expect(emmeline.marital_status).to eq('single')
+    expect(emmeline.nationality).to eq('DE')
+    expect(emmeline.title).to eq('MSc in writing')
+    expect(emmeline.location).to eq('Genf')
+    expect(emmeline.birthdate.to_s).to eq('2001-01-05 00:00:00 UTC')
+    expect(emmeline.department.name).to eq('/dev/tre')
+    expect(emmeline.roles.first.name).to eq('Software Engineer')
+
+    emmeline_person_role = PersonRole.find_by_person_id(emmeline.id)
+    expect(emmeline_person_role.percent).to eq(80.0)
+    expect(emmeline_person_role.person_role_level_id).to eq(PersonRoleLevel.find_by_level('S5').id)
+
+    expect(charlie.company.name).to eq('Ex-Mitarbeiter') # Not employed
   end
 end
