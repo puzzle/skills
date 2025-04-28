7916f7edf0d33e4668cf473b0e3adf265354cfb8
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-04-28 13:34:10 +0200

    change picture name of 1.png to be more reasonable

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -61,7 +61,7 @@ describe :people do
 
 
   def fill_out_person_form
-    page.attach_file("avatar-uploader", Rails.root + 'app/assets/images/1.png')
+    page.attach_file("avatar-uploader", Rails.root + 'app/assets/images/anonymous_profile_picture.png')
     fill_in 'person_name', with: 'Hansjakobli'
     fill_in 'person_email', with: 'hanswurst@somemail.com'
     fill_in 'person_title', with: 'Wurstexperte'
@@ -96,7 +96,7 @@ describe :people do
     expect(page).to have_content("Hansjakobli")
 
     person = Person.find_by(name: 'Hansjakobli')
-    expect(person.picture.identifier).to eql('1.png')
+    expect(person.picture.identifier).to eql('anonymous_profile_picture.png')
     expect(person.email).to eql('hanswurst@somemail.com')
     expect(person.title).to eql('Wurstexperte')
     expect(person.person_roles.count).to eql(old_number_of_roles + 1)
