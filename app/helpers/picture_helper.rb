72351e5ecee9e99c6ba0c2dfdec531c45b7f3af5
Miguel Lehmann <lehmann@puzzle.ch>
2025-04-29 14:37:02 +0200

    edit helper for shorter methode

--- app/helpers/picture_helper.rb
+++ app/helpers/picture_helper.rb
@@ -2,17 +2,20 @@
 
 module PictureHelper
   def language_skill_svg(level)
-    native = I18n.available_locales.any? do |locale|
-      I18n.t('language_skills.native', locale: locale) == level
-    end
-    none = I18n.available_locales.any? do |locale|
-      I18n.t('language_skills.none', locale: locale) == level
-    end
-    if native
+    if matching_skill_level('native', level)
       return 'muttersprache'
-    elsif none
+    elsif matching_skill_level('none', level)
       return 'keine'
     end
+
     level
+  end
+
+  private
+
+  def matching_skill_level(translation_to_check, level)
+    I18n.available_locales.any? do |locale|
+      I18n.t("language_skills.#{translation_to_check}", locale: locale) == level
+    end
   end
 end
