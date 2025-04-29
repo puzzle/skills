ba6c0c8ab31021a684b228f6130432c01da39828
Jannik Pulfer <pulfer@puzzle.ch>
2025-04-29 14:16:53 +0200

    rename before action in manual ptime sync controller to match naming scheme

--- app/controllers/admin/manual_ptime_sync_controller.rb
+++ app/controllers/admin/manual_ptime_sync_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ManualPtimeSyncController < CrudController
   self.nesting = :admin
   before_action :render_unauthorized_not_admin
-  before_action :redirect_admin_without_ptime_sync
+  before_action :redirect_admin_unless_use_ptime_sync
 
   def manual_sync
     update_failed_names = NightlyUpdatePeopleDataPtimeJob.perform_now(is_manual_sync: true)
@@ -15,7 +15,7 @@ class Admin::ManualPtimeSyncController < CrudController
 
   private
 
-  def redirect_admin_without_ptime_sync
+  def redirect_admin_unless_use_ptime_sync
     redirect_to admin_index_path unless Skills.use_ptime_sync?
   end
 end
