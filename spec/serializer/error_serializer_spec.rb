6b3ed8e7660f6600286df71efc90a43b39040009
Miguel Lehmann <lehmann@puzzle.ch>
2025-04-29 14:19:26 +0200

    fix test that expects not translated text

--- spec/serializer/error_serializer_spec.rb
+++ spec/serializer/error_serializer_spec.rb
@@ -12,7 +12,7 @@ describe ErrorSerializer do
       expect(json[:errors].first).to include(:id)
       expect(json[:errors].first).to include(:title)
       expect(json[:errors].first[:id]).to eq(:company)
-      expect(json[:errors].first[:title]).to eq('Company muss ausgefüllt werden')
+      expect(json[:errors].first[:title]).to eq('Firma muss ausgefüllt werden')
       expect(json[:errors].second[:id]).to eq(:birthdate)
       expect(json[:errors].second[:title]).to eq('Geburtsdatum muss ausgefüllt werden')
     end
