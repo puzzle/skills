90505e98460c9138089f8043a4a52129ec48e829
Jannik Pulfer <pulfer@puzzle.ch>
2025-04-30 11:09:21 +0200

    Make rubocop happy and use flash.now instead of flash because render is used in manual_ptime_sync_controller

--- app/controllers/admin/manual_ptime_sync_controller.rb
+++ app/controllers/admin/manual_ptime_sync_controller.rb
@@ -3,21 +3,21 @@ class Admin::ManualPtimeSyncController < CrudController
   before_action :render_unauthorized_not_admin
   before_action :redirect_admin_unless_use_ptime_sync
 
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def manual_sync
     begin
       update_failed_names = NightlyUpdatePeopleDataPtimeJob.perform_now(is_manual_sync: true)
     rescue PtimeExceptions::PtimeClientError
-      flash[:alert] = t('.fetching_data_failed')
+      flash.now[:alert] = t('.fetching_data_failed')
       return render :index, status: :internal_server_error
     end
-
     if update_failed_names.any?
-      flash[:alert] = t('.failed_people_updates', names: update_failed_names.to_sentence)
-      render :index, status: :internal_server_error
-    else
-      flash[:notice] = t('.people_updated')
+      flash.now[:alert] = t('.failed_people_updates', names: update_failed_names.to_sentence)
+      return render :index, status: :internal_server_error
     end
+    flash.now[:notice] = t('.people_updated')
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
   private
 
