9c50b7fd69172272621dca8faaaffa386b209db2
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-05-08 14:38:05 +0200

    Add categories and sub categories to skills to rate

--- spec/features/edit_people_skills_spec.rb
+++ spec/features/edit_people_skills_spec.rb
@@ -132,6 +132,7 @@ describe :people do
       within '#default-skills' do
         not_rated_default_skills.each do |person_skill|
           expect(page).to have_content(person_skill.skill.title)
+          expect(page).to have_content("#{person_skill.skill.category.parent.title} - #{person_skill.skill.category.title}")
         end
 
         expect(page).to have_content('Azubi', count: not_rated_default_skills.count)
