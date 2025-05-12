8346266e77e8a82faf23af2726f82ed32a444444
Jannik Pulfer <pulfer@puzzle.ch>
2025-05-12 08:29:05 +0200

    Add explanatory comment to the department skill snapshot builder

--- app/domain/department_skills_snapshot_builder.rb
+++ app/domain/department_skills_snapshot_builder.rb
@@ -1,4 +1,11 @@
 class DepartmentSkillsSnapshotBuilder
+  # This will create snapshots of all departments with their respective skills and levels.
+  # Each snapshot is an instance of DepartmentSkillSnapshot which takes a hash of skills.
+  # This hash has the format:
+  # { <skill_id1> => [skill_level1, skill_level2], <skill_id2> => [skill_level1, skill_level2] }
+  # The count of skill_levels per skill_id is equal to the count of people that have rated that
+  # skill in a given department.
+
   def snapshot_all_departments
     Person.all.group_by(&:department_id).each do |department_id, people|
       department_skills = PeopleSkill
