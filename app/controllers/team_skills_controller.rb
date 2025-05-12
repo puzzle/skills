fde17e1c13f750dcdd1d6a7579057130ebda1c33
Manuel <moeri@puzzle.ch>
2025-05-12 15:39:26 +0200

    Add basic functions of graph to correctly dummy data

--- app/controllers/team_skills_controller.rb
+++ app/controllers/team_skills_controller.rb
@@ -1,6 +1,26 @@
 class TeamSkillsController < CrudController
 
   def index
+    colors = %w[red blue green purple orange]
+    color_index = 0
 
+    @data = {
+      labels: Date::MONTHNAMES.compact,
+      datasets: Skill.all.map do |skill|
+        color = colors[color_index]
+        color_index = (color_index + 1) % colors.size
+        {
+          label: skill.title,
+          data: Array.new(12) { rand(10) },
+          backgroundColor: color,
+          borderColor: color,
+          fill: false,
+          tension: 0.1
+        }
+      end
+    }.to_json
+
+    super
   end
 end
+
