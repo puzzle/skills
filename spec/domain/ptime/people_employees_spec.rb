59c29fd56844e42e95d4b2b975aef15b342807a8
Jannik Pulfer <pulfer@puzzle.ch>
2025-05-21 15:43:19 +0200

    Now show full error message when something goes wrong in ptime sync and update all tests to test new multi-provider setup

--- spec/domain/ptime/people_employees_spec.rb
+++ spec/domain/ptime/people_employees_spec.rb
@@ -15,51 +15,56 @@ describe Ptime::PeopleEmployees do
     # Longmax: Currently unemployed in skills DB -> Ptime data will update him to be employed
     # Emmeline: Not in skills DB -> Will be created from Ptime data
     # Charlie: Currently employed in skills Db -> Ptime data will update him to be unemployed
+    # Bob: Currently employed in skills Db -> Will be updated with data from second ptime provider
+
+    updated_company_employees = ptime_company_employees
+    updated_company_employees[:data].third[:attributes][:email] = 'charlie_ford@example.com'
+    stub_ptime_request(*ptime_company_request_data.values, updated_company_employees.to_json)
 
     longmax = people(:longmax)
     charlie = people(:charlie)
+    bob = people(:bob)
 
     longmax.update!(ptime_employee_id: 33)
+    longmax.update!(ptime_data_provider: 'Firma')
     longmax.update!(company: Company.find_by(name: 'Ex-Mitarbeiter'))
     charlie.update!(ptime_employee_id: 45)
+    charlie.update!(ptime_data_provider: 'Firma')
+    bob.update!(ptime_employee_id: 23)
+    bob.update!(ptime_data_provider: 'Partner')
 
     expect { people_employees.update_people_data }.to change(Person, :count).by(1)
 
     longmax.reload
     emmeline = Person.find_by(name: 'Emmeline Charlotte')
     charlie.reload
+    bob.reload
 
-    employee_data = ptime_employees_data
-    longmax_data = employee_data.first[:attributes]
-    emmeline_data = employee_data.second[:attributes]
-    charlie_data = employee_data.third[:attributes]
+    company_employee_data = updated_company_employees[:data]
 
-    charlie_data[:email] = 'charlie_ford@example.com'
+    longmax_data = company_employee_data.first[:attributes]
+    emmeline_data = company_employee_data.second[:attributes]
+    charlie_data = company_employee_data.third[:attributes]
+    bob_data = ptime_partner_employee_data.first[:attributes]
 
     check_person_data_updated(longmax, longmax_data)
     check_person_data_updated(emmeline, emmeline_data)
     check_person_data_updated(charlie, charlie_data)
+    check_person_data_updated(bob, bob_data)
   end
 
   it 'should return a list with the names of the people for which the update failed if the manual sync is used' do
-    people_employees_json = ptime_employees
-    longmax_attributes = people_employees_json[:data].first[:attributes]
-    longmax_attributes[:email] = ''
+    longmax_attributes, bob_attributes = stub_invalid_ptime_response
 
-    stub_ptime_request(people_employees_json.to_json)
-
-    expect(Ptime::PeopleEmployees.new.update_people_data(is_manual_sync: true)).to match_array([employee_full_name(longmax_attributes)])
+    expect(Ptime::PeopleEmployees.new.update_people_data(is_manual_sync: true)).to eql({"Firma" => [employee_full_name(longmax_attributes)], "Partner" => [employee_full_name(bob_attributes)]})
   end
 
   it 'should throw custom exception when nightly job is used' do
-    people_employees_json = ptime_employees
-    longmax_attributes = people_employees_json[:data].first[:attributes]
-    longmax_attributes[:email] = ''
-
-    stub_ptime_request(people_employees_json.to_json)
+    longmax_attributes, bob_attributes = stub_invalid_ptime_response
 
     expect { Ptime::PeopleEmployees.new.update_people_data }.to raise_error(PtimeExceptions::PersonUpdateWithPtimeDataFailed,
-      "Records were invalid while updating #{employee_full_name(longmax_attributes)} with data from PuzzleTime")
+      "Something went wrong while updating #{employee_full_name(longmax_attributes)} from provider Firma and #{employee_full_name(bob_attributes)} from
+      provider Partner. Please try again and, if necessary, check the data in PuzzleTime.".squish)
   end
 
   def check_person_data_updated(person, employee_data)
@@ -67,7 +72,7 @@ describe Ptime::PeopleEmployees do
 
     expect(person.name.eql?(employee_full_name(employee_data))).to eql(is_employed)
     expect(person.email.eql?(employee_data[:email])).to eql(is_employed)
-    expect(person.company.name).to eq(is_employed ? 'Firma' : 'Ex-Mitarbeiter')
+    expect(person.company.name).to eq(is_employed ? person.ptime_data_provider : 'Ex-Mitarbeiter')
     expect(person.marital_status.eql?(employee_data[:marital_status])).to eq(is_employed)
     expect(person.nationality.eql?(employee_data[:nationalities].first)).to eq(is_employed)
     expect(person.title.eql?(employee_data[:graduation])).to eq(is_employed)
