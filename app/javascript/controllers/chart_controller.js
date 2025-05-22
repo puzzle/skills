5f3e8965a482a9cb34bd7d1491d29193a77c514e
Manuel <moeri@puzzle.ch>
2025-05-22 15:21:21 +0200

    Add lots of translations and make chart display message when no data is selected

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,32 +1,63 @@
 import { Controller } from "@hotwired/stimulus"
 import Chart from "chart.js/auto"
 import { Colors } from 'chart.js';
+import annotationPlugin from 'chartjs-plugin-annotation';
 
 export default class extends Controller {
   static targets = ["canvas"]
   static values = {
-    dataset: String
+    dataset: String,
+    emptychart: String
   }
 
   connect() {
+    Chart.register(annotationPlugin);
     Chart.register(Colors);
 
-    const ctx = this.canvasTarget.getContext("2d")
+    console.log(this.emptychartValue)
 
-    const chartData = JSON.parse(this.datasetValue)
+    const ctx = this.canvasTarget.getContext("2d");
+    const chartData = JSON.parse(this.datasetValue);
+
+    const isEmpty = !chartData?.datasets?.length || chartData.datasets.every(ds => !ds.data?.length);
+
+    const options = {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+
+    if (isEmpty) {
+      options.plugins = {
+        annotation: {
+          annotations: {
+            noData: {
+              type: 'label',
+              content: [ this.emptychartValue ],
+              position: {
+                x: '50%',
+                y: '50%'
+              },
+              font: {
+                size: 32,
+                weight: 'bold'
+              },
+              color: 'gray',
+              textAlign: 'center'
+            }
+          }
+        }
+      };
+    }
 
     this.chart = new Chart(ctx, {
       type: 'bar',
       data: chartData,
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    })
+      options: options
+    });
   }
 
   disconnect() {
