3d676598f47dedc7a1189a70aedc45c4c9701b38
Jannik Pulfer <pulfer@puzzle.ch>
2025-05-23 16:04:56 +0200

    Start implementing admin view which allows to delete ex-employees and people that are not updated by puzzle skills

--- app/controllers/admin/people_management_controller.rb
+++ app/controllers/admin/people_management_controller.rb
@@ -1,3 +1,15 @@
 class Admin::PeopleManagementController < CrudController
   self.nesting = :admin
+  def self.model_class
+    Person
+  end
+
+  def destroy_person
+    @person = Person.find(params['person_id'])
+    if @person.destroy.destroyed?
+      flash.now[:notice] = t('crud.destroy.flash.success', model: @person)
+    else
+      flash.now[:alert] = t('crud.destroy.flash.failure', model: @person)
+    end
+  end
 end
