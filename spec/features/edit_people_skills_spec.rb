43bbfd97dfad0c56764b7ee5bda640b8434f61c3
Jannik Pulfer <pulfer@puzzle.ch>
2025-06-11 09:51:47 +0200

    Format code and fix profile scroll to spec

--- spec/features/edit_people_skills_spec.rb
+++ spec/features/edit_people_skills_spec.rb
@@ -224,7 +224,7 @@ describe :people do
       fill_in "level_#{first_unrated_skill.id}", with: 4
       expect(page).to have_content(first_unrated_skill.title)
       visit person_people_skills_path(bob, rating: 0)
-      expect(page).to  have_content(first_unrated_skill.title)
+      expect(page).to have_content(first_unrated_skill.title)
     end
   end
 
