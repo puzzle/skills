121f55db3e54be8e69ca1b5dcd3f12886e5afb39
Manuel <moeri@puzzle.ch>
2025-06-16 11:51:29 +0200

    Make rubocop  😄

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class ContributionsController < People::PersonRelationsController
-  self.permitted_attrs = %i[title link person_id display_in_cv year_from year_to month_from month_to]
+  self.permitted_attrs = %i[title link person_id display_in_cv year_from year_to month_from
+                            month_to]
 end
