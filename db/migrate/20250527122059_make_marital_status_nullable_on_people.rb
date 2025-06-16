638a0322c6a8e05c6432873ed5ffa7617ae76829
Jannik Pulfer <pulfer@puzzle.ch>
2025-06-16 11:18:11 +0200

    Fix typo in migration name and remove .sec from gitignore

--- db/migrate/20250527122059_make_marital_status_nullable_on_people.rb
+++ db/migrate/20250527122059_make_marital_status_nullable_on_people.rb
@@ -1,4 +1,4 @@
-class MakeMartialStatusNullableOnPeople < ActiveRecord::Migration[8.0]
+class MakeMaritalStatusNullableOnPeople < ActiveRecord::Migration[8.0]
   def change
     change_column_null :people, :marital_status, true, 0
   end
