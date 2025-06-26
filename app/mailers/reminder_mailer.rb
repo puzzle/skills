074e7caed223966065cc275a3be10d3e09439df1
Miguel Lehmann <lehmann@puzzle.ch>
2025-06-26 15:28:28 +0200

    write tests for the mailer

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,6 +1,5 @@
 class ReminderMailer < ApplicationMailer
-  def update_user_reminder_email
-    @user = user
-    mail(to: @user.email, subject: "Time to update your profile!")
+  def update_user_reminder_email(person)
+    mail(to: person.email, subject: "Time to update your profile!")
   end
 end
--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe ReminderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify" do
+    let(:person) {people(:bob)}
+    let(:mail) { ReminderMailer.update_user_reminder_email(person) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Time to update your profile!")
+      expect(mail.to).to eq([person.email])
+      ## expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.parts.to_s).to match('[#<Mail::Part:26032, Multipart: false, Headers: <Content-Type: text/plain>>, #<Mail::Part:26040, Multipart: false, Headers: <Content-Type: text/html>>]')
+    end
+  end
 end
