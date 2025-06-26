4d83fdcb0b9c7fc82bd3645729a1ce7745ca5e5a
Miguel Lehmann <lehmann@puzzle.ch>
2025-06-26 14:19:32 +0200

    write tests for the logic of the job

--- spec/jobs/send_update_profile_reminder_mail_job_spec.rb
+++ spec/jobs/send_update_profile_reminder_mail_job_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe SendUpdateProfileReminderMailJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'when a person is outdated and all reminders are active' do
+    let(:person) { people(:bob) }
+    let(:company) { companies(:firma) }
+
+    before do
+      allow(Company).to receive(:find).with(id: person.company_id).and_return(company)
+      allow(Person).to receive(:find_each).and_yield(person)
+      person.update_column(:updated_at, 2.years.ago)
+    end
+
+    it 'should send a reminder email' do
+      mailer_double = double('ReminderMailer', deliver_later: true)
+      expect(ReminderMailer).to receive(:update_user_reminder_email).with(person).and_return(mailer_double)
+
+      SendUpdateProfileReminderMailJob.new.perform
+    end
+  end
+
+  describe 'when company reminders are inactive' do
+    let(:person) { people(:bob) }
+    let(:company) { companies(:partner) }
+
+    before do
+      allow(Company).to receive(:find).with(id: person.company_id).and_return(company)
+      allow(Person).to receive(:find_each).and_yield(person)
+      person.update_column(:updated_at, 2.years.ago)
+    end
+
+    it 'should send no reminder email' do
+      expect(ReminderMailer).not_to receive(:update_user_reminder_email)
+
+      SendUpdateProfileReminderMailJob.new.perform
+    end
+  end
+
+  describe 'when the person is not outdated' do
+    let(:person) { people(:bob) }
+    let(:company) { companies(:firma) }
+
+    before do
+      allow(Company).to receive(:find).with(id: person.company_id).and_return(company)
+      allow(Person).to receive(:find_each).and_yield(person)
+      person.update_column(:updated_at, 2.month.ago)
+    end
+
+    it 'should send no reminder email' do
+      expect(ReminderMailer).not_to receive(:update_user_reminder_email)
+
+      SendUpdateProfileReminderMailJob.new.perform
+    end
+  end
+
+  describe 'when person reminders are inactive' do
+    let(:person) { people(:alice) }
+    let(:company) { companies(:firma) }
+
+    before do
+      allow(Company).to receive(:find).with(id: person.company_id).and_return(company)
+      allow(Person).to receive(:find_each).and_return(:person)
+      person.update_column(:updated_at, 2.years.ago)
+    end
+
+    it 'should send no reminder email' do
+      expect(ReminderMailer).not_to receive(:update_user_reminder_email)
+
+      SendUpdateProfileReminderMailJob.new.perform
+    end
+  end
 end
