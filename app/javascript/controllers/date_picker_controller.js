481ed9521a4bc5cd79d9e0629403b6fb31cac10e
Manuel <moeri@puzzle.ch>
2025-06-26 10:39:23 +0200

    Add functionality for copying values

--- app/javascript/controllers/date_picker_controller.js
+++ app/javascript/controllers/date_picker_controller.js
@@ -3,14 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="date-picker"
 export default class extends Controller {
 
-  static targets = [ "hideable" ]
+  static targets = [ "hideable", "monthTo", "yearTo"]
 
   toggleTargets() {
+    const wasHidden = this.hideableTargets[0].hidden;
+
     this.hideableTargets.forEach((el) => {
       el.hidden = !el.hidden;
-      el.querySelectorAll('select').forEach((select) => {
-        select.value = "";
-      });
+
+      if (el.hidden) {
+        el.querySelectorAll('select').forEach((select) => {
+          select.value = "";
+        });
+      } else if (wasHidden) {
+        const monthFrom = this.element.querySelector('select[name$="[month_from]"]')
+        const yearFrom = this.element.querySelector('select[name$="[year_from]"]')
+
+        if (monthFrom && this.hasMonthToTarget) {
+          this.monthToTarget.value = monthFrom.value;
+        }
+        if (yearFrom && this.hasYearToTarget) {
+          this.yearToTarget.value = yearFrom.value;
+        }
+      }
     });
   }
 }
