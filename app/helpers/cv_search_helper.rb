943e6b7855627a8c50e95bd5eaedb613408da962
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-07-01 13:54:11 +0200

    move query to own helper method

--- app/helpers/cv_search_helper.rb
+++ app/helpers/cv_search_helper.rb
@@ -5,4 +5,8 @@ module CvSearchHelper
   def found_in_skills?(result)
     result[:found_in][:attribute].include?('Skills')
   end
+
+  def query
+    params[:q].split(",").find {|keyword| result[:found_in][:keywords_in_attribute].include?(keyword)}
+  end
 end
