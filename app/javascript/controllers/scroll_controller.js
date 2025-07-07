d12d4201633539dd806834ecb16b3693fd026069
Manuel <moeri@puzzle.ch>
2025-07-07 11:28:48 +0200

    Fix offset of top without breaking the scrolling

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -9,6 +9,8 @@ export default class extends Controller {
         document.removeEventListener('scroll', () => {});
         ['turbo:frame-load', 'turbo:load'].forEach(e => {
             window.addEventListener(e, () => {
+                this.offsetY = this.parentTarget.getBoundingClientRect().top + window.scrollY;
+                // this.listTarget.style.top = `${this.offsetY}px`;
                 document.addEventListener("scroll", () => {
                     this.highlight();
                 });
