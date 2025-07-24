3c6e4cbf5221601f4aa37cf66a620139fd87f17e
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-07-24 09:41:58 +0200

    Add discard to departments so we can keep department_skill_snapshots even after a department is deleted

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class Department < ApplicationRecord
+  include Discard::Model
+
   has_many :people, dependent: :restrict_with_error
 
   validates :name, presence: true, length: { maximum: 100 }
   scope :list, -> { order('name asc') }
+
+  default_scope { kept }
+
+  def destroy
+    self.discard
+  end
 
   def to_s
     name
