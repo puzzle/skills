bfb0d2a65701eb431bb75ab403089406fb858c4a
nevio18324 <141240169+nevio18324@users.noreply.github.com>
2025-07-24 10:14:20 +0200

    Make tooltips disappear when they get clicked (#992)

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,9 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 import * as bootstrap from "bootstrap"
 
 export default class extends Controller {
     connect() {
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    }
+
+    hide(event) {
+        const element = event.currentTarget;
+        const tooltip = bootstrap.Tooltip.getInstance(element);
+        tooltip.hide();
     }
 }
