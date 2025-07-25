bfaba2997cb6ab7e635c31143a3c407e8481fb61
Jannik Pulfer <pulfer@puzzle.ch>
2025-07-25 09:26:52 +0200

    Remove unnecessary specs

--- spec/features/department_skill_snapshots_spec.rb
+++ spec/features/department_skill_snapshots_spec.rb
@@ -19,30 +19,4 @@ describe 'Department Skill Snapshots', type: :feature, js: true do
 
     expect(page).to have_selector("canvas")
   end
-
-  it "successfully searches and selects a department from the dropdown" do
-    department = departments(:sys)
-    department_slim_select = page.find_all('.ss-main')[0]
-    department_slim_select.click
-
-    department_search_input = page.find('.ss-search')
-    department_search_input.fill_in with: department.name
-
-    page.find('.ss-option', text: department.name).click
-
-    expect(page).to have_field('department_id', with: department.id.to_s, visible: :hidden)
-  end
-
-  it "successfully searches and selects a skill from the dropdown" do
-    skill = skills(:junit)
-    skill_slim_select = page.find_all('.ss-main')[1]
-    skill_slim_select.click
-
-    skill_search_input = page.find('.ss-search')
-    skill_search_input.fill_in with: skill.title
-
-    page.find('.ss-option', text: skill.title).click
-
-    expect(page).to have_field('skill_id', with: skill.id.to_s, visible: :hidden)
-  end
 end
