fcd0993decbea6d34dc00b9c9978eeca659ebaf2
Jannik Pulfer <pulfer@puzzle.ch>
2025-07-25 08:41:58 +0200

    Fix flaky spec helper

--- spec/support/people_skills_helper.rb
+++ spec/support/people_skills_helper.rb
@@ -23,11 +23,11 @@ module PeopleSkillsHelpers
   def validate_people_skill(person, skill_name)
     expect(page).to have_content(skill_name)
     people_skill = people_skill_from_skill_name(person, skill_name)
-    skill_div = find("#default-skill-#{people_skill.skill.id}")
+    skill_div = find("#default-skill-#{people_skill.skill_id}")
     within skill_div do
       expect(page).to have_field("level_#{people_skill.skill_id}", with: people_skill.level)
       validate_skill_level_label(people_skill.level)
-      validate_interest(people_skill.interest)
+      validate_interest(people_skill.interest, people_skill.skill_id)
       expect(find("#certificate_#{people_skill.skill_id}").checked?).to eq people_skill.certificate
       expect(find("#core_competence_#{people_skill.skill_id}").checked?).to eq people_skill.core_competence
     end
@@ -42,13 +42,13 @@ module PeopleSkillsHelpers
     expect(page).to have_selector('[data-people-skills-target="label"]', text: /#{skill_level_label}/i)
   end
 
-  def validate_interest(interest)
-    star_labels = page.find_all("label[id^='star']", visible: false).to_a.reverse
-    star_labels.each_with_index do |label, index|
+  def validate_interest(interest, skill_id)
+    1.upto 5 do |index|
       body = page.document.find('body')
       body.send_keys(:tab)
       body.hover
-      if index < interest
+      label = find("#star-label#{index}#{skill_id}")
+      if index <= interest
         expect(label).to match_style(color: 'rgb(255, 199, 0)').or(match_style(color: 'rgba(255, 199, 0, 1)'))
       else
         expect(label).to match_style(color: 'rgb(204, 204, 204)').or(match_style(color: 'rgba(204, 204, 204, 1)'))
