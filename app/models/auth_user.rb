7b6f880ccd53822a48d1025b5c83ef420ea7a13d
Manuel <moeri@puzzle.ch>
2025-07-28 13:23:54 +0200

    Modify ldap_username to be correct in api and make rubocop happy

--- app/models/auth_user.rb
+++ app/models/auth_user.rb
@@ -6,16 +6,18 @@ class AuthUser < ApplicationRecord
 
   class << self
     def from_omniauth(auth)
-      person = where(uid: auth.uid).first_or_create do |user|
-        user.name = auth.info.name
-        user.email = auth.info.email
-        user.ldap_username = auth.extra.raw_info.pitc.uid
-      end
+      person = where(uid: auth.uid).first_or_create { |user| initialize_user(user, auth) }
       person.last_login = Time.zone.now
       set_admin(person, auth)
     end
 
     private
+
+    def initialize_user(user, auth)
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.ldap_username = auth.extra.raw_info.pitc.uid
+    end
 
     def set_admin(person, auth)
       person.is_admin = role?(auth, AuthConfig.admin_role)
