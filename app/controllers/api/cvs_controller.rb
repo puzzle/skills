e945724c0611a1e811d9e237be312574f05a6be6
Miguel Lehmann <lehmann@puzzle.ch>
2026-02-16 11:18:56 +0100

    test: add working tests for contoller #1020

--- app/controllers/api/cvs_controller.rb
+++ app/controllers/api/cvs_controller.rb
@@ -2,30 +2,25 @@ class Api::CvsController < Api::ApplicationController
   def index
     people = Person
              .includes(
-               :company,
-               :department,
+               :company, :department,
                :language_skills,
-               :projects,
-               :activities,
+               :projects, :activities,
                :advanced_trainings,
-               :educations,
-               :contributions,
-               :skills,
+               :educations, :contributions, :skills,
                person_roles: [:role, :person_role_level]
              )
 
-    render json: {
-      data: ActiveModelSerializers::SerializableResource.new(
-        people,
-        each_serializer: CvSerializer,
-        anon: cast_boolean(params[:anon])
-      )
-    }
+    render_collection(people, CvSerializer)
   end
 
   private
 
-  def cast_boolean(value)
-    ActiveModel::Type::Boolean.new.cast(value)
+  def render_collection(collection, serializer)
+    render json: {
+      data: ActiveModelSerializers::SerializableResource.new(
+        collection,
+        each_serializer: serializer
+      )
+    }
   end
 end
