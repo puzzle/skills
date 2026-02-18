677a4993fc8e9a337941ddee67ab2849c6676368
Miguel Lehmann <lehmann@puzzle.ch>
2026-02-18 06:18:30 +0100

    feat: implement editor role #948

--- db/seeds/development/01_auth_users.rb
+++ db/seeds/development/01_auth_users.rb
@@ -6,14 +6,17 @@ seeder = AuthUserSeeder.new
 
 auth_users = [
   {
-    first_name: 'Carl Albrecht', last_name: 'Conf Admin', conf_admin: true, admin: true
+    first_name: 'Carl Albrecht', last_name: 'Conf Admin', conf_admin: true, admin: true, editor: false
   },
   {
-    first_name: 'Andreas', last_name: 'Admin', admin: true, conf_admin: false
+    first_name: 'Andreas', last_name: 'Admin', admin: true, conf_admin: false, editor: false
   },
   {
-    first_name: 'Ursula', last_name: 'User', admin: false, conf_admin: false
-  }
+    first_name: 'Eddy', last_name: 'Editor', admin: false, conf_admin: false, editor: true
+  },
+  {
+    first_name: 'Ursula', last_name: 'User', admin: false, conf_admin: false, editor: false
+  },
 ]
 
 seeder.seed_auth_users(auth_users)
