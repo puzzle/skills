2d2507b12fc3dd08cbc130ed7f4a8a7e5b28671b
Miguel Lehmann <lehmann@puzzle.ch>
2026-03-04 10:00:39 +0100

    test: update tests for new auth logic #1020

--- spec/controllers/api/cvs_controller_spec.rb
+++ spec/controllers/api/cvs_controller_spec.rb
@@ -2,92 +2,109 @@ require 'rails_helper'
 
 RSpec.describe Api::CvsController, type: :controller do
   describe 'GET #index' do
-    let(:api_token) { 'test-token' }
+    let(:username) { 'test-user' }
+    let(:password) { 'test-password' }
 
     before do
-      allow(ENV).to receive(:fetch).with('CVS_API_TOKEN', nil).and_return(api_token)
+      allow(ENV).to receive(:fetch).with('API_USERNAME', 'Hallo').and_return(username)
+      allow(ENV).to receive(:fetch).with('API_PASSWORD', 'Hallo').and_return(password)
 
-      request.headers['Authorization'] = "Token token=#{api_token}"
+      credentials = ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
+      request.headers['Authorization'] = credentials
     end
 
     subject(:request_call) { get :index }
 
-    it 'returns http success' do
-      request_call
-      expect(response).to have_http_status(:ok)
+    context 'with valid credentials' do
+      it 'returns http success' do
+        request_call
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns cvs wrapped in data.data' do
+        request_call
+
+        expect(json).to have_key('data')
+        expect(json['data']).to have_key('data')
+        expect(json['data']['data']).to be_an(Array)
+      end
+
+      it 'returns cvs with expected jsonapi structure' do
+        request_call
+
+        cv = json['data']['data'].first
+
+        expect(cv).to include(
+                        'id',
+                        'type',
+                        'attributes',
+                        'relationships'
+                      )
+
+        expect(cv['type']).to eq('cv')
+      end
+
+      it 'includes main attributes on cv' do
+        request_call
+
+        attributes = json['data']['data'].first['attributes']
+
+        expect(attributes).to include(
+                                'name',
+                                'title',
+                                'email',
+                                'birthdate',
+                                'location',
+                                'nationality',
+                                'marital_status',
+                                'competence_notes',
+                                'picture_url',
+                                'roles',
+                                'skills',
+                                'language_skills',
+                                'projects',
+                                'activities',
+                                'educations',
+                                'advanced_trainings',
+                                'contributions'
+                              )
+      end
+
+      it 'includes company and department relationships' do
+        request_call
+
+        relationships = json['data']['data'].first['relationships']
+
+        expect(relationships).to include('company', 'department')
+
+        expect(relationships['company']['data']).to include(
+                                                      'id',
+                                                      'type'
+                                                    )
+
+        expect(relationships['department']['data']).to include(
+                                                         'id',
+                                                         'type'
+                                                       )
+      end
+
+      it 'returns all people as cvs' do
+        request_call
+
+        expect(json['data']['data'].size).to eq(Person.count)
+      end
     end
+    
+    context 'with invalid credentials' do
+      before do
+        credentials = ActionController::HttpAuthentication::Basic.encode_credentials('wrong', 'creds')
+        request.headers['Authorization'] = credentials
+      end
 
-    it 'returns cvs wrapped in data.data' do
-      request_call
-
-      expect(json).to have_key('data')
-      expect(json['data']).to have_key('data')
-      expect(json['data']['data']).to be_an(Array)
-    end
-
-    it 'returns cvs with expected jsonapi structure' do
-      request_call
-
-      cv = json['data']['data'].first
-
-      expect(cv).to include(
-                      'id',
-                      'type',
-                      'attributes',
-                      'relationships'
-                    )
-
-      expect(cv['type']).to eq('cv')
-    end
-
-    it 'includes main attributes on cv' do
-      request_call
-
-      attributes = json['data']['data'].first['attributes']
-
-      expect(attributes).to include(
-                              'name',
-                              'title',
-                              'email',
-                              'birthdate',
-                              'location',
-                              'nationality',
-                              'marital_status',
-                              'competence_notes',
-                              'picture_url',
-                              'roles',
-                              'skills',
-                              'language_skills',
-                              'projects',
-                              'activities',
-                              'educations',
-                              'advanced_trainings',
-                              'contributions'
-                            )
-    end
-
-    it 'includes company and department relationships' do
-      request_call
-
-      relationships = json['data']['data'].first['relationships']
-
-      expect(relationships).to include('company', 'department')
-
-      expect(relationships['company']['data']).to include(
-                                                    'id',
-                                                    'type'
-                                                  )
-
-      expect(relationships['department']['data']).to include(
-                                                       'id',
-                                                       'type'
-                                                     )
-    end
-
-    it 'returns all people as cvs' do
-      request_call
-
-      expect(json['data']['data'].size).to eq(Person.count)
+      it 'returns unauthorized' do
+        request_call
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
