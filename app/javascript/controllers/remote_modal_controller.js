8abec4684102529e5bb8853273049d559275ee3b
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-03-10 14:57:34 +0100

    Fix: skillset not scrollable when returning #1046

--- app/javascript/controllers/remote_modal_controller.js
+++ app/javascript/controllers/remote_modal_controller.js
@@ -3,20 +3,24 @@ import { Modal } from "bootstrap"
 
 export default class extends Controller {
     connect() {
-        this.modal = new Modal(this.element);
-        this.modal.show();
+        this.modal = new Modal(this.element)
+        this.modal.show()
+
+        this.beforeCache = this.beforeCache.bind(this)
+        document.addEventListener("turbo:before-cache", this.beforeCache)
     }
 
     disconnect() {
-        this.modal.dispose();
+        document.removeEventListener("turbo:before-cache", this.beforeCache)
+        if (this.modal) this.modal.dispose()
     }
 
-    isOpen() {
-        return this.element.classList.contains("show")
+    beforeCache() {
+        if (this.modal) this.modal.dispose()
+
+        // Remove bootstrap modal leftovers
+        document.body.classList.remove("modal-open")
+        document.body.style.removeProperty("overflow")
+        document.body.style.removeProperty("padding-right")
     }
 }
-
-//Before caching dispose the modal else we can't interact with the page after it was loaded from the cache.
-document.addEventListener("turbo:before-cache", () => {
-    this.modal.dispose();
-});
