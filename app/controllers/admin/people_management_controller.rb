18e4ac4d289b41245af66a4527f8bfe2e1ad745c
schiltpzzle <schilt@puzzle.ch>
2026-03-24 10:31:29 +0100

    fix: add sorting for not synced profiles #1047

--- app/controllers/admin/people_management_controller.rb
+++ app/controllers/admin/people_management_controller.rb
@@ -10,15 +10,22 @@ class Admin::PeopleManagementController < CrudController
     @unemployed_people = Person.unemployed
     @not_synced_profiles = Person.where(ptime_employee_id: nil)
                                  .or(Person.where(ptime_data_provider: nil))
+
+    @list_entries = @unemployed_people
+
+    if Skills.use_ptime_sync?
+      @list_entries = @list_entries.or(@not_synced_profiles)
+    end
   end
 
   def list_entries
     sortable = sortable?(params[:sort])
     if sortable || default_sort
       clause = [sortable ? sort_expression : nil, default_sort]
+      @list_entries.reorder(Arel.sql(clause.compact.join(', ')))
+    else
+      @list_entries
     end
-
-    @unemployed_people.reorder(Arel.sql(clause.compact.join(', ')))
   end
   def destroy_person
     @person = Person.find(params['person_id'])
