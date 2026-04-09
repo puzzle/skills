f7461f9f77a7c44c041ca4743687daa050baeabb
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-04-09 15:11:07 +0200

    Fix: javascript syntax error #1052

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -34,7 +34,7 @@ export default class extends Controller {
                 behavior: "smooth"
             });
 
-            timeout = setTimeout(() => {
+            setTimeout(() => {
                 if (!this.isElementInViewport(mark)) {
                     mark.scrollIntoView(false)
                 }
