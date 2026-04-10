23ea496ed6c20eb1d67b696f8537bcdc7077d22e
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-04-10 09:31:34 +0200

    Feat: remove comment #1052

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -44,12 +44,11 @@ export default class extends Controller {
 
     isElementInViewport(element) {
         const rect = element.getBoundingClientRect();
-
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
 
