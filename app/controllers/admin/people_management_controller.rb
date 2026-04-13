bc3da217551d241fca92a34f1d9a9b2288d42d81
schiltpzzle <schilt@puzzle.ch>
2026-04-13 16:51:24 +0200

    feat: merge unemployed and not synced controller #1047

--- app/controllers/admin/people_management_controller.rb
+++ app/controllers/admin/people_management_controller.rb
@@ -2,6 +2,19 @@ class Admin::PeopleManagementController < CrudController
   self.nesting = :admin
   before_action :render_unauthorized_not_conf_admin
 
+  def self.model_class
+    Person
+  end
+
+  def index
+    if params[:people] == 'unemployed'
+      @people = Person.unemployed
+    elsif params[:people] == 'not_synced'
+      @people = Person.where(ptime_employee_id: nil)
+                      .or(Person.where(ptime_data_provider: nil))
+    end
+  end
+
   def destroy_person
     @person = Person.find(params['person_id'])
     if @person.destroy.destroyed?
