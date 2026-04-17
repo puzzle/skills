3973194cf7d80e618222eb2dee894ff576aad582
Livio Canobbio <canobbio@puzzle.ch>
2026-04-17 14:48:57 +0200

    test(config): change all conf admin to admin  #1107

--- spec/controllers/manual_ptime_sync_controller_spec.rb
+++ spec/controllers/manual_ptime_sync_controller_spec.rb
@@ -10,7 +10,7 @@ describe Admin::ManualPtimeSyncController, type: :controller do
     expect(response).to redirect_to(admin_index_path)
   end
 
-  it 'should redirect to root when user is not conf admin' do
+  it 'should redirect to root when user is not admin' do
     sign_in(auth_users(:user))
     allow(Skills).to receive(:use_ptime_sync?).and_return(true)
 
