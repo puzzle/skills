11315143f8852a8691bbd8fc1dd9900333f1105d
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-04-20 11:22:17 +0200

    feat: remove all api endpoints and tests #1088

--- app/controllers/api/educations_controller.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Api::EducationsController < Api::PersonRelationsController
-  self.permitted_attrs = %i[location title month_to year_to month_from year_from person_id]
-end
--- app/controllers/api/person_role_levels_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class Api::PersonRoleLevelsController < Api::CrudController
-end
