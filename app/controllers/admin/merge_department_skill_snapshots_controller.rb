5e20d1b0de732baf47bc3756992463ff293b9d43
Miguel Lehmann <lehmann@puzzle.ch>
2026-06-03 11:54:46 +0200

    fix: rubocop #948

--- app/controllers/admin/merge_department_skill_snapshots_controller.rb
+++ app/controllers/admin/merge_department_skill_snapshots_controller.rb
@@ -14,13 +14,7 @@ class Admin::MergeDepartmentSkillSnapshotsController < ApplicationController
     @merge_department_snapshots_form = build_form
     return invalid_form unless @merge_department_snapshots_form.valid?
 
-    old_departments = @merge_department_snapshots_form.selected_old_departments
-    new_department = @merge_department_snapshots_form.selected_new_department
-
-    merge_department_snapshots(old_departments, new_department)
-    flash[:notice] = t('.success',
-                       old_department_names: old_departments.pluck(:name).join(', '),
-                       new_department_name: new_department.name)
+    perform_merge
 
     @merge_histories = DepartmentMergeHistory.order(created_at: :desc)
 
@@ -31,6 +25,22 @@ class Admin::MergeDepartmentSkillSnapshotsController < ApplicationController
   end
 
   private
+
+  def perform_merge
+    old_departments = @merge_department_snapshots_form.selected_old_departments
+    new_department = @merge_department_snapshots_form.selected_new_department
+
+    merge_department_snapshots(old_departments, new_department)
+    set_success_flash(old_departments, new_department)
+  end
+
+  def set_success_flash(old_departments, new_department)
+    flash.now[:notice] = t(
+      '.success',
+      old_department_names: old_departments.pluck(:name).join(', '),
+      new_department_name: new_department.name
+    )
+  end
 
   def build_form
     MergeDepartmentSkillForm.new(merge_params)
@@ -48,8 +58,10 @@ class Admin::MergeDepartmentSkillSnapshotsController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.update('flash-messages', partial: 'layouts/flash',
-                                                                   collection: [:notice, :alert], as: :level)
+        render turbo_stream: turbo_stream.update(
+          'flash-messages', partial: 'layouts/flash',
+                            collection: [:notice, :alert], as: :level
+        )
       end
       format.html { redirect_to new_admin_merge_department_skill_snapshot_path }
     end
