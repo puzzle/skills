24fc8d0a7d1ebdd6122afff519e863ffb16c4ba8
Miguel Lehmann <lehmann@puzzle.ch>
2026-06-05 14:49:56 +0200

    fix: make migration rollback work

--- db/migrate/20260512095818_remove_conf_admin_from_auth_user.rb
+++ db/migrate/20260512095818_remove_conf_admin_from_auth_user.rb
@@ -1,5 +1,5 @@
 class RemoveConfAdminFromAuthUser < ActiveRecord::Migration[8.1]
   def change
-    remove_column :auth_users, :is_conf_admin
+    remove_column :auth_users, :is_conf_admin, :boolean, default: false, null: false
   end
 end
