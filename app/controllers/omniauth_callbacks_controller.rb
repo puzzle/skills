1eb17ea0e2dd9f376efd67e088505b82d4e5ca76
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-06-05 08:43:52 +0200

    fix: bring back action skip #1177

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :keycloak_openid
 
   def keycloak_openid
     omniauth_auth = request.env['omniauth.auth']
