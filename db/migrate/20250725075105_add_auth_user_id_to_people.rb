b3a387ea8dcdb181cd18c0c821cb6146abc52f6d
Miguel Lehmann <lehmann@puzzle.ch>
2026-06-05 17:16:38 +0200

    fix: switch to change

--- db/migrate/20250725075105_add_auth_user_id_to_people.rb
+++ db/migrate/20250725075105_add_auth_user_id_to_people.rb
@@ -1,17 +1,13 @@
 class AddAuthUserIdToPeople < ActiveRecord::Migration[8.0]
-    def up
-      if column_exists?(:people, :auth_user_id)
-        remove_column :people, :auth_user_id
-      end
-
+    def change
       add_reference :people,
                     :auth_user,
                     foreign_key: { to_table: :auth_users },
                     type: :integer
     end
 
-    def down
-      remove_foreign_key :people, column: :auth_user_id if foreign_key_exists?(:people, column: :auth_user_id)
-      remove_column :people, :auth_user_id if column_exists?(:people, :auth_user_id)
-    end
+    # def down
+    #   remove_foreign_key :people, column: :auth_user_id if foreign_key_exists?(:people, column: :auth_user_id)
+    #   remove_column :people, :auth_user_id if column_exists?(:people, :auth_user_id)
+    # end
 end
