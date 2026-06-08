489ba6cc32eb6b2e53016110f762e51f4872974b
Miguel Lehmann <lehmann@puzzle.ch>
2026-06-08 08:13:56 +0200

    refactor: remove unused code

--- db/migrate/20250725075105_add_auth_user_id_to_people.rb
+++ db/migrate/20250725075105_add_auth_user_id_to_people.rb
@@ -5,9 +5,4 @@ class AddAuthUserIdToPeople < ActiveRecord::Migration[8.0]
                     foreign_key: { to_table: :auth_users },
                     type: :integer
     end
-
-    # def down
-    #   remove_foreign_key :people, column: :auth_user_id if foreign_key_exists?(:people, column: :auth_user_id)
-    #   remove_column :people, :auth_user_id if column_exists?(:people, :auth_user_id)
-    # end
 end
