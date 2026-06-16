344e4339983fa46d93c99d6c2c22e50834598ef5
Miguel Lehmann <lehmann@puzzle.ch>
2026-06-16 16:49:05 +0200

    fix: rework the slimselect dropdown to delete selected entries #1109

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -4,17 +4,17 @@ import SlimSelect from 'slim-select';
 // Connects to data-controller="dropdown-links"
 export default class extends Controller {
     static targets = ["dropdown"]
+
     static values = {
         autoWidth: { type: Boolean, default: false },
-        contentPosition:  { type: String, default: 'absolute' },
+        contentPosition: { type: String, default: 'absolute' },
         multiple: { type: Boolean, default: false },
-        placeholderText: { type: String, default: "" }
+        placeholderText: { type: String, default: "" },
+        hideSelected: { type: Boolean, default: false }
     }
 
     connect() {
         if (!this.hasDropdownTarget) return;
-
-        this.sortOptions();
 
         const settings = {
             contentPosition: this.contentPositionValue,
@@ -23,19 +23,24 @@ export default class extends Controller {
         if (this.multipleValue) {
             settings.allowDeselect = true;
             settings.closeOnSelect = false;
-            settings.hideSelected = true;
+            settings.hideSelected = this.hideSelectedValue;
 
             if (this.hasPlaceholderTextValue) {
                 settings.placeholderText = this.placeholderTextValue;
             }
         }
 
-        this.slimSelectDropdown = new SlimSelect({
+        const slimSelectDropdown = new SlimSelect({
             select: this.dropdownTarget,
             settings: settings,
             events: {
                 searchFilter: (option, search) => {
-                    return option.text.toLowerCase().replace(/\s/g, '').indexOf(search.toLowerCase().replace(/\s/g, '')) !== -1
+                    return option.text
+                        .toLowerCase()
+                        .replace(/\s/g, '')
+                        .indexOf(
+                            search.toLowerCase().replace(/\s/g, '')
+                        ) !== -1;
                 },
                 beforeChange: (newVal) => {
                     const item = newVal[0];
@@ -52,7 +57,9 @@ export default class extends Controller {
                             const searchInput = this.element.querySelector('.ss-search input');
                             if (searchInput) {
                                 searchInput.value = '';
-                                searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+                                searchInput.dispatchEvent(
+                                    new Event('input', { bubbles: true })
+                                );
                                 searchInput.focus();
                             }
                         }, 10);
@@ -60,26 +67,10 @@ export default class extends Controller {
                 }
             },
         });
-
-        // Make currently selected element not follow link when clicked on, so opening the dropdown is possible
         if(slimSelectDropdown.getSelected()[0]?.startsWith("/")) {
             const linkTag = document.querySelector('.ss-main .dropdown-option-link');
             if (linkTag) linkTag.href = "javascript:void(0)";
         }
-    }
-
-    sortOptions() {
-        const select = this.dropdownTarget;
-        const options = Array.from(select.options);
-
-        const placeholder = options.find(opt => opt.value === "");
-        const sortableOptions = options.filter(opt => opt.value !== "");
-
-        sortableOptions.sort((a, b) => a.text.localeCompare(b.text));
-
-        select.innerHTML = '';
-        if (placeholder) select.appendChild(placeholder);
-        sortableOptions.forEach(opt => select.appendChild(opt));
     }
 
     navigateOnChange(event) {
