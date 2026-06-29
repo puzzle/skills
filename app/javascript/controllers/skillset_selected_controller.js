95771d1aef274a0ccdd7dc925c1cd8343e1b2e14
Miguel <141239860+Miguel7373@users.noreply.github.com>
2026-06-29 15:20:46 +0200

    fix: add tab index to skills filter #1158 (#1192)
    * fix: add tab index #1158
    
    * fix: make filter buttons selectable with enter #1158
    
    * test: add a spec test for tabbing filter buttons #1158
    
    * fix: remove tabindex from radio buttons and add activation with spacebar #1158
    
    * test: update tests for feature #1158
    
    * fix: add buttons instead of radio #1158
    
    * refactor: clean up #1158
    
    * fix: rubocop issues #1158
    
    * fix: rename ids #1158
    
    * test: rename id in tests #1158
    
    ---------
    
    Co-authored-by: schiltpzzle <schilt@puzzle.ch>
    Co-authored-by: Nevio Di Gennaro <digennaro@puzzle.ch>

--- app/javascript/controllers/skillset_selected_controller.js
+++ app/javascript/controllers/skillset_selected_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="skillset-selected"
 export default class extends Controller {
-    static targets = ["selectButton"];
+    static targets = ["selectButton", "form", "defaultSet", "button"];
     selectedButton = null;
 
     selectButton({params}) {
@@ -13,5 +13,15 @@ export default class extends Controller {
         this.selectedButton = params.id;
         this.selectButtonTargets[params.id].classList.add("btn-primary");
         this.selectButtonTargets[params.id].classList.add("text-white");
+    }
+
+    setDefaultSet(event) {
+        this.defaultSetTarget.value = event.currentTarget.dataset.value
+
+        this.buttonTargets.forEach(btn => btn.classList.remove("active-filter-button"))
+
+        event.currentTarget.classList.add("active-filter-button")
+
+        this.formTarget.requestSubmit()
     }
 }
