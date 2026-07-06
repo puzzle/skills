03e6dab2b6b77ae3940204a71244838b9eaf66aa
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-07-06 09:04:06 +0200

    fix: wrong syntax for helper method #1111

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,5 +1,5 @@
 module ProjectHelper
   def humanize_project_technologies(project)
-    project.project_technologies.map(&:technology).join(', ')
+    ([project.technology] + project.skills.map(&:title)).compact_blank.join(', ')
   end
 end
