9fc5266175359818e3c10d8ec78bfebc08b0da81
schiltpzzle <schilt@puzzle.ch>
2026-07-07 09:21:22 +0200

    feat: update cors #1205

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,12 +9,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://skills.puzzle.ch"
+            "https://skills.puzzle.ch",
+            "https://skills-int.puzzle.ch"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
