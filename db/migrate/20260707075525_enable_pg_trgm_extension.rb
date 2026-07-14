4b56929baa9e53235efdfe387476dde6717b840d
Livio Canobbio <canobbio@puzzle.ch>
2026-07-14 13:48:39 +0200

    fix(db): make enablement of trigram no rollbackable  #1145

--- db/migrate/20260707075525_enable_pg_trgm_extension.rb
+++ db/migrate/20260707075525_enable_pg_trgm_extension.rb
@@ -1,5 +1,9 @@
 class EnablePgTrgmExtension < ActiveRecord::Migration[8.1]
-  def change
+  def up
     enable_extension 'pg_trgm'
+  end
+
+  def down
+    # It has to be empty because we don't want to disable the extension during the rollback.
   end
 end
