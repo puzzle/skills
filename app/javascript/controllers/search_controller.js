c377cd23635dba3e59ef697b52b5efc3cc8b9aa5
Nevio Di Gennaro <digennaro@puzzle.ch>
2026-07-17 16:19:01 +0200

    feat: add some minecraft refs to cv search #1221

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
 
@@ -19,12 +19,25 @@ export default class extends Controller {
         const form = e.target.parentElement;
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
+            this.checkInputForEasterEggs(e.target.value)
             form.requestSubmit();
         }, 200)
     }
 
-    whitespaceCheckbox(){
-        const includesWhitespace =  document.getElementById("cv_search_field").value.includes(" ")
+    whitespaceCheckbox() {
+        const includesWhitespace = document.getElementById("cv_search_field").value.includes(" ")
         document.getElementById("handle_whitespaces_checkbox").disabled = !includesWhitespace;
+    }
+
+    checkInputForEasterEggs(value) {
+        const backgroundColor = value === "jeb_" ?  "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet, red)" : ""
+        const rotation = value === "Dinnerbone" ? 180 : 0
+        Object.assign(document.body.style, {
+            transform: `rotate(${rotation}deg)`,
+            transformOrigin: "center",
+        });
+        document.querySelectorAll(".bg-body").forEach(el => {
+            el.style.backgroundImage = backgroundColor;
+        });
     }
 }
